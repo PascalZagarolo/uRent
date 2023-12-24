ebd105e64b78315c842f5bc9d26a989902a4fa72
Pascal <zagarolopascal@gmail.com>
2023-12-24 22:50:30 +0100

    nextauth fix

---
+++ app/api/auth/[. . .nextauth]/route.ts
@@ -0,0 +1,8 @@
+
+import nextAuth from "next-auth";
+import NextAuth from "next-auth/next";
+import { authOptions } from '../../../utils/auth';
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }

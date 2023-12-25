a35703f90c1e49b80789fe6e3a3b9528777d2b77
Pascal <zagarolopascal@gmail.com>
2023-12-25 21:01:24 +0100

    ...

---
+++ app/(main)/_components/sign-in-github.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { Button } from "@/components/ui/button";
+import { GithubIcon } from "lucide-react";
+import { signIn } from "next-auth/react";
+
+const SignInWithGithub = () => {
+
+
+
+    return ( 
+        <div>
+            <Button className="" variant="ghost" size="sm"
+                onClick={() => signIn("github", {
+                    callbackUrl : `${window.location.origin}`
+                })}
+            >
+                    <GithubIcon className="h-4 w-4"/>
+                </Button>
+        </div>
+     );
+}
+ 
+export default SignInWithGithub;
--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,14 +1,28 @@
+import { getServerSession } from "next-auth";
 import Image from "next/image";
+import { authOptions } from "../utils/auth";
 
-const LoginLayout = (
+const LoginLayout = async (
     {children} : { children : React.ReactNode}
 ) => {
+
+    const session = await getServerSession(authOptions)
+
     return ( 
         <div className="w-full h-full">
             <div>
                 <h3 className="font-semibold text-4xl flex justify-center mt-8">
                     <p className="text-blue-800">u</p> Rent
                 </h3>
+                { session ? (
+                    <h2>
+                        Eingeloggt
+                    </h2>
+                ) : (
+                    <h2>
+                        Nicht eingeloggt
+                    </h2>
+                )}
             </div>
             <div className="mt-8">
             {

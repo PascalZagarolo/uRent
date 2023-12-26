8e406d917a341d13c61a566d45e3fe9b681edd0f
Pascal <zagarolopascal@gmail.com>
2023-12-26 19:51:41 +0100

    mainpage

--- app/(dashboard)/_components/header.tsx
+++ app/(dashboard)/_components/header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
     return ( 
-        <div className="w-full h-[60px] bg-blue-800/90 mt-4">
+        <div className="w-full h-[60px] bg-blue-800/90 mt-4 mb-4 outline outline-1 outline-offset-0">
             ..
         </div>
      );

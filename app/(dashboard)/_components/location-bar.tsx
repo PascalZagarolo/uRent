6a760f1e8c138b1f6453664f1f8ce6a741acda6e
Pascal <zagarolopascal@gmail.com>
2023-12-27 13:57:50 +0100

    location perimeter

--- app/(dashboard)/_components/location-bar.tsx
+++ app/(dashboard)/_components/location-bar.tsx
@@ -1,14 +1,24 @@
+'use client'
+
 import { Input } from "@/components/ui/input";
+import { Map } from "lucide-react";
+import LocationPerimeter from "./location-perimeter";
 
 const LocationBar = () => {
-    return ( 
-        <div className="ml-16">
+    return (
+        <div className="ml-16 flex items-center justify-start position: static">
             <Input
-            placeholder="Ich komme aus..."
-            className="mt-2"
+                placeholder="Ich komme aus..."
+                className="mt-2"
             />
+
+            <Map
+                className="mt-2 ml-2 h-10 w-10 text-white flex items-center"
+            />
+
+            <LocationPerimeter/>
         </div>
-     );
+    );
 }
- 
+
 export default LocationBar;
---
+++ app/(dashboard)/_components/location-perimeter.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { Infinity, icons } from 'lucide-react';
+import Select from 'react-select';
+
+const LocationPerimeter = () => {
+
+    const options = [
+        { value : '5', label : '5 km' },
+        { value : '10', label : '10 km' },
+        { value : '20', label : '20 km' },
+        { value : '35', label : '35 km' },
+        { value : '50', label : '50 km' },
+        { value : 'inf' , label : 'Ꝏ',icons : <Infinity/> }
+    ]
+
+    return (
+        <div className='w-[300px]'>
+            <Select options={options}
+        defaultValue={options[0]} 
+        className='flex justify-start mt-2  w-full ml-8 position: fixed'
+        />
+        </div> 
+        
+     );
+}
+ 
+export default LocationPerimeter;

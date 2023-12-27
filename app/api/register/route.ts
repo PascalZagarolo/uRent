04ab4c0f6628e590cec2b9215fb0ae0ead8ee256
Pascal <zagarolopascal@gmail.com>
2023-12-27 15:46:53 +0100

    register route

---
+++ app/api/register/route.ts
@@ -0,0 +1,33 @@
+import bcrypt from 'bcrypt'
+import { NextResponse } from 'next/server'
+
+export async function POST(
+    request: Request,
+) {
+    try {
+        const body = await request.json()
+    const { email,
+            name,
+            password } = body
+
+        if(!email || !name || !password) {
+            return new NextResponse("Fehlende Daten" , { status : 400 })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        const user = await prisma?.user.create({
+            data : {
+                email : email,
+                name : name,
+                hashedPassword
+            }
+        })
+
+        return NextResponse.json(user)
+    } catch(error) {
+        console.log(error);
+        return NextResponse.json("Interner Server Error" , { status : 500 })
+    }
+    
+}

a1e277f874535727736276041ccf3e5d064032bf
Pascal <zagarolopascal@gmail.com>
2023-12-27 22:40:46 +0100

    ...

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,37 +1,38 @@
 import { getServerSession } from "next-auth";
 import Image from "next/image";
 import { authOptions } from "../utils/auth";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const LoginLayout = async (
-    {children} : { children : React.ReactNode}
+    { children }: { children: React.ReactNode }
 ) => {
 
     const session = await getServerSession(authOptions)
 
-    return ( 
+    return (
         <div className="w-full h-full">
             <div>
+
                 <h3 className="font-semibold text-4xl flex justify-center mt-8">
-                    <p className="text-blue-800">u</p> Rent
+                    
+                        <Link href="/" className="flex ">
+                            <p className="text-blue-800">u</p> Rent
+                        </Link>
+                    
                 </h3>
-                { session ? (
-                    <h2>
-                        Eingeloggt
-                    </h2>
-                ) : (
-                    <h2>
-                        Nicht eingeloggt
-                    </h2>
-                )}
+
+
+
             </div>
             <div className="mt-8">
-            {
-                children
-            }
+                {
+                    children
+                }
             </div>
-            
+
         </div>
-     );
+    );
 }
- 
+
 export default LoginLayout;
--- app/(main)/login/page.tsx
+++ app/(main)/login/page.tsx
@@ -1,24 +1,22 @@
 import Image from 'next/image'
 import CredForm from '../_components/login-form'
 import { getServerSession } from 'next-auth'
-import { authOptions } from '@/app/utils/auth'
+
 import { redirect } from 'next/navigation';
 
 
 export default async function Home() {
 
-    const session = await getServerSession(authOptions);
+
 
     
-    if(session) {
-        return redirect('/')
-    }
+   
     return (
         <div>
             <main>
                 <div className='flex justify-center items-center'>
                     <h3 className='font-semibold text-2xl py-8'>
-                        Login
+                        Anmelden
                     </h3>
                 </div>
 

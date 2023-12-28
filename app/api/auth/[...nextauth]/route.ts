20d71ec582b7d767d77fbdfc80bbd781cbfc1530
Pascal <zagarolopascal@gmail.com>
2023-12-28 18:22:38 +0100

    ...

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,80 +1,15 @@
-import bcrypt from "bcrypt"
-import NextAuth, { AuthOptions, getServerSession } from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
-import GithubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google"
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import { db } from "@/app/utils/db"
-import { NextResponse } from "next/server"
+import { authOptions } from "@/app/utils/auth";
+import NextAuth from "next-auth";
 
 
 
-export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(db),
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-    }),
-    CredentialsProvider({
-      name: 'credentials',
-      credentials: {
-        email: { label: 'email', type: 'text' },
-        password: { label: 'password', type: 'password' }
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) {
-          throw new Error('Invalid credentials');
-        }
-
-        const user = await db.user.findUnique({
-          where: {
-            email: credentials.email
-          }
-        });
-
-        if (!user || !user?.hashedPassword) {
-          throw new Error('Invalid credentials');
-        }
-
-        const isCorrectPassword = await bcrypt.compare(
-          credentials.password,
-          user.hashedPassword
-        );
-
-        if (!isCorrectPassword) {
-          throw new Error('Invalid credentials');
-        }
-
-        return user;
-      }
-    })
-  ],
-  debug: process.env.NODE_ENV === 'development',
-  session: {
-    strategy: "jwt",
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-}
 
 
-export async function GET(request: Request) {
-  const session = await getServerSession(authOptions);
-  
-  if (!session) {
-  return new NextResponse(JSON.stringify({ error: "unauthorized" }), {
-  status: 401,
-  });
-  }
-  
-  return NextResponse.json({ authenticated: !!session });
-  }
+
+
+
 
 
 const handler = NextAuth(authOptions);
 
-export { handler as POST };
+export {handler as GET, handler as POST };

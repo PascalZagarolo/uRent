bfa0a6b9af42ba94c3749a29a028abf346d5017d
Pascal <zagarolopascal@gmail.com>
2023-12-29 18:32:35 +0100

    ...

---
+++ app/profile/[profileId]/_components/edit-name-component.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { useParams } from "next/navigation";
+
+const EditName = () => {
+
+    const params = useParams();
+
+
+
+    return ( 
+        <div>
+            ...
+        </div>
+     );
+}
+ 
+export default EditName;

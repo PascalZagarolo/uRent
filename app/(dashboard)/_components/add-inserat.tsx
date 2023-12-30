c29113bb38b9f219f4aba3e3e0c1d40a55fdb8df
Pascal <zagarolopascal@gmail.com>
2023-12-30 17:59:49 +0100

    ...

--- app/(dashboard)/_components/add-inserat.tsx
+++ app/(dashboard)/_components/add-inserat.tsx
@@ -1,21 +1,87 @@
 'use client'
 
 import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import { PlusIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 const Inserat = () => {
     const router = useRouter();
 
-    const onClick = () => {
-        router.push("/inserat/create");
-    }
+    const formSchema = z.object({
+        title : z.string().min(3,{
+            message : "Titel ist zu kurz"
+        })
+    })
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver : zodResolver(formSchema),
+        defaultValues : {
+            title : ""
+        }
+    })
+    
 
     return ( 
         <div>
-            <Button variant="ghost" className="bg-white items-center mt-2 flex" onClick={onClick}>
+            <Dialog >
+                <DialogTrigger>
+                <Button variant="ghost" className="bg-white items-center mt-2 flex">
                 <PlusIcon className="w-4 h-4 mr-2"/> Neue Anzeige 
             </Button>
+                </DialogTrigger>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>
+                            Neue Anzeige erstellen
+                            <p><Separator className="w-8 bg-black mt-2"/></p>
+                        </DialogTitle>
+                        
+                    </DialogHeader>
+                    <div>
+                        <div className="flex justify-center">
+                            <h3 className="mt-8  text-lg font-semibold">Gebe deiner Anzeige einen Titel</h3>
+                            
+
+                        </div>
+                        <p className="font-semibold text-xs text-gray-800/50 flex justify-center mb-8">der Titel kann jederzeit geändert werden</p>
+                        <Form {...form}>
+                            <form>
+                                <FormField
+                                control = {form.control}
+                                name = "title"
+                                render = {({field}) => (
+                                    <FormItem>
+                                        <FormControl>
+                                            <Input
+                                            {...field}
+                                            placeholder="Titel der Anzeige..."
+                                            />
+                                        </FormControl>
+                                        <FormMessage/>
+                                    </FormItem>
+                                )}
+                                />
+                                <p><Separator className="ml-auto w-8 bg-black mt-4"/></p>
+                                <div className="flex ml-auto">
+                                
+                                    <Button className="bg-blue-800 mt-4 flex">
+                                        Anzeige erstellen
+                                    </Button>
+                                </div>
+                            </form>
+                        </Form>
+                    </div>
+                </DialogContent>
+            </Dialog>
+            
         </div>
      );
 }
--- app/inserat/create/_components/input-fields/title-inserat.tsx
+++ app/inserat/create/_components/input-fields/title-inserat.tsx
@@ -1,7 +1,87 @@
+'use client'
+
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { PenIcon } from "lucide-react";
+import { title } from "process";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
 const TitleInserat = () => {
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [currentTitle, setCurrentTitle] = useState("");
+
+    const formSchema = z.object({
+        title : z.string().min(3, {
+            message: "Titel zu kurz"
+        })
+    })
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver : zodResolver(formSchema),
+        defaultValues : {
+            title : ""
+        }
+    })
+
+    const onClick = () => {
+        setIsEditing(isEditing => !isEditing);
+    }
+
+
     return ( 
         <div>
-            s
+            <h1 className="text-xl flex justify-start ml-8 font-semibold">
+                Titel deiner Anzeige <PenIcon className="w-4 h-4 ml-4 mt-1" onClick={onClick}/>
+            </h1>
+            <div className="ml-8 mt-2 w-80">
+                {isEditing ? (
+                    <div className="flex"> 
+                        <Form {...form}>
+                    <form className="flex">
+                        <FormField
+                        control={form.control}
+                        name="title"
+                        render = {({field}) => (
+                            <FormItem>
+                                <FormControl>
+                                    <Input
+                                    {...field}
+                                    className=" w-80"
+                                    />
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>
+                        )}
+                        
+                        />
+                        <div>
+                            <Button variant="ghost" className="ml-4">
+                                Titel speichern
+                            </Button>
+                        </div>
+                    </form>
+                </Form>
+
+                    </div>
+                    
+                ): (
+                    <div>
+                        {currentTitle}
+                    </div>
+                )}
+            
+            </div>
+            
+            {isEditing && (
+                <div>
+                    ja!!
+                </div>
+            )}
         </div>
      );
 }

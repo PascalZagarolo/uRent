0c2e5d775a122465c2b2b058c66a140da19aa1d9
Pascal <zagarolopascal@gmail.com>
2023-12-30 16:11:22 +0100

    ...

--- app/(dashboard)/_components/header.tsx
+++ app/(dashboard)/_components/header.tsx
@@ -1,24 +1,30 @@
 import { useSession } from "next-auth/react";
 import LocationBar from "./location-bar";
 import SearchItem from "./search-item";
+import Inserat from "./add-inserat";
 
-const Header = () => {
+const Header = async () => {
 
-    
 
-    return ( 
-        <div className="w-full h-[60px] bg-blue-800/90 mt-4 mb-4 outline outline-1 outline-offset-0 items-center">
+    return (
+        <div className="w-full h-[60px] bg-blue-800/80 mt-4 mb-4 outline outline-1 outline-offset-0 items-center">
+
             <div className="flex justify-center items-center">
-            <div className="flex justify-center">
-                <SearchItem/>
+
+                <div className="mr-32 items-center">
+                    <Inserat />
+                </div>
+
+                <div className="flex justify-center">
+                    <SearchItem />
+                </div>
+                <div className="flex justify-center">
+                    <LocationBar />
+                </div>
             </div>
-            <div className="flex justify-center">
-                <LocationBar/>
-            </div>
-            </div>
-            
+
         </div>
-     );
+    );
 }
- 
+
 export default Header;
---
+++ app/inserat/create/page.tsx
@@ -0,0 +1,28 @@
+import Logo from "@/app/profile/[profileId]/_components/u-rent-logo";
+import CreationHeader from "./_components/creation-header";
+import InseratBodyLeft from "./_components/inserat-body-left";
+
+const InseratCreation = () => {
+    return (
+        <div>
+            <div className="mt-8 flex justify-center">
+                <Logo />
+            </div>
+
+            <div className="flex justify-center mt-8">
+                <CreationHeader />
+            </div>
+            <div className="grid grid-cols-2 gap-4 mt-8">
+                <div className="col-span-1 bg-gray-300">
+                    <InseratBodyLeft/>
+                </div>
+                <div className="col-span-1 bg-gray-300">
+                    dasda
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default InseratCreation;

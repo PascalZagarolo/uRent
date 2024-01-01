9ca4ae48a286454804f6e6e190625279f3603f33
Pascal <zagarolopascal@gmail.com>
2024-01-01 17:43:31 +0100

    ...

--- app/inserat/create/_components/creation-header.tsx
+++ app/inserat/create/_components/creation-header.tsx
@@ -1,8 +1,21 @@
-const CreationHeader = () => {
+import { Inserat } from "@prisma/client";
+
+interface CreationHeaderProps {
+    inserat : Inserat;
+}
+
+const CreationHeader: React.FC<CreationHeaderProps> = ({
+    inserat
+}) => {
     return ( 
         <div>
             <h3 className="text-3xl font-semibold"> Anzeige bearbeiten </h3>
-            <p className=" flex justify-center text-base outline outline-offset-2 outline-2 mt-2 font-bold rounded-md"> noch nicht veröffentlicht </p>
+            { inserat.isPublished ? (
+                <p className=" flex justify-center text-base outline outline-offset-2 outline-2 mt-2 font-bold rounded-md">  veröffentlicht </p>
+            ) : (
+                <p className=" flex justify-center text-base outline outline-offset-2 outline-2 mt-2 font-bold rounded-md"> noch nicht veröffentlicht </p>
+            )}
+            
         </div>
      );
 }
--- app/inserat/create/_components/input-fields/select-category.tsx
+++ app/inserat/create/_components/input-fields/select-category.tsx
@@ -1,7 +1,37 @@
+'use client';
+
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { CarFront } from "lucide-react";
+import { useEffect, useState } from "react";
 
 const SelectCategoryInserat = () => {
+
+    type Categories = {
+        pkw : string;
+        lkw : string;
+        land : string;
+        bau : string;
+        trailor : string;
+        caravan : string
+    }
+
+    const initialValues : Categories = {
+        pkw : "pkw",
+        lkw : "lkw",
+        land : "land",
+        bau : "bau",
+        trailor : "trailor",
+        caravan : "caravan"
+    }
+
+    
+
+    const [category, setCategory] = useState<Categories | string>(initialValues);
+
+    useEffect(() => {
+
+    }, [category])
+
     return (
         <div className="ml-4 mt-8 flex items-center">
             <CarFront className="mr-4"/>
@@ -10,17 +40,23 @@ const SelectCategoryInserat = () => {
                     <SelectValue className="font-bold" placeholder="Wähle deine Fahrzeugkategorie" />
                 </SelectTrigger>
                 <SelectContent>
-                    <SelectGroup>
-                        <SelectLabel className="font-bold">Fahrzugkategorien</SelectLabel>
+                    
+                        <SelectLabel className="font-bold">Fahrzeugkategorien</SelectLabel>
                         <SelectItem value="pkw">PKW</SelectItem>
                         <SelectItem value="lkw">LKW</SelectItem>
                         <SelectItem value="land">Landwirtschaft</SelectItem>
                         <SelectItem value="bau">Baumaschinen</SelectItem>
-                        <SelectItem value="trailor">Anhänger</SelectItem>
+                        <SelectItem value="trailor" >Anhänger</SelectItem>
                         <SelectItem value="caravan">Wohnmobil</SelectItem>
-                    </SelectGroup>
+                    
                 </SelectContent>
             </Select>
+            
+            {category === "pkw" && (
+                <div>
+                    PKW ausgesucht :D
+                </div>
+            )}
         </div>
     );
 }

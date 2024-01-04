19d32e34366d4d7b6e6048c8bcc0e29fd8c1d7d2
Pascal <zagarolopascal@gmail.com>
2024-01-05 00:56:08 +0100

    ...

--- app/profile/[profileId]/_components/profil-anzeigen.tsx
+++ app/profile/[profileId]/_components/profil-anzeigen.tsx
@@ -1,9 +1,9 @@
 const ProfilAnzeigen = () => {
     return ( 
         <div>
-            <h3 className="text-xl font-semibold">Aktive Anzeigen</h3>
+            <h3 className="text-2xl font-bold">Aktive Anzeigen</h3>
             <div>
-                keine...
+                
             </div>
         </div>
      );

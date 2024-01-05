5251332e31eb18ce2b1d71627a922c63d549bcf4
Pascal <zagarolopascal@gmail.com>
2024-01-05 15:01:00 +0100

    ...

--- app/(dashboard)/_components/inserat-card.tsx
+++ app/(dashboard)/_components/inserat-card.tsx
@@ -1,6 +1,7 @@
+import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { Images, Inserat, User } from "@prisma/client";
-import { CarFront } from "lucide-react";
+import { Banknote, CalendarCheck2, CarFront, LocateFixedIcon, MapPinIcon, Star } from "lucide-react";
 import Image from "next/image";
 
 interface InseratCardProps {
@@ -18,9 +19,14 @@ const InseratCard: React.FC<InseratCardProps> = ({
       };
 
     return (
-        <div className="w-[400px] h-[300px]  border-black border-2 rounded-md">
-            <h3 className="flex justify-start font-semibold mt-1 ml-2">
-                <CarFront className="mr-2" /> {inserat.title} 
+        <div className="w-[400px] h-[320px]  border-black border-2 rounded-md">
+            <h3 className="flex justify-stretch font-semibold mt-1 ml-2 text-lg">
+                <CarFront className="" /> <p className="flex ml-4"> {inserat.title} </p>
+                <div className="ml-auto items-center flex mr-4">
+                <p className="font-bold text-gray-800/50 italic text-xs">23.10.23</p>
+                <p className="ml-4"><Star className="hover:text-yellow-800/20"/></p>
+                </div>  
+                
             </h3>
             <Separator
             className="w-8 bg-black rounded-lg ml-2 mt-2"
@@ -40,29 +46,36 @@ const InseratCard: React.FC<InseratCardProps> = ({
                 className="ml-2 w-8 bg-black"
                 />
                 <div className="flex mt-2">
-                    <p className="text-gray-900/80 font-black mr-4 ">
-                        Zeitraum : 
+                    <p className="text-gray-900/80 font-bold mr-4 flex">
+                      <CalendarCheck2 className="mr-2"/>  Zeitraum : 
                     </p>
-                    <p className="font-semibold">
+                    <p className="font-semibold text-[#434b70]">
                         {formatDate(inserat.begin)}
                     </p>
                     <p className="font-bold text-blue-800 mr-2 ml-2">
                         -
                     </p>
-                    <p className="font-semibold">
+                    <p className="font-bold text-[#434b70]">
                     {formatDate(inserat.end)}
                     </p>
                 </div>
 
-                <div className="font-semibold text-gray-900 flex ">
-                    <p className="mr-2">
+                <div className="font-semibold text-gray-900 flex mt-1 items-center ">
+                    <p className="mr-2 flex font-bold">
+                        <Banknote
+                        className="mr-2"/>
                         Preis : 
                     </p>
                     {inserat.price} €
+                    <div className="ml-auto mr-4 flex items-center">
+                        <MapPinIcon className="text-rose-600 mr-2"/> Mömer <p className="text-gray-800/50 text-xs ml-2">(187 Km)</p>
+                    </div>
                 </div>
             </div>
+            <div className="mt-8">
+
             <div className="rounded-md bg-[#6e7ab3]">
-                <div className="flex mt-4 items-center border border-black rounded-md">
+                <div className="flex mt-4 items-center border border-black rounded-md ">
                     <Image
                     className="rounded-full ml-2 mt-2 mb-2 border border-[#555e89] "
                     src={inserat.user.image}
@@ -73,8 +86,17 @@ const InseratCard: React.FC<InseratCardProps> = ({
                     <p className="ml-4 font-semibold text-gray-900">
                         {inserat.user.name}
                     </p>
+                    <div className="ml-auto mr-2">
+                        <Button className="bg-[#505881]  border border-white  font-semibold">
+                            Besichtigen
+                        </Button>
+                    </div>
                 </div>
+            </div> 
             </div>
+            
+
+            
         </div>
     );
 }

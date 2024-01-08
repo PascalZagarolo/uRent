ec2ee19fa239c91249acbe7731b813ee206c4799
Pascal <zagarolopascal@gmail.com>
2024-01-08 13:04:06 +0100

    favourite api

---
+++ app/api/profile/[profileId]/favourites/route.ts
@@ -0,0 +1,25 @@
+import { db } from "@/app/utils/db";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+    req : Request, 
+    { params } : { params : { profileId : string }}
+) {
+    try {
+
+        const { inseratId } = await req.json();
+
+        const patchedFavourites = await db.favourites.update({
+            where : {
+                userId : params.profileId
+            }, data :{
+                inseratId : inseratId
+            }
+        })
+
+        return NextResponse.json(patchedFavourites);
+    } catch(error) {
+        console.log("Fehler in favourites..", error);
+        return new NextResponse("Interner Server Error", { status : 500 })
+    }
+}

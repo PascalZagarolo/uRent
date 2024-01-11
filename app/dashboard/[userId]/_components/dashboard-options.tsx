2b5c9c9e45624d07c9706a88c4e9b52de8a54149
Pascal <zagarolopascal@gmail.com>
2024-01-11 17:12:21 +0100

    ...

---
+++ app/dashboard/[userId]/_components/dashboard-options.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import { ArrowRight, LucideIcon } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+
+
+
+interface DashboardOptionsProps {
+    label : string;
+    icon : LucideIcon;
+    link : string;
+    userId : string
+}
+
+
+const DashboardOptions: React.FC<DashboardOptionsProps> = ({
+    label,
+    icon : Icon,
+    link,
+    userId
+}) => {
+
+    const pathname = usePathname();
+
+    const isActive = pathname.includes(link)
+
+    const router = useRouter();
+
+    const onPush = () => {
+        if (isActive) {
+            router.push(
+                `/dashboard/${userId}`
+            )
+        } else {
+            router.push(
+                `${pathname}/${link}`
+            )
+        }
+    }
+
+    return ( 
+        <div className="mt-6 flex ml-4 hover:cursor-pointer" onClick={onPush}>
+            { isActive && (
+                <ArrowRight
+                className="h-4 w-4 text-[#44448e]"
+                />
+            )}
+            <div className="flex ml-4 justify-center">
+                <p>
+                    <Icon size={24} />
+                </p>
+                <p className="ml-4 text-sm font-semibold">
+                    {label}
+                </p>
+               
+            </div>
+           
+        </div>
+     );
+}
+ 
+export default DashboardOptions;

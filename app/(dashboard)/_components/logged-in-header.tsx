87c96fe25c5c0b41435ad58054cf5beb5cedca5f
Pascal <zagarolopascal@gmail.com>
2024-01-12 13:30:31 +0100

    <p> in <p> error fix

--- app/(dashboard)/_components/logged-in-header.tsx
+++ app/(dashboard)/_components/logged-in-header.tsx
@@ -24,10 +24,10 @@ const LoggedInBarHeader: React.FC<LoggedInBarHeaderProps> = ({
    
     return ( 
         <div className="flex justify-start  items-center mt-2">
-            <p className="flex font-semibold mr-4 text-sm text-gray-200 items-center">
+            <div className="flex font-semibold mr-4 text-sm text-gray-200 items-center">
                     Willkommen zurück
                     <p className="ml-1 font-bold  text-gray-100 mr-4">{currentUser.name.toUpperCase()}</p>
-                </p>
+                </div>
             <Button variant="ghost"  className="outline outline-offset-1 outline-1 mr-4 bg-[#2e3552]" onClick={onClick}>
                 <UserIcon className="text-black"/>
             </Button>

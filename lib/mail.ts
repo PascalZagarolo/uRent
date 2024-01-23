27cfdd714900425b7285d2d2187156053c9d84f8
Pascal <zagarolopascal@gmail.com>
2024-01-23 23:20:39 +0100

    ...

--- lib/mail.ts
+++ lib/mail.ts
@@ -10,7 +10,7 @@ export const sendVerificationEmail = async (
     const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
 
     await resend.emails.send({
-        from: "onboarding@resend.dev",
+        from: "support@u-rent-rental.de",
         to: email,
         subject: "Bestätige deine uRent-Anmeldung",
         html:`<p> Klick  <a href="${confirmLink}"> hier </a> um deinen Account zu verifizieren </p>`,

c1ce8ed490341389663ba6c3118fc32307add234
Pascal <zagarolopascal@gmail.com>
2024-01-25 15:09:37 +0100

    ...

--- app/inserat/create/_components/inserat-body-left.tsx
+++ app/inserat/create/_components/inserat-body-left.tsx
@@ -6,50 +6,58 @@ import SelectCategoryInserat from "./input-fields/select-category";
 import { BookOpen, Car } from "lucide-react";
 import RentPrice from "./input-fields/rent-price";
 import { Separator } from "@/components/ui/separator";
+import Sitze from "./input-fields/attribute-fields/sitze";
 
 
 
 interface InseratBodyLeftProps {
-    inserat : Inserat;
+    inserat: Inserat;
 }
 
 const InseratBodyLeft: React.FC<InseratBodyLeftProps> = ({
     inserat
 }) => {
-    return ( 
+    return (
         <div className="p-4 bg-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]  ml-8 rounded-md mr-8 ">
             <h3 className="font-bold italic flex">
-                <BookOpen className="mr-2"/> Anzeigendetails
+                <BookOpen className="mr-2" /> Anzeigendetails
             </h3>
             <p className="text-xs text-gray-900/50 font-medium">versehe deine Anzeige mit nützlichen Information - desto genauer du bist, desto einfacher finden dich Interessenten</p>
-            <Separator className="bg-gray-400 mt-2 w-1/4"/>
+            <Separator className="bg-gray-400 mt-2 w-1/4" />
             <TitleInserat
-            inserat={inserat}
+                inserat={inserat}
             />
             <div className="mt-8">
                 <DescriptionInserat
-                inserat={inserat}
+                    inserat={inserat}
                 />
             </div>
             <div className="ml-4 mr-8">
                 <div >
-                    <h3 className="flex justify-center text-xl mt-8 font-semibold bg-gray-700 p-4 text-gray-100 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.2)] border-gray-800 border-2"><Car className="mr-4 h-6 w-6"/>
-                    Spezifikationen deiner Anzeige
+                    <h3 className="flex justify-center text-xl mt-8 font-semibold bg-gray-700 p-4 text-gray-100 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.2)] border-gray-800 border-2"><Car className="mr-4 h-6 w-6" />
+                        Spezifikationen deiner Anzeige
                     </h3>
                 </div>
-                <SelectCategoryInserat
-                inserat={inserat}
-                />
+                <div className="flex w-full">
+                    <div className="w-1/2">
+                        <SelectCategoryInserat
+                            inserat={inserat}
+                        />
+                    </div>
+                    <div>
+                        <Sitze/>
+                    </div>
+                </div>
             </div>
             <div>
                 <RentPrice
-                inserat={inserat}
+                    inserat={inserat}
                 />
             </div>
-                
-            
+
+
         </div>
-     );
+    );
 }
- 
+
 export default InseratBodyLeft;

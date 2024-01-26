11604f08d254e920e8e3514b0b638b38dc3b4d86
Pascal <zagarolopascal@gmail.com>
2024-01-26 14:58:41 +0100

    ...

--- app/profile/[profileId]/_components/profile-header.tsx
+++ app/profile/[profileId]/_components/profile-header.tsx
@@ -11,7 +11,7 @@ import { use } from "react";
 
 interface ProfileHeaderProps {
     currentUser: User;
-    user : User;
+    user: User;
 }
 
 
@@ -30,54 +30,84 @@ const ProfileHeader: React.FC<ProfileHeaderProps> = ({
 
     return (
         <div className="ml-16">
-            <div color="mt-4">
-            <NotVerifiedYet
-            email={user.email}
-            />
-            </div>
-            
-            <div className="flex  mt-4 p-8 rounded-md border-2 border-gray-300 w-1/2 bg-[#10121a] text-gray-100 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+            {!user.confirmedMail && (
+                <div className="mt-4">
+                    <NotVerifiedYet
+                        email={user.email}
+                    />
+                </div>
+            )}
+
+            <div className="flex   p-8 rounded-md border-2 border-gray-300 w-1/2 bg-[#10121a] text-gray-100 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
                 <h3 className="text-4xl font-semibold flex justify-start items-center">
                     <p className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]  text-[#475aa7] text-5xl">P</p> rofilübersicht
                 </h3>
 
             </div>
             <div>
-            <p className="flex font-bold text-sm text-gray-900/60"> Offline</p>
-            <div className="mt-8 text-xl font-semibold ml-2 flex">
-            <BookUser /> Profildetails
-            </div>
-            <div className="flex">
-            
-            <div className="flex mt-1   p-8  bg-gray-100 border-2 border-gray-900 mr-16 rounded-md w-1/2">
-            
-                <div className="">
-                    <div className="bg-[#10121a] text-gray-100 p-2  rounded-lg border-2 border-gray-100 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
-                        <p className="font-bold text-lg  drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.4)] flex justify-center">
-                            {currentUser.name.charAt(0).toUpperCase() || ""}{currentUser.name.slice(1)}
-                        </p>
-                    </div>
-                    <div className="flex mt-4">
-                    <Avatar
-                imageUrl={currentUser.image}
-                />
-               <div className="items-center ml-8">
-               <UploadProfilePic/>
-               <div className="text-gray-900 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.2)] mt-2 flex">
-                <UserCircle2 className="mr-1"/> Mitglied seit : {convertDateTimeToString(currentUser.createdAt)}
+                <p className="flex font-bold text-sm text-gray-900/60"> Offline</p>
+                <div className="mt-8 text-xl font-semibold ml-2 flex">
+                    <BookUser /> Profildetails
                 </div>
-               </div>
+                <div className="flex">
+
+                    <div className="flex mt-1   p-8  bg-white border-2 border-gray-900 mr-16 rounded-md w-1/2">
+
+                        <div className="">
+                            <div className="bg-[#10121a] text-gray-100 p-2  rounded-lg border-2 border-gray-100 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+                                <p className="font-bold text-lg  drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.4)] flex justify-center">
+                                    {currentUser.name.charAt(0).toUpperCase() || ""}{currentUser.name.slice(1)}
+                                </p>
+                            </div>
+                            <div className="flex mt-4">
+                                <Avatar
+                                    imageUrl={currentUser.image}
+                                />
+                                <div className="items-center ml-8">
+                                    <UploadProfilePic />
+                                    <div className="text-gray-900 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.2)] mt-2 flex">
+                                        <UserCircle2 className="mr-1" /> Mitglied seit : {convertDateTimeToString(currentUser.createdAt)}
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
                     </div>
-                
+
                 </div>
-                
-            </div>
-            </div>
-            
-            
+                <div className="mt-8 text-xl font-semibold ml-2 flex">
+                    <UserCircle2 /> Über mich
+                </div>
+                <div className="flex ">
+
+                    <div className="flex mt-1   p-8  bg-white border-2 border-gray-900 mr-16 rounded-md w-1/2">
+
+                        <div className="">
+                            <div className="bg-[#10121a] text-gray-100 p-2  rounded-lg border-2 border-gray-100 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+                                <p className="font-bold text-lg  drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.4)] flex justify-center">
+                                    {currentUser.name.charAt(0).toUpperCase() || ""}{currentUser.name.slice(1)}
+                                </p>
+                            </div>
+                            <div className="flex mt-4">
+                                <Avatar
+                                    imageUrl={currentUser.image}
+                                />
+                                <div className="items-center ml-8">
+                                    <UploadProfilePic />
+                                    <div className="text-gray-900 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.2)] mt-2 flex">
+                                        <UserCircle2 className="mr-1" /> Mitglied seit : {convertDateTimeToString(currentUser.createdAt)}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
 
-           
+
         </div>
     );
 }

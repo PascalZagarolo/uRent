fa313890977d16edfaceb3427897bc498e77bb9f
Pascal <zagarolopascal@gmail.com>
2024-01-26 21:02:21 +0100

    ...

--- app/profile/[profileId]/_components/right-side.tsx
+++ app/profile/[profileId]/_components/right-side.tsx
@@ -1,7 +1,11 @@
+import { Milestone, Signal } from "lucide-react";
+
 const RightSideProfile = () => {
     return ( 
-        <div>
-            ...
+        <div className="">
+            <h3 className=" flex justify-center p-4 text-3xl font-bold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.2)] items-center">
+               <Milestone className="mr-2"/> Meine Inhalte
+            </h3>
         </div>
      );
 }

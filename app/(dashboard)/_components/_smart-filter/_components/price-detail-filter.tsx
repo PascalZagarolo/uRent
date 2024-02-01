bf4fb6434662d0afec04e671afeb76b3176ad2b5
Pascal <zagarolopascal@gmail.com>
2024-02-01 17:19:44 +0100

    ...

---
+++ app/(dashboard)/_components/_smart-filter/_components/price-detail-filter.tsx
@@ -0,0 +1,168 @@
+'use client'
+
+
+import * as React from "react"
+import qs from "query-string"
+
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import { Banknote, Link } from "lucide-react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { z } from "zod";
+import { useForm, FieldValues } from 'react-hook-form';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+
+
+const PriceDetailForm = () => {
+
+    const router = useRouter();
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+    const currentTitle = searchParams.get("title");
+    const category = searchParams.get("category");
+
+    const [currentStart, setCurrentStart] = React.useState(null);
+    const [currentEnd, setCurrentEnd] = React.useState(null);
+
+    const startPrice = searchParams.get("start");
+    const endPrice = searchParams.get("end");
+    
+
+
+    const onClick = (startPrice: string, endPrice: string) => {
+
+        setCurrentStart(Number(startPrice));
+        setCurrentEnd(Number(endPrice));
+
+        const url = qs.stringifyUrl({
+            url: pathname,
+            query: {
+                title: currentTitle,
+                category: category,
+                end: endPrice,
+                start: startPrice,
+                
+            }
+        }, { skipNull: true, skipEmptyString: true });
+
+        router.push(url)
+    }
+
+    const onPriceReset = () => {
+        setCurrentStart(null);
+        setCurrentEnd(null);
+
+        const url = qs.stringifyUrl({
+            url: pathname,
+            query: {
+                title: currentTitle,
+                category: category,
+                
+                
+            }
+        }, { skipNull: true, skipEmptyString: true });
+
+        router.push(url)
+    }
+
+    const formSchema = z.object({
+        start: z.string().optional(),
+        end: z.string().optional()
+    })
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver : zodResolver(formSchema),
+        defaultValues : {
+            start : null,
+            end : null
+        }
+    })
+   
+
+    const onStartPrice = (values : z.infer<typeof formSchema>) => {
+        console.log(values);
+    }
+
+    const onEndPrice = (values : z.infer<typeof formSchema>) => {
+        console.log(values);
+    }
+
+
+
+    return (
+        <div>
+            
+            <div className="flex gap-x-32 mt-2 justify-center">
+                <div >
+                    <h3 className="text-sm  text-gray-900 font-medium mb-1 flex justify-center">
+                        Von :
+                    </h3>
+                    <Select onValueChange={(e) => onClick(e, currentEnd)} value={startPrice || "0"} defaultValue="Start" >
+                        <SelectTrigger className="w-[120px] font-semibold rounded-lg border-[#282c45]">
+                            <SelectValue className="font-bold" placeholder="Start" />
+                        </SelectTrigger>
+                        <SelectContent className="" >
+                            <SelectGroup>
+                                <SelectLabel>Startpreis</SelectLabel>
+                                
+                                <SelectItem value="0" className="font-bold" >0 €</SelectItem>
+                                <SelectItem value="50" className="font-bold">50 €</SelectItem>
+                                <SelectItem value="75" className="font-bold">75 €</SelectItem>
+                                <SelectItem value="100" className="font-bold">100 €</SelectItem>
+                                <SelectItem value="125" className="font-bold">125 €</SelectItem>
+                                <SelectItem value="150" className="font-bold">150 €</SelectItem>
+                                <SelectItem value="200" className="font-bold">200 €</SelectItem>
+                                <SelectItem value="250" className="font-bold">250 €</SelectItem>
+                                <SelectItem value="300" className="font-bold">300 €</SelectItem>
+                                <SelectItem value="400" className="font-bold">400 €</SelectItem>
+                                <SelectItem value="500" className="font-bold">500 €</SelectItem>
+                                
+                            </SelectGroup>
+                        </SelectContent>
+                    </Select>
+                </div>
+                <div className="">
+                    <h3 className="text-sm  text-gray-900 font-medium mb-1 flex justify-center">
+                        Bis :
+                    </h3>
+                    <Select onValueChange={(e) => onClick(currentStart, e)} value={endPrice || "max"} >
+                        <SelectTrigger className="w-[120px] font-semibold rounded-lg border-[#282c45]">
+                            <SelectValue className="font-bold" placeholder="Ende"/>
+                        </SelectTrigger>
+                        <SelectContent >
+                            <SelectGroup>
+                                <SelectLabel>Endpreis</SelectLabel>
+                                <SelectItem value="0" className="font-bold" >0 €</SelectItem>
+                                <SelectItem value="50" className="font-bold">50 €</SelectItem>
+                                <SelectItem value="75" className="font-bold">75 €</SelectItem>
+                                <SelectItem value="100" className="font-bold">100 €</SelectItem>
+                                <SelectItem value="125" className="font-bold">125 €</SelectItem>
+                                <SelectItem value="150" className="font-bold">150 €</SelectItem>
+                                <SelectItem value="200" className="font-bold">200 €</SelectItem>
+                                <SelectItem value="250" className="font-bold">250 €</SelectItem>
+                                <SelectItem value="300" className="font-bold">300 €</SelectItem>
+                                <SelectItem value="400" className="font-bold">400 €</SelectItem>
+                                <SelectItem value="500" className="font-bold">500 €</SelectItem>
+                                <SelectItem value="max" className="font-bold"> &lt; 500 €</SelectItem>
+                            </SelectGroup>
+                        </SelectContent>
+                    </Select>
+                        
+                </div>
+                
+            </div>
+            
+        </div>
+    );
+}
+
+export default PriceDetailForm;
---
+++ app/(dashboard)/_components/filter-dialog.tsx
@@ -0,0 +1,93 @@
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogHeader, DialogTrigger } from "@/components/ui/dialog";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Separator } from "@/components/ui/separator";
+import { Label } from "@radix-ui/react-label";
+
+import { AlignLeft, DollarSignIcon, Settings, Settings2Icon } from "lucide-react";
+import PriceFormFilter from "./_smart-filter/_components/price";
+import PriceDetailForm from "./_smart-filter/_components/price-detail-filter";
+
+const FilterDialog = () => {
+    return (
+        <div>
+            <Dialog>
+                <DialogTrigger className="mr-4">
+                    <Button className="p-2 bg-white rounded-md text-gray-900 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] border border-gray-100 hover:bg-gray-200">
+                        <Settings2Icon />
+                    </Button>
+                </DialogTrigger>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogHeader className="italic font-semibold text-xl mb-4">
+                            Anzeigen filtern
+                        </DialogHeader>
+                    </DialogHeader>
+                    <div>
+                        <div>
+                        <p className="text-medium flex font-semibold mb-2 items-center"> 
+                            <DollarSignIcon className="mr-1"/> Preis 
+                            <Separator className="w-2/3 ml-4 bg-gray-400 h-[0.5px]"/> 
+                            </p>
+                            <PriceDetailForm/>
+                        </div>
+                        <div className="mt-4">
+                            <p className="text-medium flex font-semibold mb-2 items-center"> 
+                            <AlignLeft className="mr-1"/> Kategorie 
+                            <Separator className="w-2/3 ml-4 bg-gray-400 h-[0.5px]"/> 
+                            </p>
+
+                            <RadioGroup className="flex flex-wrap text-sm">
+                                
+                                <div className="flex items-center mr-4 mb-2">
+                                    <RadioGroupItem value="all" />
+                                    <Label className="ml-1 font-semibold">Alle</Label>
+                                </div>
+
+                                <div className="flex items-center mr-4 mb-2">
+                                    <RadioGroupItem value="PKW" />
+                                    <Label className="ml-1 font-semibold">PKW</Label>
+                                </div>
+
+                                <div className="flex items-center mr-4 mb-2">
+                                    <RadioGroupItem value="LKW" />
+                                    <Label className="ml-1 font-semibold">LKW</Label>
+                                </div>
+
+                                <div className="flex items-center mr-4 mb-2">
+                                    <RadioGroupItem value="LAND" />
+                                    <Label className="ml-1 font-semibold">Land</Label>
+                                </div>
+
+                                <div className="flex items-center mr-4 mb-2">
+                                    <RadioGroupItem value="BAU" />
+                                    <Label className="ml-1 font-semibold">Bau</Label>
+                                </div>
+
+                                <div className="flex items-center mr-4 mb-2">
+                                    <RadioGroupItem value="CARAVAN" />
+                                    <Label className="ml-1 font-semibold">Wohnmobile</Label>
+                                </div>
+
+                                <div className="flex items-center mr-4 mb-2">
+                                    <RadioGroupItem value="TRAILOR" />
+                                    <Label className="ml-1 font-semibold">Anhänger</Label>
+                                </div>
+                            </RadioGroup>
+
+                        </div>
+                        <div className="mt-4">
+                        <p className="text-medium flex font-semibold mb-2 items-center"> 
+                            <Settings className="mr-1"/> Spezifikationen 
+                            <Separator className="w-2/3 ml-4 bg-gray-400 h-[0.5px]"/> 
+                            </p>
+                        </div>
+                    </div>
+
+                </DialogContent>
+            </Dialog>
+        </div>
+    );
+}
+
+export default FilterDialog;

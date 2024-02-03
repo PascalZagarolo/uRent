51b43f08e20df0af309829273c1b2633c18d9aab
Pascal <zagarolopascal@gmail.com>
2024-02-03 15:12:45 +0100

    homepage

---
+++ app/(landingpage)/_components/landing-page-header.tsx
@@ -0,0 +1,23 @@
+import { TruckIcon } from "lucide-react";
+import NavigationBar from "./navigation-bar";
+import RentNow from "./rent-now";
+
+const LandingPageHeader = () => {
+    return ( 
+        <div>
+            <div className="p-8 flex items-center">
+                <h3 className="flex font-semibold text-2xl items-center">
+                  <TruckIcon className="mr-1"/>  <p className="flex italic text-[#414c78]">u</p>Rent
+                </h3>
+                <div className="ml-16">
+                    <RentNow/>
+                </div>
+                <div className="flex ml-auto">
+                <NavigationBar/>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default LandingPageHeader;

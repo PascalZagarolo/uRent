fd4766b6d0436749c36508e8170fe87bf43bd120
Pascal <zagarolopascal@gmail.com>
2024-02-03 17:02:22 +0100

    ...

--- app/(landingpage)/_components/landing-page-header.tsx
+++ app/(landingpage)/_components/landing-page-header.tsx
@@ -5,7 +5,7 @@ import RentNow from "./rent-now";
 const LandingPageHeader = () => {
     return ( 
         <div>
-            <div className="p-8 flex items-center">
+            <div className="p-8 flex items-center bg-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
                 <h3 className="flex font-semibold text-2xl items-center">
                   <TruckIcon className="mr-1"/>  <p className="flex italic text-[#414c78]">u</p>Rent
                 </h3>

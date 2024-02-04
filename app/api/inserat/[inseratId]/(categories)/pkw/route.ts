d3f8ecf2ee7e2019dbea4c8594f547494e972011
Pascal <zagarolopascal@gmail.com>
2024-02-04 14:55:05 +0100

    ...

---
+++ app/api/inserat/[inseratId]/(categories)/pkw/route.ts
@@ -0,0 +1,45 @@
+import { db } from "@/utils/db";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+    req : Request,
+    { params } : { params : { inseratId : string }}
+) {
+    try {
+
+        const values = await req.json();
+
+        const findAttributes = await db.pkwAttribute.findFirst({
+            where : {
+                inseratId : params.inseratId
+            }
+        })
+
+        if(!findAttributes) {
+            const patchedInserat = await db.pkwAttribute.create({
+                data: {
+                    inseratId : params.inseratId,
+                    ...values
+                }
+            })
+            return NextResponse.json(patchedInserat);
+            
+        } else {
+            const patchedInserat = await db.pkwAttribute.update({
+                where : {
+                    inseratId : params.inseratId
+                }, data : {
+                    ...values
+                }
+            })
+            return NextResponse.json(patchedInserat);
+        }
+
+        
+
+    } catch (error) {
+        console.log(error);
+        return new NextResponse(error, { status: 500 });
+        
+    }
+}

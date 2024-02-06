082977f29f3ead9076f9796ad6fc1fd9bb6bdead
Pascal <zagarolopascal@gmail.com>
2024-02-06 15:23:05 +0100

    remove margin right on input bar

--- app/(dashboard)/_components/search-item.tsx
+++ app/(dashboard)/_components/search-item.tsx
@@ -32,7 +32,7 @@ const SearchItem = () => {
     }, [debouncedValue, router, pathname])
 
     return (
-        <div className="flex items-center justify-start position: static mr-16 ">
+        <div className="flex items-center justify-start position: static sm:mr-16 ">
 
             <FilterDialog/>
             

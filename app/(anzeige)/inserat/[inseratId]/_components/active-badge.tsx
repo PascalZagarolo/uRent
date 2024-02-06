a1efb62ab2d66644ff9b2f3c63673c594ad17757
Pascal <zagarolopascal@gmail.com>
2024-02-06 15:55:03 +0100

    ...

--- app/(anzeige)/inserat/[inseratId]/_components/active-badge.tsx
+++ app/(anzeige)/inserat/[inseratId]/_components/active-badge.tsx
@@ -6,7 +6,7 @@ const Active = () => {
             <Badge className="bg-emerald-500 border border-black hidden sm:block">
                 Aktiv
             </Badge>
-            <Badge className="bg-emerald-500 border border-black sm:hidden p-2"/>
+            
                 
             
         </div>

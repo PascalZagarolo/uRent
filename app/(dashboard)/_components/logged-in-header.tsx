b4df3fa7f894305b0491d4b5a6122333471a7f39
Pascal <zagarolopascal@gmail.com>
2024-02-07 13:55:03 +0100

    ...

--- app/(dashboard)/_components/logged-in-header.tsx
+++ app/(dashboard)/_components/logged-in-header.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { User } from "@prisma/client";
-import { UserIcon } from "lucide-react";
+import { Settings, Settings2Icon, SettingsIcon, UserIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 
 
@@ -31,6 +31,7 @@ const LoggedInBarHeader: React.FC<LoggedInBarHeaderProps> = ({
             <Button variant="ghost"  className="outline outline-offset-1 outline-1 lg:mr-4 bg-[#e1dfdf] drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] border-2 border-gray-300" onClick={onClick}>
                 <UserIcon className="text-black"/>
             </Button>
+            
         </div>
      );
 }

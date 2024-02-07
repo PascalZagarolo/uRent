e3857a7ed4dfc571c44b135a4da2bf5476bbfc36
Pascal <zagarolopascal@gmail.com>
2024-02-07 20:32:13 +0100

    change name in settings sheet

--- app/(dashboard)/_components/settings-sheet.tsx
+++ app/(dashboard)/_components/settings-sheet.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
@@ -5,10 +7,13 @@ import { Sheet, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetT
 import { Switch } from "@/components/ui/switch";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { User } from "@prisma/client";
+import axios from "axios";
 import { set } from "lodash";
 import { SettingsIcon, UserCheck } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 import { z } from "zod";
 
 interface SettingsSheetProps {
@@ -20,14 +25,20 @@ const SettingsSheet: React.FC<SettingsSheetProps> = ({
     currentUser
 }) => {
 
+    function isWhitespace(str: string): boolean {
+        return /^\s*$/.test(str);
+    }
+
     const [currentName, setCurrentName] = useState(currentUser.name);
     const [currentEmail, setCurrentEmail] = useState(currentUser.email);
 
+    const router = useRouter();
+
     const formSchema = z.object({
-        name: z.string().min(3, {
-            message: "Titel ist zu kurz"
+        name: z.string().min(1, {
+            message: "Name ist zu kurz"
         }),
-        email: z.string().email()
+        
     })
 
     const form = useForm<z.infer<typeof formSchema>>({
@@ -37,8 +48,22 @@ const SettingsSheet: React.FC<SettingsSheetProps> = ({
         }
     })
 
-    const onSubmit = (values: z.infer<typeof formSchema>) => {
-        console.log(values)
+    const onSubmit = () => {
+
+        const values = {
+            name : currentName,
+        }
+
+        try {
+            axios.patch(`/api/profile/${currentUser.id}`, values)
+            toast.success("Änderungen gespeichert");
+            setTimeout(() => {
+                router.refresh();
+            }, 1000)
+
+        } catch {
+            toast.error("Fehler beim Speichern");
+        }
     }
 
     const somethingChanged = currentName !== currentUser.name || currentEmail !== currentUser.email;
@@ -52,7 +77,7 @@ const SettingsSheet: React.FC<SettingsSheetProps> = ({
                 />
             </SheetTrigger>
             <Form {...form}>
-                <form>
+                <form onSubmit={form.handleSubmit(onSubmit)}>
                     <SheetContent>
                         <SheetHeader>
                             <div>
@@ -69,24 +94,17 @@ const SettingsSheet: React.FC<SettingsSheetProps> = ({
                                 <div>
                                     <p className="text-sm mb-2">
                                         Name
-                                    </p>
-                                    <FormField
-                                        control={form.control}
-                                        name="name"
-                                        render={({ field }) => (
-                                            <FormItem>
-                                                <FormControl>
-                                                    <Input
+                                    </p>     
+                                                
+                                           
+                                                <Input
                                                         className="rounded-none border border-gray-300 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] focus:ring-0 focus:border-none "
                                                         defaultValue={currentUser?.name}
-
+                                                        
                                                         onChange={(e) => { setCurrentName(e.target.value) }}
                                                     />
-                                                </FormControl>
-                                                <FormMessage />
-                                            </FormItem>
-                                        )}
-                                    />
+                                                
+                                       
 
                                 </div>
                                 {currentName}
@@ -94,21 +112,12 @@ const SettingsSheet: React.FC<SettingsSheetProps> = ({
                                     <p className="text-sm mt-4 mb-2">
                                         E-Mail
                                     </p>
-                                    <FormField
-                                        control={form.control}
-                                        name="email"
-                                        render={({ field }) => (
-                                            <FormItem>
-                                                <FormControl>
+                                    
                                                     <Input
                                                         className="rounded-none border border-gray-300 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] focus:ring-0 focus:border-none "
                                                         value={currentUser?.email}
                                                     />
-                                                </FormControl>
-                                                <FormMessage />
-                                            </FormItem>
-                                        )}
-                                    />
+                                                
 
                                     <p>
                                         <span className="text-xs text-gray-400 hover:underline hover:cursor-pointer">Ich möchte meine E-Mail ändern</span>
@@ -144,7 +153,11 @@ const SettingsSheet: React.FC<SettingsSheetProps> = ({
 
 
                         <SheetFooter className="mt-8">
-                            <Button className="w-full bg-[#1f2332] drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] border border-gray-500" disabled={!somethingChanged}>
+                            <Button className=
+                            "w-full bg-[#1f2332] drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] border border-gray-500"
+                             disabled={!somethingChanged || isWhitespace(currentName)}
+                             onClick={onSubmit}
+                             >
                                 Änderungen speichern
                             </Button>
                         </SheetFooter>

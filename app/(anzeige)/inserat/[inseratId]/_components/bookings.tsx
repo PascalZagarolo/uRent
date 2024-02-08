1edf05c3fbde7b04e65b3d42ab35a7f8479ac8b2
Pascal <zagarolopascal@gmail.com>
2024-02-08 17:14:15 +0100

    ...

--- app/(anzeige)/inserat/[inseratId]/_components/bookings.tsx
+++ app/(anzeige)/inserat/[inseratId]/_components/bookings.tsx
@@ -1,16 +1,170 @@
-import { Button } from "@/components/ui/button";
-import { Dialog } from "@/components/ui/dialog";
-import { DialogTrigger } from "@radix-ui/react-dialog";
-import { CalendarRange } from "lucide-react";
+"use client"
+ 
+import { zodResolver } from "@hookform/resolvers/zod"
+
+import { format, set } from 'date-fns';
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+ 
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Calendar } from "@/components/ui/calendar"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { toast } from "@/components/ui/use-toast"
+import { values } from "lodash"
+import { CalendarCheck2, CalendarIcon } from "lucide-react"
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog"
+import { useState } from "react";
+
 
 const Bookings = () => {
+
+    const [currentStart, setCurrentStart] = useState(new Date());
+    const [currentEnd, setCurrentEnd] = useState(new Date());
+
+    const formSchema = z.object({
+        start: z.date({
+          required_error: "A date of birth is required.",
+        }), end: z.date({
+            required_error: "A date of birth is required.",
+          }),
+      })
+       
+      const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            start: new Date(),
+            end: new Date(),
+
+        }
+    })
+      
+       
+        const onSubmit = (valus : z.infer<typeof formSchema>) => {
+            console.log(values)
+        }
+
     return ( 
         <Dialog>
-            <DialogTrigger asChild className="p-2">
-                <Button className="sm:w-[240px] w-full  border-2 border-gray-100 bg-white" variant="ghost">
-                  <CalendarRange className="mr-2"/>  Buchungen verwalten
+            <DialogTrigger className="w-[240px]">
+                <Button className="w-full bg-white text-gray-900 border-2 border-gray-900 hover:bg-gray-200">
+                    <CalendarCheck2 className="mr-2 h-4 w-4"/> Buchungen verwalten
                 </Button>
             </DialogTrigger>
+            <DialogContent>
+                <div>
+                    <div className="flex">
+                    <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <div className="flex gap-x-8">
+          <FormField
+            control={form.control}
+            name="start"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Anfangsdatum</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant={"outline"}
+                        className={cn(
+                          "w-[200px] pl-3 text-left font-normal",
+                          !field.value && "text-muted-foreground"
+                        )}
+                      >
+                        {field.value ? (
+                          format(field.value, "PPP")
+                        ) : (
+                          <span>Pick a date</span>
+                        )}
+                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      mode="single"
+                      selected={field.value}
+                      onSelect={field.onChange}
+                      disabled={(date) =>
+                        date > new Date() || date < new Date("1900-01-01")
+                      }
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
+                
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+<FormField
+            control={form.control}
+            name="end"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Enddatum</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant={"outline"}
+                        className={cn(
+                          "w-[200px] pl-3 text-left font-normal",
+                          !field.value && "text-muted-foreground"
+                        )}
+                      >
+                        {field.value ? (
+                          format(field.value, "PPP")
+                        ) : (
+                          <span>Pick a date</span>
+                        )}
+                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      mode="single"
+                      selected={field.value}
+                      onSelect={field.onChange}
+                      disabled={(date) =>
+                        date > new Date() || date < new Date("1900-01-01")
+                      }
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
+                
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          </div>
+
+          <Button type="submit" 
+          className="bg-white border border-gray-300 text-gray-900 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] hover:bg-gray-200">
+            Buchung hinzufügen</Button>
+        </form>
+      </Form>
+                    </div>
+                </div>
+            </DialogContent>
         </Dialog>
      );
 }

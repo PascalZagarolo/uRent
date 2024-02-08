8d4058c78678cc90218b30b97a02d074e026d161
Pascal <zagarolopascal@gmail.com>
2024-02-08 12:26:19 +0100

    return homepage

--- app/profile/[profileId]/page.tsx
+++ app/profile/[profileId]/page.tsx
@@ -5,6 +5,7 @@ import getCurrentUser from "@/actions/getCurrentUser";
 import ProfileFooter from "./_components/profile-footer";
 import RightSideProfile from "./_components/right-side";
 import { Rezension, User } from "@prisma/client";
+import ReturnHomePage from "./_components/return-homepage";
 
 type RezensionWithSender = Rezension & {
     sender: User;
@@ -48,7 +49,10 @@ const ProfilePage = async ({ params } : { params : { profileId : string }}) => {
             <div className="w-full p-4">
             <div className="flex p-8 rounded-md border-2 border-gray-300  bg-[#10121a] text-gray-100 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] w-full ">
                 <h3 className="text-4xl font-semibold flex justify-start items-center">
-                    <p className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]  text-[#475aa7] text-5xl">P</p> rofilübersicht
+                  <div className="mr-4">
+                  <ReturnHomePage/> 
+                </div>
+                     <p className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]  text-[#475aa7] text-5xl">P</p> rofilübersicht
                 </h3>
 
             </div>

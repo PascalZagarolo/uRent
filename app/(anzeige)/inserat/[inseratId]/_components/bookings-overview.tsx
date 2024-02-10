54b8a4c4204b7ebdb16769736006f4dfc32c1070
Pascal <zagarolopascal@gmail.com>
2024-02-10 15:26:47 +0100

    ...

---
+++ app/(anzeige)/inserat/[inseratId]/_components/bookings-overview.tsx
@@ -0,0 +1,57 @@
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { Booking } from "@prisma/client";
+import { CalendarCheck, CalendarClock } from "lucide-react";
+
+interface BookingsOverviewProps{
+    bookings : Booking[]
+}
+
+const BookingsOverview: React.FC<BookingsOverviewProps> = ({
+    bookings
+}) => {
+
+    function formatDateToDDMMYY(date: Date): string {
+        const day: number = date.getDate();
+        const month: number = date.getMonth() + 1; 
+        const year: number = date.getFullYear() % 100; 
+    
+       
+        const formattedDay: string = (day < 10) ? '0' + day : day.toString();
+        const formattedMonth: string = (month < 10) ? '0' + month : month.toString();
+    
+        
+        const formattedYear: string = (year < 10) ? '0' + year : year.toString();
+    
+        return `${formattedDay}.${formattedMonth}.${formattedYear}`;
+    }
+
+    return ( 
+        <Popover>
+            <PopoverTrigger>
+                <CalendarCheck/>
+            </PopoverTrigger>
+            <PopoverContent side="right">
+                <div>
+                    <h1 className="flex font-semibold"> Termine </h1>
+                    <div className="">
+                        {bookings.length > 0 ? (
+                            bookings.map((bookings) => (
+                                <span className="flex mt-2 border border-gray-300 p-4 rounded-md drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.2)] ">
+                                    <CalendarClock/>
+                                    <p className="ml-2 font-semibold text-sm">{formatDateToDDMMYY(bookings.startDate)} - {formatDateToDDMMYY(bookings.endDate)}</p>
+                                </span>
+                            ))
+                        ) : (
+                            <span className="flex mt-2 border border-gray-300 p-4 rounded-md drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.2)] ">
+                                    <CalendarClock/>
+                                    <p className="ml-2 font-semibold text-gray-900/50 italic text-xs">Noch keine Reservierungen vorhanden</p>
+                                </span>
+                        )}
+                    </div>
+                </div>
+            </PopoverContent>
+        </Popover>
+     );
+}
+ 
+export default BookingsOverview;
--- app/(anzeige)/inserat/[inseratId]/page.tsx
+++ app/(anzeige)/inserat/[inseratId]/page.tsx
@@ -1,11 +1,12 @@
 import { db } from "@/utils/db";
 import InseratImageCarousel from "./_components/inserat-image";
-import { AlignLeft, CarFront, MapPin } from "lucide-react";
+import { AlignLeft, Calendar, CarFront, MapPin } from "lucide-react";
 import Active from "./_components/active-badge";
 import ProfileView from "./_components/profile-view";
 import InseratOptions from "./_components/inserat-options";
 import getCurrentUser from "@/actions/getCurrentUser";
 import InseratDescription from "./_components/inserat-description";
+import BookingsOverview from "./_components/bookings-overview";
 
 
 
@@ -34,53 +35,61 @@ const InseratAnzeige = async ({
     })
 
     const inseratArray = await db.inserat.findMany({
-        where : {
-            userId : user.id
+        where: {
+            userId: user.id
         }
     })
 
     const inseratOwner = await db.user.findUnique({
-        where : {
-            id : inserat.userId
+        where: {
+            id: inserat.userId
         }
     })
 
     if (currentUser) {
         const purchases = await db.purchase.findUnique({
-            where : {
-                inseratId_userId : {
-                    inseratId : inserat.id,
-                    userId : currentUser?.id
-                
+            where: {
+                inseratId_userId: {
+                    inseratId: inserat.id,
+                    userId: currentUser?.id
+
                 }
             }
         })
-        
+
     } else {
         const purchases = null;
-        
+
     }
 
     const rezensionen = await db.rezension.findMany({
-        where : {
-            receiverId : user.id
+        where: {
+            receiverId: user.id
         }
     })
 
     let contactOptions;
 
-    if(currentUser) {
-         contactOptions = await db.contactOptions.findUnique({
-            where : {
-                userId : currentUser?.id
+    if (currentUser) {
+        contactOptions = await db.contactOptions.findUnique({
+            where: {
+                userId: currentUser?.id
             }
         })
     }
 
-   
-   
+    const inseratBookings = await db.booking.findMany({
+        where: {
+            inseratId: inserat.id
+        }, orderBy : {
+            startDate : "asc"
+        }
+    })
 
-    
+
+
+
+
 
     return (
         <div className="2xl:grid  2xl:grid-cols-2 xl:flex justify-center  gap-12 xl:mt-24 h-max">
@@ -92,13 +101,15 @@ const InseratAnzeige = async ({
                                 <div className="bg-[#1d1f2b] sm:px-8 rounded-lg p-4">
                                     <CarFront className="" />
                                 </div>
-                                
+
                             )}
                             <p className=" text-md sm:text-xl ml-4 font-bold text-gray-100 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.2)] 
                             bg-[#1d1f2b] px-8 rounded-lg p-4 w-[400px] truncate flex justify-center border
                              border-gray-300"> {inserat.title} </p>
                             <div className="flex justify-end ml-2 sm:ml-4 sm:bg-[#1d1f2b] sm:px-8 rounded-lg sm:p-4">
-                                <Active />
+                                <BookingsOverview
+                                    bookings={inseratBookings}
+                                />
                             </div>
                         </div>
                         <div className="flex items-center gap-x-2 mt-1 sm:text-sm text-xs">
@@ -108,33 +119,33 @@ const InseratAnzeige = async ({
 
 
                         <div className="mt-4 rounded-md     flex justify-center text-gray-900">
-                        <InseratImageCarousel
+                            <InseratImageCarousel
                                 images={images}
                             />
                         </div>
                         <div>
                             <div className="flex justify-end items-center bg-gray-100/100 am:mt-8 p-2 text-gray-900 rounded-md border-gray-800 border-4 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.2)]">
-                            <div className="flex mr-auto items-center">
-                                <div className="flex font-bold italic text-sm items-center"> <MapPin className="text-rose-600 "/> Mömer </div>
+                                <div className="flex mr-auto items-center">
+                                    <div className="flex font-bold italic text-sm items-center"> <MapPin className="text-rose-600 " /> Mömer </div>
                                 </div>
                                 <div className="justify-end flex mt-2 text-2xl font-bold">
                                     {inserat.price} <p className="text-sm mr-1">00 €</p>
                                 </div>
-                                
-                                
+
+
                             </div>
-                            
+
                         </div>
                         <div className="">
 
                         </div>
                         <div className="mt-2">
                             <div className="">
-                                <p className="flex text-lg sm:text-xl font-bold"><AlignLeft className="mr-2"/> Beschreibung der Anzeige</p>
+                                <p className="flex text-lg sm:text-xl font-bold"><AlignLeft className="mr-2" /> Beschreibung der Anzeige</p>
                             </div>
-                                <InseratDescription
-                                    inserat = {inserat}
-                                />
+                            <InseratDescription
+                                inserat={inserat}
+                            />
                         </div>
                     </div>
 
@@ -142,35 +153,40 @@ const InseratAnzeige = async ({
                 </div>
 
             </div>
-            
 
-            <div className=" p-4 xl:mt-24 sm:flex justify-center xl:block">
-            <div className="xl:hidden flex sm:block justify-center">
-                    <ProfileView
-                        user={user}
-                        inseratArray = {inseratArray}
-                        inseratOwner = {inseratOwner}
-                        averageRating={rezensionen.reduce((a, b) => a + b.rating, 0) / rezensionen.length}
-                    />
-                </div>
-            <div className="sm:ml-16 xl:ml-0 flex sm:block justify-center">
-                    <InseratOptions 
-                    user = {user}
-                    
-                    ownUser = {currentUser}
-                    
-                    contactOptions = {contactOptions}
-                    />
-                </div>
-                <div className="hidden xl:mt-16 xl:block">
-                    <ProfileView
-                        user={user}
-                        inseratArray = {inseratArray}
-                        inseratOwner = {inseratOwner}
-                        averageRating={rezensionen.reduce((a, b) => a + b.rating, 0) / rezensionen.length}
-                    />
-                </div>
 
+
+            <div>
+                
+                <div className=" p-2 xl:mt-24 sm:flex justify-center xl:block">
+
+                    <div className="xl:hidden flex sm:block justify-center">
+                        <ProfileView
+                            user={user}
+                            inseratArray={inseratArray}
+                            inseratOwner={inseratOwner}
+                            averageRating={rezensionen.reduce((a, b) => a + b.rating, 0) / rezensionen.length}
+                        />
+                    </div>
+                    <div className="sm:ml-16 xl:ml-0 flex sm:block justify-center">
+                        <InseratOptions
+                            user={user}
+
+                            ownUser={currentUser}
+
+                            contactOptions={contactOptions}
+                        />
+                    </div>
+                    <div className="hidden xl:mt-16 xl:block">
+                        <ProfileView
+                            user={user}
+                            inseratArray={inseratArray}
+                            inseratOwner={inseratOwner}
+                            averageRating={rezensionen.reduce((a, b) => a + b.rating, 0) / rezensionen.length}
+                        />
+                    </div>
+
+                </div>
             </div>
         </div>
     );

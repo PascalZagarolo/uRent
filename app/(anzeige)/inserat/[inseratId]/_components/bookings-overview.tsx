79c838060f6b84ead83c19e498cc1d9489979a56
Pascal <zagarolopascal@gmail.com>
2024-02-10 18:07:15 +0100

    bookings datum

--- app/(anzeige)/inserat/[inseratId]/_components/bookings-overview.tsx
+++ app/(anzeige)/inserat/[inseratId]/_components/bookings-overview.tsx
@@ -1,6 +1,6 @@
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Booking } from "@prisma/client";
-import { CalendarCheck, CalendarClock } from "lucide-react";
+import { CalendarCheck, CalendarClock, CheckCheckIcon, CheckIcon } from "lucide-react";
 
 interface BookingsOverviewProps{
     bookings : Booking[]
@@ -32,23 +32,32 @@ const BookingsOverview: React.FC<BookingsOverviewProps> = ({
             </PopoverTrigger>
             <PopoverContent side="right">
                 <div>
-                    <h1 className="flex font-semibold"> Termine </h1>
-                    <div className="">
+                    <h1 className="flex font-semibold mb-2"> Termine ({bookings.length}) </h1>
+                    <div className="h-[240px] overflow-y-auto no-scrollbar ">
+                        <div className="">
                         {bookings.length > 0 ? (
-                            bookings.map((bookings) => (
-                                <span className="flex mt-2 border border-gray-300 p-4 rounded-md drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.2)]"
-                                key={bookings.id}
-                                >
-                                    <CalendarClock/>
-                                    <p className="ml-2 font-semibold text-sm">{formatDateToDDMMYY(bookings.startDate)} - {formatDateToDDMMYY(bookings.endDate)}</p>
-                                </span>
+                            bookings.map((booking) => (
+                                [...Array(4)].map((_, index) => (
+                                    <span 
+                                        className="flex mt-2 border border-gray-300 p-4 rounded-md drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.2)]"
+                                        key={`${booking.id}_${index}`} // Making sure the key is unique
+                                    >
+                                        <CalendarClock />
+                                        <div className="ml-2 font-semibold text-sm drop-shadow-none flex">
+                                            {formatDateToDDMMYY(booking.startDate)} - {formatDateToDDMMYY(booking.endDate)} <CheckIcon className="ml-1 text-emerald-600" />
+                                        </div>
+                                    </span>
+                                ))
                             ))
+                            
+                            
                         ) : (
                             <span className="flex mt-2 border border-gray-300 p-4 rounded-md drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.2)] ">
                                     <CalendarClock/>
                                     <p className="ml-2 font-semibold text-gray-900/50 italic text-xs">Noch keine Reservierungen vorhanden</p>
                                 </span>
                         )}
+                        </div>
                     </div>
                 </div>
             </PopoverContent>

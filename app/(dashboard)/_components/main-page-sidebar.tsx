c0d1266869e443760f7f7799cbe51779110e847c
Pascal <zagarolopascal@gmail.com>
2024-02-12 23:37:04 +0100

    ...

--- app/(dashboard)/_components/main-page-sidebar.tsx
+++ app/(dashboard)/_components/main-page-sidebar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { AlignCenter, CarFront, CaravanIcon, ConstructionIcon, Contact, Glasses, SearchIcon, TractorIcon, Truck, X } from "lucide-react";
+import { AlignCenter, CarFront, CaravanIcon, ConstructionIcon, Contact, Glasses, SearchIcon, TractorIcon, TramFront, Truck, X } from "lucide-react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 
@@ -12,7 +12,7 @@ import { cn } from "@/lib/utils";
 
 interface MainPageSideBarProps {
     treffer: number;
- }
+}
 
 const MainPageSideBar: React.FC<MainPageSideBarProps> = ({
     treffer
@@ -57,17 +57,17 @@ const MainPageSideBar: React.FC<MainPageSideBarProps> = ({
     return (
         <div className="h-full fixed no-scrollbar w-[280px] hidden 2xl:block bg-[#2b2f48] border border-black sm:overflow-auto p-2   ">
             <h3 className="text-bold text-2xl p-2 rounded-md mt-4 flex justify-center text-gray-100 items-center drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] bg-[#1b1e2c]">
-                <AlignCenter className="mr-4" /> Suchfilter <X className="h-4 w-4 mb-2 ml-2 hover:cursor-pointer" onClick={onReset}/>
+                <AlignCenter className="mr-4" /> Suchfilter <X className="h-4 w-4 mb-2 ml-2 hover:cursor-pointer" onClick={onReset} />
             </h3>
             <div className="mt-4 ">
                 <h3 className="text-gray-100 font-semibold border-2 rounded-md border-black p-2 flex justify-center ml-2 mr-2 bg-[#1f2332]">
-                    Fahrzeugkategorie 
+                    Fahrzeugkategorie
                 </h3>
 
-                <div className="flex justify-between ml-12 mr-12 mt-8 ]">
+                <div className="flex justify-between ml-12 mr-12 mt-4 ">
                     <div className="">
-                        <p className={cn("p-4 rounded-md bg-white border-2 hover:cursor-pointer", 
-                        currentCategory === "PKW" ? "border-[#ed580dec]" : "border-[#212539]")} onClick={() => { onClick("PKW") }}>
+                        <p className={cn("p-4 rounded-md bg-white border-2 hover:cursor-pointer",
+                            currentCategory === "PKW" ? "border-[#ed580dec]" : "border-[#212539]")} onClick={() => { onClick("PKW") }}>
                             <CarFront />
                         </p>
                         <p className="flex justify-center text-gray-100 text-xs font-semibold mt-1">
@@ -76,8 +76,8 @@ const MainPageSideBar: React.FC<MainPageSideBarProps> = ({
                     </div>
 
                     <div>
-                        <p className={cn("p-4 rounded-md bg-white border-2 hover:cursor-pointer", 
-                        currentCategory === "LKW" ? "border-[#ed580dec]" : "border-[#212539]")} onClick={() => { onClick("LKW") }}>
+                        <p className={cn("p-4 rounded-md bg-white border-2 hover:cursor-pointer",
+                            currentCategory === "LKW" ? "border-[#ed580dec]" : "border-[#212539]")} onClick={() => { onClick("LKW") }}>
                             <Truck />
                         </p>
                         <p className="flex justify-center text-gray-100 text-xs font-semibold mt-1">
@@ -87,10 +87,10 @@ const MainPageSideBar: React.FC<MainPageSideBarProps> = ({
 
                 </div>
 
-                <div className="flex justify-between ml-12 mr-12 mt-8 ">
+                <div className="flex justify-between ml-12 mr-12 mt-4 ">
                     <div>
-                        <p className={cn("p-4 rounded-md bg-white border-2 hover:cursor-pointer", 
-                        currentCategory === "LAND" ? "border-[#ed580dec]" : "border-[#212539]")} onClick={() => { onClick("LAND") }}>
+                        <p className={cn("p-4 rounded-md bg-white border-2 hover:cursor-pointer",
+                            currentCategory === "LAND" ? "border-[#ed580dec]" : "border-[#212539]")} onClick={() => { onClick("LAND") }}>
                             <TractorIcon />
                         </p>
                         <p className="flex justify-center text-gray-100 text-xs font-semibold mt-1">
@@ -99,8 +99,8 @@ const MainPageSideBar: React.FC<MainPageSideBarProps> = ({
                     </div>
 
                     <div>
-                        <p className={cn("p-4 rounded-md bg-white border-2 hover:cursor-pointer", 
-                        currentCategory === "BAU" ? "border-[#ed580dec]" : "border-[#212539]")} onClick={() => { onClick("BAU") }}>
+                        <p className={cn("p-4 rounded-md bg-white border-2 hover:cursor-pointer",
+                            currentCategory === "BAU" ? "border-[#ed580dec]" : "border-[#212539]")} onClick={() => { onClick("BAU") }}>
                             <ConstructionIcon />
                         </p>
                         <p className="flex justify-center text-gray-100 text-xs font-semibold mt-1">
@@ -111,29 +111,43 @@ const MainPageSideBar: React.FC<MainPageSideBarProps> = ({
 
                 </div>
 
-                <div className="flex justify-between ml-12 mr-12 mt-8 ">
+                <div className="flex justify-between ml-12 mr-12 mt-4 ">
 
 
-                    <div>
-                        <p className={cn("p-4 rounded-md bg-white border-2  w-[185px] flex justify-center hover:cursor-pointer",
-                        currentCategory === "CARAVAN" ? "border-[#ed580dec]" : "border-[#212539]")} onClick={() => { onClick("CARAVAN") }}>
-                            <CaravanIcon className="" />
+                    <div className="w-[40px] ml-2.5">
+                        <div className="flex flex-col items-center">
+                            <p className={cn("p-4 rounded-md bg-white border-2 flex justify-center items-center hover:cursor-pointer",
+                                currentCategory === "CARAVAN" ? "border-[#ed580dec]" : "border-[#212539]")} onClick={() => { onClick("CARAVAN") }}>
+                                <CaravanIcon className="" />
+                            </p>
+                            <p className="text-gray-100 text-xs font-semibold mt-1  text-center">
+                                Wohnmobile <br /> & Anhänger
+                            </p>
+                        </div>
+                    </div>
+
+
+
+                    <div className="">
+                        <p className={cn("p-4 rounded-md bg-white border-2   flex justify-center hover:cursor-pointer",
+                            currentCategory === "TRANSPORT" ? "border-[#ed580dec]" : "border-[#212539]")} onClick={() => { onClick("TRANSPORT") }}>
+                            <TramFront className="" />
                         </p>
-                        <p className="flex justify-center text-gray-100 text-xs font-semibold mt-1">
-                            Wohnmobile & Anhänger
+                        <p className="flex justify-center text-gray-100 text-xs font-semibold mt-1 ">
+                            Transport
                         </p>
                     </div>
 
                 </div>
                 <div>
-                    <PKW/>
+                    <PKW />
                 </div>
                 <div className="text-xs flex justify-center mt-2 text-gray-100 underline">
-                  <Contact className="mr-2 h-4 w-4"/>  kein passendes Angebot dabei ?
+                    <Contact className="mr-2 h-4 w-4" />  kein passendes Angebot dabei ?
                 </div>
                 <div className="flex justify-center mt-2 rounded-md">
                     <Button className="bg-[#ed580dec] w-full h-[100px] m ml-2 mr-2 border-2 border-[#000000] flex justify-center drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
-                        <SearchIcon className="h-5 w-5 mr-2"/> <p className="font-bold mr-1 ">{treffer}</p> Ergebnisse
+                        <SearchIcon className="h-5 w-5 mr-2" /> <p className="font-bold mr-1 ">{treffer}</p> Ergebnisse
                     </Button>
                 </div>
             </div>

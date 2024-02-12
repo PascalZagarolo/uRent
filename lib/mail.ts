13f0c8f35996ab58b9886d3027d3aadeb09ac815
Pascal <zagarolopascal@gmail.com>
2024-02-12 20:21:55 +0100

    ...

--- lib/mail.ts
+++ lib/mail.ts
@@ -9,7 +9,7 @@ export const sendVerificationEmail = async (
     email: string,
     token : string
 ) => {
-    const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+    const confirmLink = `${process.env.NEXT_PUBLIC_BASE_URL}/auth/new-verification?token=${token}`;
 
     console.log("sendVerificationEmail" + email + token)
 

2846bd4557ef79d76c6eb9638d18e23243a99f94
Pascal <zagarolopascal@gmail.com>
2024-02-13 14:58:08 +0100

    fixed register button + removed GitHub sign in Option

--- app/(dashboard)/_components/login-bar-header.tsx
+++ app/(dashboard)/_components/login-bar-header.tsx
@@ -15,7 +15,7 @@ const LoginBarHeader = () => {
             </Button>
             <p className="text-gray-200 font-bold hidden 2xl:flex"> oder </p>
             <Button variant="ghost"  className=" 2xl:flex hidden outline outline-offset-1 outline-1 ml-4 bg-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] border-2 border-gray-300">
-                <Link href="/register">
+                <Link href="/login">
                     Registrieren
                 </Link>
             </Button>

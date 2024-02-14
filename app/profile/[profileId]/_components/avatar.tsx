d003c28669e8e64957e31c8469800ae881de7dbc
Pascal <zagarolopascal@gmail.com>
2024-02-14 19:20:07 +0100

    ...

--- app/profile/[profileId]/_components/avatar.tsx
+++ app/profile/[profileId]/_components/avatar.tsx
@@ -9,7 +9,7 @@ const Avatar: React.FC<AvatarProps> = ({
     imageUrl
 }) => {
     return ( 
-        <div className="h-[120px] w-[120px] overflow-hidden rounded-full ">
+        <div className="h-[120px] w-[120px]  rounded-full ">
             <img 
         src={imageUrl || "/placeholder-person.jpg"} 
         className="rounded-full  object-cover border-2 border-gray-700 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]"

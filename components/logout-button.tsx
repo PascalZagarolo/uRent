72f745d8bc5c68677c8c93c8c424f3c67b6601c5
Pascal <zagarolopascal@gmail.com>
2024-02-16 19:48:04 +0100

    flex-shrink location search

--- components/logout-button.tsx
+++ components/logout-button.tsx
@@ -7,11 +7,11 @@ import { ArrowRight, LogOut } from "lucide-react";
 const LogOutButton = () => {
     return ( 
         <>
-            <Button className="hidden bg-[#e1dfdf] drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] lg:block border-2 border-gray-900" variant="ghost" onClick={() => signOut({ callbackUrl : `${window.location.origin}/login`})}>
+            <Button className="hidden bg-[#e1dfdf] drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] 2xl:block border-2 border-gray-900" variant="ghost" onClick={() => signOut({ callbackUrl : `${window.location.origin}/login`})}>
             <p className="hidden lg:block">Abmelden</p>
             
         </Button>
-        <p className="lg:hidden mr-2 text-white hover:cursor-pointer"> <LogOut/> </p>
+        <p className="2xl:hidden mr-2 text-white hover:cursor-pointer"> <LogOut/> </p>
         </>
         
 

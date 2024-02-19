32c7f65772892e40a438b246e644e2815f6bc28f
Pascal <zagarolopascal@gmail.com>
2024-02-19 18:49:48 +0100

    order by darkmode..

--- app/(dashboard)/_components/_smart-filter/order-by.tsx
+++ app/(dashboard)/_components/_smart-filter/order-by.tsx
@@ -33,10 +33,10 @@ const OrderBy = () => {
 
     return (
         <Select onValueChange={(e) => {onClick(e)}} >
-            <SelectTrigger className="w-[180px] dark:text-gray-100 dark:focus:ring-0 dark:bg-slate-900">
+            <SelectTrigger className="w-[180px] dark:text-gray-100 dark:focus:ring-0 dark:bg-[#0F0F0F] dark:border-gray-900">
                 <SelectValue placeholder="Sortieren nach" className="dark:text-gray-100" />
             </SelectTrigger>
-            <SelectContent>
+            <SelectContent className="dark:bg-[#0F0F0F] dark:border-gray-900">
                 <SelectItem value="desc">Preis absteigend</SelectItem>
                 <SelectItem value="asc">Preis aufsteigend</SelectItem>
                 <SelectItem value="relevance">Relevanz</SelectItem>

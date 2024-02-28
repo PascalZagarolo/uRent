fa1965b52ef4f63d98f825b39a6c18b9dcd5fc67
Pascal <zagarolopascal@gmail.com>
2024-02-28 21:40:12 +0100

    missing key prop + hover effect

--- app/dashboard/[userId]/page.tsx
+++ app/dashboard/[userId]/page.tsx
@@ -116,6 +116,7 @@ const DashboardPage = async ({
                                         <BookingRequestRender
                                         //@ts-ignore
                                         request={request}
+                                        key={request.id}
                                         />
                                     
                                     ))}

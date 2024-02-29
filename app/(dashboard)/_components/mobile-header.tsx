ab7dcc07a04bdece5c5d0f430a0a63e33da1a3a0
Pascal <zagarolopascal@gmail.com>
2024-02-29 18:45:11 +0100

    mobile header fix

--- app/(dashboard)/_components/mobile-header.tsx
+++ app/(dashboard)/_components/mobile-header.tsx
@@ -1,16 +1,50 @@
+'use client'
+
+
 import SearchItem from "./search-item";
 import MobileLogoDialog from "./mobile-logo-dialog";
+import getCurrentUser from "@/actions/getCurrentUser";
+import { useSession } from "next-auth/react";
+import LoggedInBarHeader from "./logged-in-header";
+import { db } from "@/utils/db";
+import { User, Notification } from "@prisma/client";
 
-const MobileHeader = () => {
+interface MobileHeaderProps {
+    currentUser : User;
+    notifications : Notification[];
+}
+
+const MobileHeader: React.FC<MobileHeaderProps> = ({
+    currentUser,
+    notifications
+}) => {
 
     
+    const { data: session, status } = useSession();
 
+    
     return ( 
-        <div className="bg-[#1f2332] h-[100px] border-2 border-black p-2">
-            <div className="flex items-center">
-                <MobileLogoDialog/>
-                <div className="flex justify-center ml-4 p-2">
+        <div className="bg-[#1f2332] h-[100px]  flex items-center">
+            <div className="flex items-center ml-2">
+            <MobileLogoDialog/>
+                <div className="flex justify-center ml-4">
                     <SearchItem/>
+                </div>
+                <div>
+                {
+                        status === 'unauthenticated' || !currentUser ? (
+                            <>
+                            </>
+                        ) : (
+                            <div className="flex ml-auto ">
+                                <LoggedInBarHeader
+                                    currentUser={currentUser}
+                                    notifications = {notifications}
+                                />
+                                
+                            </div>
+                        )
+                    }
                 </div>
             </div>
         </div>

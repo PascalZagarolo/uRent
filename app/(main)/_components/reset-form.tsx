c0fc1defb829cef81457c118ba9ae7fd3d145bea
Pascal <zagarolopascal@gmail.com>
2024-03-03 18:48:23 +0100

    reset password ui

--- app/(main)/_components/reset-form.tsx
+++ app/(main)/_components/reset-form.tsx
@@ -21,7 +21,7 @@ import Link from "next/link";
 
 
 
-const CredForm = ({
+const ResetForm = ({
 
 }) => {
 
@@ -145,18 +145,7 @@ const CredForm = ({
           className="space-y-4"
           onSubmit={handleSubmit(onSubmit)}
         >
-          {variant === 'REGISTER' && (
-            <Input
-              disabled={isLoading}
-              register={register}
-              errors={errors}
-              required
-              id="name"
-              label="Nutzername"
-            
-              
-            />
-          )}
+          
           <Input
             disabled={isLoading}
             register={register}
@@ -167,87 +156,20 @@ const CredForm = ({
             type="email"
           />
           
-          <Input
-            disabled={isLoading}
-            register={register}
-            errors={errors}
-            required
-            id="password"
-            label="Passwort"
-            type="password"
-            
-          /> 
-          
-            
-              <Link href={"/forget"} className="underline text-xs">
-                Passwort vergessen?
-              </Link>
-            
-          
-          
           <div>
             <Button disabled={isLoading} type="submit" className="bg-[#1f2332] hover:bg-[#25293a] drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]
              dark:bg-[#0d0d0d] dark:hover:bg-[#171717] dark:text-gray-100">
-              {variant === 'LOGIN' ? 'Einloggen' : 'Registrieren'}
+              Email anfordern
             </Button>
           </div>
         </form>
 
-        <div className="mt-6">
-          <div className="relative">
-            <div
-              className="
-                absolute 
-                inset-0 
-                flex 
-                items-center
-              "
-            >
-              <div className="w-full border-t border-gray-300" />
-            </div>
-            <div className="relative flex justify-center text-sm">
-              <span className="bg-white px-2 text-gray-500  dark:bg-[#161616] dark:text-gray-100/70">
-                sonstige Anmeldemöglichkeiten
-              </span>
-            </div>
-          </div>
-
-          <div className="flex mt-4  justify-center ">
-            <div className="w-full gap-y-4">
-            
-            <Button variant="ghost" className="text-medium w-full  bg-gray-100  drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]
-             dark:bg-[#0d0d0d] dark:hover:bg-[#171717]" 
-            onClick={() => {onClick("google")}}>
-              Google
-            </Button>
-            </div>
-          </div>
-        </div>
-        <div
-          className="
-            flex 
-            gap-2 
-            justify-center 
-            text-sm 
-            mt-6 
-            px-2 
-            text-gray-500
-          "
-        >
-          <div>
-            {variant === 'LOGIN' ? 'Neu auf uRent?' : 'Ich besitze schon einen Account'}
-          </div>
-          <div
-            onClick={toggleVariant}
-            className="underline cursor-pointer"
-          >
-            {variant === 'LOGIN' ? 'Erstelle einen Account' : 'Einloggen'}
-          </div>
-        </div>
+        
+        
       </div>
       
     </div>
   );
 }
 
-export default CredForm;
+export default ResetForm;

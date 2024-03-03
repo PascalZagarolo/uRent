d37ce19bc12d784ff6990d2a44ca3ab4dceb4428
Pascal <zagarolopascal@gmail.com>
2024-03-03 18:48:23 +0100

    reset password ui

---
+++ app/(main)/forget/page.tsx
@@ -0,0 +1,14 @@
+import ResetForm from "../_components/reset-form";
+
+const ForgetPage = () => {
+    return ( 
+        <div>
+            <h3 className="flex justify-center font-semibold text-xl">
+                Mein Passwort zurücksetzen
+            </h3>
+            <ResetForm/>
+        </div>
+     );
+}
+ 
+export default ForgetPage;

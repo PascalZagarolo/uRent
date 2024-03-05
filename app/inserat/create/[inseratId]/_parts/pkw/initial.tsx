692d18fd6c396247c6f970e8365dfd2180e09246
Pascal <zagarolopascal@gmail.com>
2024-03-05 18:11:07 +0100

    baujahr copy and delete all function

--- app/inserat/create/[inseratId]/_parts/pkw/initial.tsx
+++ app/inserat/create/[inseratId]/_parts/pkw/initial.tsx
@@ -30,12 +30,25 @@ const InitialForm: React.FC<InitialFormProps> = ({
 
     
 
-    const [currentInitial, setcurrentInitial] = useState<Date | null>(inital || new Date);
+    const [currentInitial, setcurrentInitial] = useState<Date | null>(null);
     const [inputValue, setInputValue] = useState('');
     const [validValue, setValidValue] = useState(false);
     const [isLoading, setIsLoading] = useState(false)
     
+    function formatDateToMMYYYY(date) {
+        
+        const month = date?.getMonth() + 1; 
+        const year = date?.getFullYear();
+    
+        
+        const dateString = `${month.toString().padStart(2, '0')}/${year}`;
 
+        if(!date) {
+            return "";
+        }
+    
+        return dateString;
+    }
 
 
 
@@ -59,23 +72,30 @@ const InitialForm: React.FC<InitialFormProps> = ({
 
     }
 
-
+    //! fix cant copy values in handleInputChange
     const handleInputChange = (event) => {
         const { value } = event.target;
-        
+    
         setInputValue(value);
-        
-        const regex = /^(0[1-9]|1[0-2])\/(19\d{2}|[2-9]\d{3})$/; 
+    
+        if (value.trim() === '') { 
+            setValidValue(false);
+            setcurrentInitial(null);
+            return;
+        }
+    
+        const regex = /^(0[1-9]|1[0-2])\/(19\d{2}|[2-9]\d{3})$/;
         const isValidDate = regex.test(value);
-        const isYearValid = parseInt(value.split('/')[1]) >= 1900; 
+        const isYearValid = parseInt(value.split('/')[1]) >= 1900;
         setValidValue(!isValidDate || !isYearValid);
-
-        if(validValue) {
+    
+        if (isValidDate && isYearValid) {
             const [month, year] = value.split('/').map(Number);
             const newInitial = new Date(year, month - 1, 1);
-            setcurrentInitial(new Date(newInitial));
+            setcurrentInitial(newInitial);
         }
     };
+    
 
 
     return (
@@ -88,13 +108,13 @@ const InitialForm: React.FC<InitialFormProps> = ({
                 <Input
                     placeholder="MM/YYYY"
                     className="p-2.5 2xl:pr-16 xl:pr-4 rounded-md input:text-sm border mt-2 border-black dark:bg-[#151515] input:justify-start dark:focus-visible:ring-0"
-                    value={inputValue || format(new Date(currentInitial), "MM/yyyy")}
+                    value={inputValue ||  formatDateToMMYYYY(currentInitial) }
                     onChange={handleInputChange}
                 />
             </div>
         </div>
         <Button onClick={onSubmit} className="mt-8 dark:bg-[#000000] dark:hover:bg-[#0c0c0c] dark:text-gray-100" 
-        disabled={validValue || currentInitial.toDateString() === inital.toDateString()}>
+        disabled={validValue || currentInitial?.toDateString() === inital.toDateString() || !currentInitial}>
             <span className="">Baujahr angeben</span>
         </Button>
         

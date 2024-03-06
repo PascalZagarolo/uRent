d58141fb9d7aca5e9a3bdf092f566c8c8f69188a
Pascal <zagarolopascal@gmail.com>
2024-03-06 18:43:25 +0100

    free Miles + Extra Cost

--- app/inserat/create/[inseratId]/_parts/pkw/extracost.tsx
+++ app/inserat/create/[inseratId]/_parts/pkw/extracost.tsx
@@ -3,31 +3,31 @@ import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Inserat } from "@prisma/client";
+import { Inserat, PkwAttribute } from "@prisma/client";
 import axios from "axios";
 import { set } from "date-fns";
-import { Banknote, EuroIcon } from "lucide-react";
+import { Banknote, CarTaxiFrontIcon, EuroIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 import { z } from "zod";
 
-interface SelectPriceProps {
-    inserat: Inserat;
+interface ExtraCostProps {
+    inserat: Inserat & { pkwAttribute : PkwAttribute };
 }
 
-const SelectPrice: React.FC<SelectPriceProps> = ({
+const ExtraCost: React.FC<ExtraCostProps> = ({
     inserat
 }) => {
 
     const router = useRouter();
 
     const [isLoading, setIsLoading] = useState(false);
-    const [currentValue, setCurrentValue] = useState(inserat.price || 0);
+    const [currentValue, setCurrentValue] = useState(inserat.pkwAttribute?.freeMiles || 0);
 
     const formSchema = z.object({
-        price: z.preprocess(
+        extraCost: z.preprocess(
             (args) => (args === '' ? undefined : args),
             z.coerce
                 .number({ invalid_type_error: 'Preis muss eine Nummer sein' })
@@ -39,14 +39,14 @@ const SelectPrice: React.FC<SelectPriceProps> = ({
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
-            price: inserat.price || 0
+            extraCost: inserat.pkwAttribute?.freeMiles || 0
         }
     })
 
     const onSubmit = (values: z.infer<typeof formSchema>) => {
         try {
             setIsLoading(true);
-            axios.patch(`/api/inserat/${inserat.id}`, values);
+            axios.patch(`/api/inserat/${inserat.id}/pkw`, values);
             toast.success("Preis erfolgreich gespeichert");
             setTimeout(() => {
                 router.refresh();
@@ -65,16 +65,16 @@ const SelectPrice: React.FC<SelectPriceProps> = ({
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)}>
                     <FormLabel className="flex justify-start items-center">
-                        <Banknote className="w-4 h-4" /><p className="ml-2 font-semibold"> Mietpreis </p>
+                        <CarTaxiFrontIcon className="w-4 h-4" /><p className="ml-2 font-semibold"> Zusatskosten/KM </p>
                     </FormLabel>
-                    <p className="font-semibold text-gray-800/50 text-xs dark:text-gray-100/80"> Alle angaben in EUR </p>
+                    <p className="font-semibold text-gray-800/50 text-xs dark:text-gray-100/80"> Wie viel mehr pro KM?</p>
                     <FormField
                         control={form.control}
-                        name="price"
+                        name="extraCost"
                         render={({ field }) => (
                             <FormField
                                 control={form.control}
-                                name="price"
+                                name="extraCost"
                                 render={({ field }) => (
                                     <FormItem className="mt-2 ">
                                         <FormControl>
@@ -90,10 +90,10 @@ const SelectPrice: React.FC<SelectPriceProps> = ({
                                                     const cleanedValue = rawValue.replace(/[^0-9.]/g, '');
 
 
-                                                    let formattedValue = parseFloat(cleanedValue).toFixed(2);
+                                                    let formattedValue = Number(cleanedValue).toFixed(2);
 
                                                     if(isNaN(Number(formattedValue))){
-                                                        formattedValue = String(0);
+                                                        formattedValue = String(2);
                                                     }
                                                     e.currentTarget.value = formattedValue;
 
@@ -116,9 +116,9 @@ const SelectPrice: React.FC<SelectPriceProps> = ({
                         <Button
                             className="bg-white hover:bg-gray-200 text-gray-900 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]  mt-2
                              dark:bg-black dark:text-gray-100 dark:hover:bg-gray-900"
-                            type="submit" disabled={!isValid || isSubmitting || currentValue == inserat.price}
+                            type="submit" disabled={!isValid || isSubmitting || currentValue == inserat.pkwAttribute?.extraCost}
                         >
-                            Preis festlegen
+                            Zusatskosten angeben
                         </Button>
                         
                     </div>
@@ -128,4 +128,4 @@ const SelectPrice: React.FC<SelectPriceProps> = ({
     );
 }
 
-export default SelectPrice;
+export default ExtraCost;

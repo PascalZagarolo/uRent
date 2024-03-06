2a4db7f30df891d0d5ad045e5986e945c262c2b6
Pascal <zagarolopascal@gmail.com>
2024-03-06 14:09:14 +0100

    select category details page

--- app/search/_components/category-search.tsx
+++ app/search/_components/category-search.tsx
@@ -1,11 +1,25 @@
+'use client'
+
 import { Button } from "@/components/ui/button";
 import { SelectItem } from "@/components/ui/select";
 import { Separator } from "@/components/ui/separator";
+import { cn } from "@/lib/utils";
+import { useSavedSearchParams } from "@/store";
 import { Category } from "@prisma/client";
 import { CarFrontIcon, CaravanIcon, Construction, TractorIcon, TramFrontIcon, TruckIcon } from "lucide-react";
 
 
 const CategorySearch = () => {
+
+    const { searchParams, changeSearchParams } = useSavedSearchParams();
+
+    const currentObject = useSavedSearchParams((state) => state.searchParams)
+
+    const setCategory = (category) => {
+        changeSearchParams("category", category);
+        console.log('category' in searchParams && searchParams['category'] === "PKW")
+    }
+
     return (
         <div className="w-full">
             <h3 className="font-semibold text-md flex items-center">
@@ -17,7 +31,13 @@ const CategorySearch = () => {
             </h3>
             <div className="w-full flex justify-between mt-4">
             {Object.values(Category).map((category, index) => (
-                <Button key={index} value={category} className="dark:bg-[#141414] dark:hover:bg-[#1d1d1d] dark:text-gray-100 py-6">
+                <Button key={index} value={category} 
+                className={cn(
+                    "dark:bg-[#141414] dark:hover:bg-[#1d1d1d] dark:text-gray-100 py-6 ", 
+                    currentObject["category"] === category && "border-2 border-blue-800"
+                )}
+                onClick={() => setCategory(category)}
+                >
                     {
                         {
                             'PKW': <CarFrontIcon className="w-6 h-6" />,

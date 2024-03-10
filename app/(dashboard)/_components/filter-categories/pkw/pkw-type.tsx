040e840b2949fa3b906b396e1b6f13930f550687
Pascal <zagarolopascal@gmail.com>
2024-03-10 19:48:55 +0100

    type on pkw attributes mainpage bar

--- app/(dashboard)/_components/filter-categories/pkw/pkw-type.tsx
+++ app/(dashboard)/_components/filter-categories/pkw/pkw-type.tsx
@@ -3,47 +3,38 @@
 import { getSearchParamsFunction } from "@/actions/getSearchParams";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { useSavedSearchParams } from "@/store";
-import { Inserat, LkwBrand } from "@prisma/client";
-
-
-
-
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-
 import { useState } from "react";
 import qs from "query-string";
 
 
 
-const PkwSeatsBar = () => {
-    const params = getSearchParamsFunction("seats");
-    const seats = useSearchParams().get("seats");
 
-    const [currentSeats, setCurrentSeats] = useState(seats);
+const PkwTypeBar = () => {
+    const type = useSearchParams().get("type");
+    const [currentType, setCurrentType] = useState(type);
     const [isLoading, setIsLoading] = useState(false);
 
     
-
-    const router = useRouter();
     const pathname = usePathname();
 
-    
+    const router = useRouter();
+
+    const params = getSearchParamsFunction("type");
 
     const onSubmit = (selectedValue: string) => {
-        setCurrentSeats(selectedValue)
+        setCurrentType(selectedValue)
         const url = qs.stringifyUrl({
             url : pathname,
             query : {
-                seats : selectedValue,
+                type : selectedValue,
                 ...params
             }
         }, { skipEmptyString: true, skipNull: true })
 
         router.push(url) 
+        
     }
-
-   
 
     function removeUnderscore(inputString: string): string {
         const outputString = inputString.replace(/_/g, ' ');
@@ -54,39 +45,41 @@ const PkwSeatsBar = () => {
         <div className="w-full">
             <div className="w-full">
                 <Label className="flex justify-start items-center ">
-                    <p className="ml-2 font-semibold"> Sitze </p> 
+                    <p className="ml-2 font-semibold"> Fahrzeugtyp </p>
                 </Label>
 
                 <Select
                     onValueChange={(brand) => {
                         onSubmit(brand)
                     }}
-                    value={currentSeats}
+                    value={currentType}
                     disabled={isLoading}
                 >
 
-                    <SelectTrigger className="dark:bg-[#151515] dark:border-gray-200 dark:border-none   focus-visible:ring-0 mt-2 rounded-md "
-                        disabled={isLoading}  
-                        value={currentSeats}
-                        >
+                    <SelectTrigger className="dark:bg-[#151515] dark:border-gray-200 dark:border-none focus-visible:ring-0 mt-2 rounded-md "
+                        disabled={isLoading}  >
                         <SelectValue
-                            placeholder="Wie viele Sitze?"
-                        className="flex justify-center"
-                        defaultValue={currentSeats}
+                            placeholder="Welcher Autotyp?"
+
+
                         />
                     </SelectTrigger>
 
-                    <SelectContent className="dark:bg-[#000000] border-white dark:border-none w-full flex justify-center" >
+                    <SelectContent className="dark:bg-[#000000] border-white dark:border-none w-full">
                         <SelectItem key="beliebig" value={null} className="font-semibold">
                             Beliebig
                         </SelectItem>
-                        <SelectItem value="1">1</SelectItem>
-                        <SelectItem value="2">2</SelectItem>
-                        <SelectItem value="3">3</SelectItem>
-                        <SelectItem value="4">4</SelectItem>
-                        <SelectItem value="5">5</SelectItem>
-                        <SelectItem value="6">6</SelectItem>
-                        <SelectItem value="7">7</SelectItem>
+
+
+                        <SelectItem value="KOMBI">Kombi</SelectItem>
+                        <SelectItem value="COUPE">Coupe</SelectItem>
+                        <SelectItem value="SUV">SUV</SelectItem>
+                        <SelectItem value="VAN">Van</SelectItem>
+                        <SelectItem value="KLEINBUS">Kleinbus</SelectItem>
+                        <SelectItem value="CABRIO">Cabrio</SelectItem>
+                        <SelectItem value="KLEIN">Kleinwagen</SelectItem>
+                        <SelectItem value="SPORT">Sportwagen</SelectItem>
+                        <SelectItem value="SUPERSPORT">Supersportwagen</SelectItem>
                     </SelectContent>
                 </Select>
             </div>
@@ -94,4 +87,4 @@ const PkwSeatsBar = () => {
     );
 }
 
-export default PkwSeatsBar;
+export default PkwTypeBar;

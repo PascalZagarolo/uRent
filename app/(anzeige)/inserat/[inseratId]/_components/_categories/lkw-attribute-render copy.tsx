1c9ff101d8a03e4af9b461498f2c1da760c44359
Pascal <zagarolopascal@gmail.com>
2024-03-12 13:18:25 +0100

    whitemode fix inserat page

--- app/(anzeige)/inserat/[inseratId]/_components/_categories/lkw-attribute-render copy.tsx
+++ app/(anzeige)/inserat/[inseratId]/_components/_categories/lkw-attribute-render copy.tsx
@@ -19,31 +19,31 @@ const LkwAttributeRender: React.FC<LkwAttributeRenderProps> = ({
     let shownItems = 0;
 
     return ( 
-        <div className="w-full grid grid-cols-2 gap-4 mt-4">
+        <div className="w-full grid grid-cols-2 gap-4 mt-4 text-gray-200">
             {attributes?.lkwBrand && (
                 
-                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md">
+                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md text-gray-200">
                     <FaTruckMoving className="w-4 h-4 mr-2" />    {attributes.lkwBrand}
                 </div>
             )}
             {attributes?.application && (
-                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md">
+                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md text-gray-200">
                     <LiaTruckLoadingSolid className="w-4 h-4 mr-2" />    {attributes.application.substring(0,1)}{attributes.application.substring(1).toLowerCase()}
                 </div>
             )}
             {attributes?.loading && (
                 
-                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md">
+                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md text-gray-200">
                     <PiEngine className="w-4 h-4 mr-2" />    {attributes.loading.substring(0,1)}{attributes.loading.substring(1).toLowerCase()}   
                 </div>
             )}
             {attributes?.drive && (
-                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md">
+                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md text-gray-200">
                     <GearIcon className="w-4 h-4 mr-2" />    {attributes.drive.substring(1)}
                 </div>
             )}
             {attributes?.weightClass && (
-                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md">
+                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md text-gray-200">
                     <WeightIcon className="w-4 h-4 mr-2" />   Gewichtsklasse :
                     { 
                         {
@@ -60,7 +60,7 @@ const LkwAttributeRender: React.FC<LkwAttributeRenderProps> = ({
             )}
 
             {attributes?.seats && (
-                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md">
+                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md text-gray-200">
                     <PiCouchFill className="w-4 h-4 mr-2" />    {attributes.seats} {attributes.seats > 1 ? 'Sitze' : 'Sitz'}
                 </div>
             )}

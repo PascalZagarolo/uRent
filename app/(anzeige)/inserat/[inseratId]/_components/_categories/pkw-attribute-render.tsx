51942f2b65f7e75846bfbd92cc79912899fbb548
Pascal <zagarolopascal@gmail.com>
2024-03-12 13:18:25 +0100

    whitemode fix inserat page

--- app/(anzeige)/inserat/[inseratId]/_components/_categories/pkw-attribute-render.tsx
+++ app/(anzeige)/inserat/[inseratId]/_components/_categories/pkw-attribute-render.tsx
@@ -21,38 +21,38 @@ const PkwAttributeRender: React.FC<PkwAttributeRenderProps> = ({
         <div className="w-full grid grid-cols-2 gap-4 mt-4">
             {attributes?.brand && (
                 
-                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md">
+                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md text-gray-200">
                     <CarFrontIcon className="w-4 h-4 mr-2" />    {attributes.brand}
                 </div>
             )}
             {attributes?.power && (
                 
-                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md">
+                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md text-gray-200">
                     <PiEngine className="w-4 h-4 mr-2" />    {attributes.power} PS 
                 </div>
             )}
             {attributes?.freeMiles && (
-                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md">
+                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md text-gray-200">
                     <Globe2 className="w-4 h-4 mr-2" />    {attributes.freeMiles} Freikilometer
                 </div>
             )}
             {attributes?.seats && (
-                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md">
+                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md text-gray-200">
                     <PiCouchFill className="w-4 h-4 mr-2" />    {attributes.seats} Sitze
                 </div>
             )}
             {attributes?.fuel && (
-                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md">
+                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md text-gray-200">
                     <RiGasStationLine className="w-4 h-4 mr-2" />    {attributes.fuel}
                 </div>
             )}
             {attributes?.doors && (
-                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md">
+                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md text-gray-200">
                     <DoorClosedIcon className="w-4 h-4 mr-2" />    {attributes.doors}/{Number(attributes.doors) + 1} Türer
                 </div>
             )}
             {attributes?.transmission && (
-                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md">
+                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md text-gray-200">
                     <GearIcon className="w-4 h-4 mr-2" />    
                     
                     { 
@@ -64,7 +64,7 @@ const PkwAttributeRender: React.FC<PkwAttributeRenderProps> = ({
                 </div>
             )}
             {attributes?.initial && (
-                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md">
+                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md text-gray-200">
                     <ConstructionIcon className="w-4 h-4 mr-2" />  Baujahr : {format(new Date(attributes?.initial), "MM/yyyy")} 
                     
                     
--- app/(anzeige)/inserat/[inseratId]/_components/inserat-attributes.tsx
+++ app/(anzeige)/inserat/[inseratId]/_components/inserat-attributes.tsx
@@ -11,7 +11,7 @@ const InseratAttributes: React.FC<InseratAttributesProps> = ({
     inserat
 }) => {
     return (
-        <div className="bg-[#161923]  rounded-md w-full p-4 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+        <div className="bg-[#161923]  rounded-md w-full p-4 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] text-gray-200">
             <h3 className="font-semibold flex items-center">
                 {
                     {

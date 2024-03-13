ef95f8127ec62324bbbf3cb115fa119c3df8c059
Pascal <zagarolopascal@gmail.com>
2024-03-13 15:43:28 +0100

    trailer attributes on pkw

---
+++ app/(anzeige)/inserat/[inseratId]/_components/_categories/trailer-attribute-render.tsx
@@ -0,0 +1,93 @@
+import { TrailerAttribute } from "@prisma/client";
+import { GearIcon } from "@radix-ui/react-icons";
+
+import { CaravanIcon, WeightIcon } from "lucide-react";
+import { LuAxis3D } from "react-icons/lu";
+import { PiCouchFill } from "react-icons/pi";
+import { FaGears } from "react-icons/fa6";
+import { TbCarCrane, TbCrane } from "react-icons/tb";
+import { GiStoneWheel } from "react-icons/gi";
+import { RiCaravanFill } from "react-icons/ri";
+
+interface TrailerAttributeRenderProps {
+    attributes: TrailerAttribute
+
+}
+
+
+const TrailerAttributeRender: React.FC<TrailerAttributeRenderProps> = ({
+    attributes
+}) => {
+
+    let shownItems = 0;
+
+    return ( 
+        <div className="w-full grid grid-cols-2 gap-4 mt-4 text-gray-200">
+            {attributes?.type && (
+                
+                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md text-gray-200">
+                    <CaravanIcon className="w-4 h-4 mr-2" />    {attributes.type.substring(0,1)}{attributes.type.substring(1).toLowerCase()}
+                </div>
+            )}
+            {attributes?.extraType && (
+                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md text-gray-200">
+                    <RiCaravanFill  className="w-4 h-4 mr-2" />    {attributes.extraType?.substring(0,1)}{attributes.extraType?.substring(1).toLowerCase()}
+                </div>
+            )}
+            {attributes?.coupling && (
+                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md text-gray-200">
+                    <FaGears  className="w-4 h-4 mr-2" />    {attributes.coupling?.substring(0,1)}{attributes.coupling?.substring(1).toLowerCase()}
+                </div>
+            )}
+            {attributes?.loading && (
+                
+                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md text-gray-200">
+                    <TbCrane   className="w-4 h-4 mr-2" />    {attributes.loading.substring(0,1)}{attributes.loading.substring(1).toLowerCase()}   
+                </div>
+            )}
+            {attributes?.axis && (
+                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md text-gray-200">
+                    <LuAxis3D  className="w-4 h-4 mr-2" />    { 
+                        {
+                            '1' : "Einachser",
+                            '2' : "Zweiachser",
+                            '3' : "Dreiachser",
+                            '4' : "Vierachser",
+                            '5' : " > 4 Achsen"
+                        }[attributes.axis]
+                    }
+                </div>
+            )}
+            {attributes?.weightClass && (
+                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md text-gray-200">
+                    <WeightIcon className="w-4 h-4 mr-2" />  
+                    { 
+                        {
+                            '75' : " bis 0,75 t",
+                            '150' : " bis 1,5 t",
+                            '280' : " bis 2,8 t",
+                            '350' : " bis 3,5 t",
+                            '750' : " bis 7,5 t",
+                            '1200' : " bis 12 t",
+                            '1800' : " bis 18 t",
+                            '2600' : " bis 26 t",
+                            '3200' : " bis 32 t",
+                            '3900' : " bis 39 t",
+                            '5000' : " {'>'} 39 t",
+                        }[attributes.weightClass]
+                    }
+
+                </div>
+            )}
+
+            {attributes?.brake && (
+                <div className="bg-[#1D1F2B] p-4 font-semibold flex items-center rounded-md text-gray-200">
+                    <GiStoneWheel  className="w-4 h-4 mr-2" />    {String(attributes.brake) === "true" ? "Hat Auflaufbremse" : "Keine Bremse"}
+                </div>
+            )}
+            
+        </div>
+     );
+}
+ 
+export default TrailerAttributeRender;
--- app/(anzeige)/inserat/[inseratId]/_components/inserat-attributes.tsx
+++ app/(anzeige)/inserat/[inseratId]/_components/inserat-attributes.tsx
@@ -1,12 +1,13 @@
-import { Inserat, LkwAttribute, PkwAttribute, TransportAttribute } from "@prisma/client";
-import { CarFrontIcon, CarFront, TruckIcon } from 'lucide-react';
+import { Inserat, LkwAttribute, PkwAttribute, TrailerAttribute, TransportAttribute } from "@prisma/client";
+import { CarFrontIcon, CarFront, TruckIcon, CaravanIcon } from 'lucide-react';
 import PkwAttributeRender from "./_categories/pkw-attribute-render";
 import LkwAttributeRender from "./_categories/lkw-attribute-render copy";
 import TransportAttributeRender from "./_categories/transport-attribute-render";
 import { PiVanFill } from "react-icons/pi";
+import TrailerAttributeRender from "./_categories/trailer-attribute-render";
 
 interface InseratAttributesProps {
-    inserat: Inserat & { pkwAttribute: PkwAttribute, lkwAttribute: LkwAttribute, transportAttribute : TransportAttribute }
+    inserat: Inserat & { pkwAttribute: PkwAttribute, lkwAttribute: LkwAttribute, transportAttribute : TransportAttribute, trailerAttribute : TrailerAttribute }
 }
 
 const InseratAttributes: React.FC<InseratAttributesProps> = ({
@@ -19,7 +20,8 @@ const InseratAttributes: React.FC<InseratAttributesProps> = ({
                     {
                         'PKW': <CarFrontIcon className="h-4 w-4 mr-2" />,
                         'LKW': <TruckIcon className="h-4 w-4 mr-2" />,
-                        'TRANSPORT' : <PiVanFill className="h-4 w-4 mr-2" />
+                        'TRANSPORT' : <PiVanFill className="h-4 w-4 mr-2" />,
+                        'TRAILOR' : <CaravanIcon className="h-4 w-4 mr-2" />
                     }[inserat.category]
                 }
 
@@ -30,7 +32,8 @@ const InseratAttributes: React.FC<InseratAttributesProps> = ({
                     {
                         'PKW': <PkwAttributeRender attributes = {inserat?.pkwAttribute} />,
                         'LKW' : <LkwAttributeRender attributes = {inserat?.lkwAttribute} />,
-                        'TRANSPORT' : <TransportAttributeRender attributes={inserat?.transportAttribute} />                        
+                        'TRANSPORT' : <TransportAttributeRender attributes={inserat?.transportAttribute} />,
+                        'TRAILOR' : <TrailerAttributeRender attributes={inserat?.trailerAttribute} />                        
                     }[inserat.category]
                 }
             </div>

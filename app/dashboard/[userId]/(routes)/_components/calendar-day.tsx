ca729865da600b0243ba47b03fa1c054c107df63
Pascal <zagarolopascal@gmail.com>
2024-03-24 00:03:03 +0100

    filter inserat bookings

--- app/dashboard/[userId]/(routes)/_components/calendar-day.tsx
+++ app/dashboard/[userId]/(routes)/_components/calendar-day.tsx
@@ -32,6 +32,14 @@ const CalendarDay: React.FC<CalendarDayProps> = ({
 
   const inseratFilter = searchParams.get("inseratId");
   const router = useRouter();
+  
+  const isShowing = (id : string) => {
+    if(inseratFilter){
+      return id === inseratFilter;
+    }
+    return true;
+  }
+
   return (
 
 
@@ -46,43 +54,49 @@ const CalendarDay: React.FC<CalendarDayProps> = ({
       {bookings?.map((pBooking) => {
         return (
 
+          <>
+          {isShowing(pBooking.inseratId) && 
           <div
-            key={pBooking.id}
-            className="   bg-blue-600 rounded-md p-2 flex justify-center mt-2"
-          >
-            {inseratFilter ? (
-              <X className="text-gray-900 w-4 h-4" />
-            ) : (
-              <HoverCard>
-                <HoverCardTrigger><CarIcon className="text-gray-200 w-4 h-4" /></HoverCardTrigger>
-                <HoverCardContent className="dark:bg-[#1C1C1C] border-none">
-                  <h3 className="font-semibold flex justify-start">
-                    {//@ts-ignore
-                    pBooking.inserat.title}
-                  </h3>
-                  <div className="flex items-center justify-start hover:underline hover:cursor-pointer" 
-                  onClick={() => {router.push(`/profile/${booking.userId}`)}}>
-                    <UserIcon className="h-4 w-4 mr-2" /> {//@ts-ignore
-                    pBooking?.user?.name} 
-                    <div className="ml-auto">
-                    <Image 
-                    alt="Mieter-Bild"
-                    src={//@ts-ignore
-                      pBooking?.user?.image || "/placeholder-person.jpg"}
-                    width={100}
-                    height={100}
-                    className="w-[32px] h-[32px] rounded-full"
-                    />
-                    </div>
+          key={pBooking.id}
+          className="   bg-blue-600 rounded-md p-2 flex justify-center mt-2"
+        >
+          {inseratFilter ? (
+            <X className="text-gray-900 w-4 h-4" />
+          ) : (
+            <HoverCard>
+              <HoverCardTrigger><CarIcon className="text-gray-200 w-4 h-4" /></HoverCardTrigger>
+              <HoverCardContent className="dark:bg-[#1C1C1C] border-none">
+                <h3 className="font-semibold flex justify-start">
+                  {//@ts-ignore
+                  pBooking.inserat.title}
+                </h3>
+                <div className="flex items-center justify-start hover:underline hover:cursor-pointer" 
+                onClick={() => {router.push(`/profile/${booking.userId}`)}}>
+                  <UserIcon className="h-4 w-4 mr-2" /> {//@ts-ignore
+                  pBooking?.user?.name} 
+                  <div className="ml-auto">
+                  <Image 
+                  alt="Mieter-Bild"
+                  src={//@ts-ignore
+                    pBooking?.user?.image || "/placeholder-person.jpg"}
+                  width={100}
+                  height={100}
+                  className="w-[32px] h-[32px] rounded-full"
+                  />
                   </div>
-                  <div className="flex items-center justify-start">
-                  <CalendarSearchIcon className="h-4 w-4 mr-2" />
-                  {format(new Date(pBooking.startDate), "dd.MM")} - {format(new Date(pBooking.endDate), "dd.MM")}
-                  </div>
-                </HoverCardContent>
-              </HoverCard>
-            )}
-          </div>
+                </div>
+                <div className="flex items-center justify-start">
+                <CalendarSearchIcon className="h-4 w-4 mr-2" />
+                {format(new Date(pBooking.startDate), "dd.MM")} - {format(new Date(pBooking.endDate), "dd.MM")}
+                </div>
+              </HoverCardContent>
+            </HoverCard>
+          )}
+        </div>
+          }
+          </>
+            
+          
 
         );
       })}

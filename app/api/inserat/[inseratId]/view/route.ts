a84e7a5d8ef8dffe96789799ea4c833e684186fa
Pascal <zagarolopascal@gmail.com>
2024-03-23 17:36:32 +0100

    ...

--- app/api/inserat/[inseratId]/view/route.ts
+++ app/api/inserat/[inseratId]/view/route.ts
@@ -1,7 +1,8 @@
+import { inserat } from './../../../../../db/schema';
 
 import db from "@/db/drizzle";
-import { inserat } from "@/db/schema";
-import { eq } from "drizzle-orm";
+
+import { eq, sql } from "drizzle-orm";
 import { NextResponse } from "next/server"
 
 export async function PATCH(
@@ -10,17 +11,17 @@ export async function PATCH(
 ) {
     try {
 
-        const foundInserat = await db.query.inserat.findFirst({
-            where : (
-                eq(inserat.id, params.inseratId)
-            )
-        })
-
-        const updateInserat = await db.update(inserat).set({
-            views : (foundInserat.views + 1)
-        }).where(eq(inserat.id, params.inseratId)).returning()
-
-        return NextResponse.json(updateInserat[0])
+        const increment = (column: typeof inserat, value = 1) => {
+            return sql`${column} + ${value}`;
+          };
+       
+        const updateInserat = db.update(inserat).set({
+            
+            views: increment(inserat.views, 1)
+        }).where(eq(inserat.id, sql.placeholder('id'))).prepare("updateInserat")
+        
+       await updateInserat.execute({ id: params.inseratId })
+        return NextResponse.json(updateInserat)
     } catch(error) {
         console.log(error)
         return new NextResponse(error, { status: 500 });

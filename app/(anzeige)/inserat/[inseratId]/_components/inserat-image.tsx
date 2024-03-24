8a481f1ffb9cbdcc67c48a4bed056c51ed4c387b
Pascal <zagarolopascal@gmail.com>
2024-03-24 13:30:14 +0100

    mobile view on inserat

--- app/(anzeige)/inserat/[inseratId]/_components/inserat-image.tsx
+++ app/(anzeige)/inserat/[inseratId]/_components/inserat-image.tsx
@@ -9,39 +9,40 @@ import Image from "next/image";
 
 
 interface InseratImageCarouselProps {
-    imagesData : typeof images.$inferSelect[]
+  imagesData: typeof images.$inferSelect[]
 }
 
 const InseratImageCarousel: React.FC<InseratImageCarouselProps> = ({
   imagesData
 }) => {
 
-    const sortedImages = [...imagesData].sort((a, b) => a.position - b.position);
+  const sortedImages = [...imagesData].sort((a, b) => a.position - b.position);
 
-    return ( 
-        <Carousel className="w-full items-center md:h-[320px]  ">
+  return (
+    <Carousel className="w-full items-center md:h-[320px]  ">
       <CarouselContent className="items-center">
         {sortedImages.map((image, index) => (
           <CarouselItem key={index}>
-          <div className="w-full md:h-[320px] rounded-md overflow-hidden drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
-          <Image
-        className="w-full h-full object-cover"
-        src={image.url}
-        alt="Inserat Image"
-        fill
-    />
-</div>
-          <p className="flex justify-center text-gray-100 font-semibold text-sm mt-2">
-            ( {index + 1} / {sortedImages.length} )
-          </p>
-        </CarouselItem>
-        
+            <div className="w-full md:h-[320px] rounded-md overflow-hidden drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+              <Image
+                className="w-full h-full object-cover"
+                src={image.url}
+                alt="Inserat Image"
+                width={1920}
+                height={1080}
+              />
+            </div>
+            <p className="flex justify-center text-gray-100 font-semibold text-sm mt-2">
+              ( {index + 1} / {sortedImages.length} )
+            </p>
+          </CarouselItem>
+
         ))}
       </CarouselContent>
-      
-      
+
+
     </Carousel>
-     );
+  );
 }
- 
+
 export default InseratImageCarousel;

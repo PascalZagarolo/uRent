33bff46442057933a7e662e392d532b07204e4e0
Pascal <zagarolopascal@gmail.com>
2024-03-25 14:00:52 +0100

    ...

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ import ActiveStatus from '@/components/ActiveStatus'
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/react"
 import { ThemeProvider } from '@/components/theme-provider'
-import CookiesDialog from '@/components/cookies'
+
 
 
 const inter = Inter({ subsets: ['latin'] })

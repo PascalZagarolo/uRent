5884932d79a43c1f059bf1155287570213d99cad
Pascal <zagarolopascal@gmail.com>
2024-03-28 19:48:16 +0100

    add vehicles

--- app/dashboard/[userId]/(routes)/manage/_components/rendered-inserat.tsx
+++ app/dashboard/[userId]/(routes)/manage/_components/rendered-inserat.tsx
@@ -5,62 +5,77 @@ import Image from "next/image";
 import { PiVanFill } from "react-icons/pi";
 import { RiCaravanLine } from "react-icons/ri";
 import { convertState } from '../../../../../../actions/convert-states';
+import { MdAddToPhotos } from "react-icons/md";
+import { Button } from "@/components/ui/button";
+import VehicleDialog from "./vehicle-dialog";
 
-interface RenderedInseratProps { 
-    thisInserat : typeof inserat.$inferSelect
+
+interface RenderedInseratProps {
+    thisInserat: typeof inserat.$inferSelect
 }
 
 const RenderedInserat: React.FC<RenderedInseratProps> = ({
     thisInserat
 }) => {
-    return ( 
+    return (
         <div className="w-full">
             <div className="p-4">
                 <div className="flex items-center space-x-2">
-                    <div className="p-2 rounded-md bg-[#1C1C1C]">
-                    {
-                        {
-                            'PKW': <CarFront className=" text-gray-100 h-4 w-4 " />,
-                            'LKW': <Truck className=" text-gray-100 h-4 w-4 " />,
-                            'TRANSPORT': <PiVanFill className=" text-gray-100 h-4 w-4 " />,
-                            'TRAILER': <RiCaravanLine className="text-gray-100 w-4 h-4" />
-                        }[thisInserat.category]
-                    }
+                    <div className="rounded-md bg-[#1C1C1C]">
+                        <Button className="" variant="ghost">
+                            {
+                                {
+                                    'PKW': <CarFront className=" text-gray-100 h-4 w-4 " />,
+                                    'LKW': <Truck className=" text-gray-100 h-4 w-4 " />,
+                                    'TRANSPORT': <PiVanFill className=" text-gray-100 h-4 w-4 " />,
+                                    'TRAILER': <RiCaravanLine className="text-gray-100 w-4 h-4" />
+                                }[thisInserat.category]
+                            }
+                        </Button>
                     </div>
-                <h3 className="text-sm font-semibold flex w-full">
-                    {thisInserat?.title} 
-                    <p className="ml-auto text-xs">
-                    {format(new Date(thisInserat?.createdAt), 'dd.MM.yyyy')}
-                    </p>
-                </h3>
+                    <h3 className="text-sm font-semibold flex w-full items-center">
+                        {thisInserat?.title}
+                        <div className="ml-auto flex items-center space-x-2">
+                            <p className="ml-auto text-xs">
+                                {format(new Date(thisInserat?.createdAt), 'dd.MM.yyyy')}
+                            </p>
+                            {thisInserat?.multi && (
+                                <div className="ml-auto">
+                                    <VehicleDialog 
+                                    thisInserat={thisInserat}
+                                    />
+                                </div>
+                            )}
+                        </div>
+                    </h3>
                 </div>
                 <div className="mt-2">
                     <div className="w-full flex justify-center">
                         <Image
-                        alt="preview image"
-                        width={1000}
-                        height={1000}
-                        src={thisInserat?.images[0].url} 
-                        className="w-full h-[200px] object-cover"
+                            alt="preview image"
+                            width={1000}
+                            height={1000}
+                            src={thisInserat?.images[0].url}
+                            className="w-full h-[200px] object-cover"
                         />
                     </div>
                     <div className="w-full mt-2 flex justify-start">
                         <MapPinIcon
-                        className="w-4 h-4 mr-2 text-rose-600"
+                            className="w-4 h-4 mr-2 text-rose-600"
                         />
-                        
+
                         <div className="w-1/2 text-xs truncate">
                             {thisInserat?.address?.locationString}
                         </div>
                         <div className="w-1/2 flex justify-end truncate text-xs font-semibold">
-                        {thisInserat?.address?.postalCode + ", "}
-                        {convertState(thisInserat?.address?.state)}
-                        </div> 
-                        
+                            {thisInserat?.address?.postalCode + ", "}
+                            {convertState(thisInserat?.address?.state)}
+                        </div>
+
                     </div>
                     <div className="w-full mt-2 mb-4 flex justify-start">
                         <CalendarCheck2Icon
-                        className="w-4 h-4 mr-2 "
+                            className="w-4 h-4 mr-2 "
                         />
                         {thisInserat?.annual ? (
                             <p className="text-sm">
@@ -78,7 +93,7 @@ const RenderedInserat: React.FC<RenderedInseratProps> = ({
                 </div>
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default RenderedInserat;
---
+++ app/dashboard/[userId]/(routes)/manage/_components/vehicle-dialog.tsx
@@ -0,0 +1,140 @@
+'use client'
+
+
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogHeader, DialogTrigger } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { inserat } from "@/db/schema";
+import axios from "axios";
+import { CldUploadButton } from "next-cloudinary";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { MdAddToPhotos } from "react-icons/md";
+
+interface VehicleDialogProps {
+    thisInserat: typeof inserat.$inferSelect
+}
+const VehicleDialog: React.FC<VehicleDialogProps> = ({
+    thisInserat
+}) => {
+
+    const [currentTitle, setCurrentTitle] = useState<string>("");
+    const [currentImage, setCurrentImage] = useState<string>("");
+    const [currentRegistration, setCurrentRegistration] = useState<string>("");
+
+    const router = useRouter();
+
+    const handleImageUpload = (result : any) => {
+        setCurrentImage(result?.info?.secure_url)
+    }
+
+    const onSubmit = async () => {
+        const values = {
+            title : currentTitle,
+        }
+        try {
+            await axios.post(`/api/inserat/${thisInserat.id}/vehicle`, values)
+                .then(() => {
+                    router.refresh();
+                })
+            toast.success("Fahrzeug erfolgreich hinzugefügt");
+
+        } catch {
+            toast.error("Fehler beim Upload");
+        }
+    }
+
+    return (
+        <>
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button variant="ghost">
+                    <MdAddToPhotos className="w-4 h-4" />
+                </Button>
+            </DialogTrigger>
+            <DialogContent className="bg-[#0F0F0F] border-none">
+                <DialogHeader>
+                    <h3 className="flex items-center">
+                        <MdAddToPhotos className="w-4 h-4 mr-2" />    Fahrzeug hinzufügen
+                    </h3>
+                </DialogHeader>
+                <div>
+                    <h1 className="text-sm flex">
+                        <p className="font-semibold ">{thisInserat.title}</p>
+                    </h1>
+                    {/*
+                    <div className="w-full">
+                    <CldUploadButton
+                        onUpload={() => {}}
+                        uploadPreset="oblbw2xl"
+                        options={{ maxFiles: 1 }}
+                        className="w-full"
+                        
+                    >
+                        {!currentImage ? (
+                            <div className="text-sm p-16 w-full justify-center flex border-dashed border border-gray-500 mt-2">
+                            Foto hinzufügen
+                        </div>
+                        ) : (
+                            <div>
+                                <Image
+                                alt="Vehicle Image" 
+                                src={currentImage}
+                                width={200}
+                                height={200}
+                                className="w-full h-16 object-cover"
+                                />
+                            </div>
+                        )}
+                    </CldUploadButton>
+                    </div>
+*/}
+                    <div className="mt-4">
+                        <Label>
+                            Titel *
+                        </Label>
+                        <Input
+                            className="dark:bg-[#080808] dark:border-none mt-2"
+                            onChange={(e) => {
+                                setCurrentTitle(e.target.value)
+                            }}
+                        />
+                    </div>
+                </div>
+                <div>
+
+                    <div className="mt-2">
+                        <Label>
+                            Kennzeichen
+                        </Label>
+                        <Input
+                            className="dark:bg-[#080808] dark:border-none mt-2"
+                            onChange={(e) => {
+                                setCurrentTitle(e.target.value)
+                            }}
+                        />
+                    </div>
+                </div>
+                <div>
+
+                </div>
+                <div className="ml-auto">
+                    <DialogTrigger asChild>
+                    <Button variant="ghost" className="bg-[#080808] hover:bg-[#151515]" disabled={!currentTitle} onClick={onSubmit}>
+                        Fahrzeug hinzufügen
+                    </Button>
+                    </DialogTrigger>
+                </div>
+            </DialogContent>
+        </Dialog>
+        
+        
+        
+        </>
+    );
+}
+
+export default VehicleDialog;

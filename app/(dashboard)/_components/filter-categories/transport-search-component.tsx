0e8afe0426ff2b0a28676e11897e92c479c8bdb4
Pascal <zagarolopascal@gmail.com>
2024-03-29 22:12:42 +0100

    power + extratype + loading volume + loading size on transport sidebar

--- app/(dashboard)/_components/filter-categories/transport-search-component.tsx
+++ app/(dashboard)/_components/filter-categories/transport-search-component.tsx
@@ -5,11 +5,17 @@ import PkwDoorsBar from "./pkw/pkw-doors";
 import PkwFuelBar from "./pkw/pkw-fuel";
 import PkwTransmissionBar from "./pkw/pkw-transmission";
 import LkwLoadingBar from "./lkw/lkw-loading";
+import { Separator } from "@/components/ui/separator";
+import LoadingVolumeBar from "./every/loading_volume";
+import LoadingSizeBar from "./every/loading_size";
+import TrailerExtraTypeBar from "./trailer/trailer-extra-type";
+import PkwPowerBar from "./pkw/pkw-power";
 
 const TransportSearchComponents = () => {
     return ( 
         <div className="mt-2 space-y-4">
             <LkwLoadingBar />
+            <TrailerExtraTypeBar />
             <div className="w-full flex gap-x-2">
             <div className="w-1/2">
             <PkwSeatsBar />
@@ -26,7 +32,14 @@ const TransportSearchComponents = () => {
             <PkwTransmissionBar />
             </div>
             </div>
-            
+            <PkwPowerBar />
+            <div className="w-full gap-x-2 space-y-4">
+            <h3 className="flex justify-center text-md items-center font-semibold  mt-4 text-gray-200">
+                <Separator className="w-1/3 mr-2 bg-gray-200 h-[0.5px]" /> Laderaum <Separator className="w-1/3 ml-2 bg-gray-200 h-[0.5px]" />
+            </h3>
+            <LoadingVolumeBar />
+            <LoadingSizeBar />
+            </div>
             
             
         </div>

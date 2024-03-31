02afcd2dd36fe10b9c21c86b71cf940e0d884839
Pascal <zagarolopascal@gmail.com>
2024-03-31 19:03:41 +0200

    arrows and enlarge inserat pic

--- app/(anzeige)/inserat/[inseratId]/_components/inserat-image.tsx
+++ app/(anzeige)/inserat/[inseratId]/_components/inserat-image.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 
-import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel";
+import { Card, CardContent } from "@/components/ui/card";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { images } from "@/db/schema";
 
 
@@ -19,27 +21,53 @@ const InseratImageCarousel: React.FC<InseratImageCarouselProps> = ({
   const sortedImages = [...imagesData].sort((a, b) => a.position - b.position);
 
   return (
-    <Carousel className="w-full items-center md:h-[320px]  ">
+    <Carousel className="w-full items-center md:h-[320px]">
       <CarouselContent className="items-center">
         {sortedImages.map((image, index) => (
-          <CarouselItem key={index}>
-            <div className="w-full md:h-[320px] rounded-md overflow-hidden drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
-              <Image
-                className="w-full h-full object-cover"
-                src={image.url}
-                alt="Inserat Image"
-                width={1920}
-                height={1080}
-              />
-            </div>
-            <p className="flex justify-center text-gray-100 font-semibold text-sm mt-2">
-              ( {index + 1} / {sortedImages.length} )
-            </p>
-          </CarouselItem>
+          <>
 
+            <CarouselItem key={index}>
+              <div className="w-full md:h-[320px] rounded-md overflow-hidden drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Image
+                      className="w-full h-full object-cover hover:cursor-pointer"
+                      src={image.url}
+                      alt="Inserat Image"
+                      width={1920}
+                      height={1080}
+                    />
+                  </DialogTrigger>
+                  <DialogContent className="dark:bg-[#0F0F0F] dark:border-none w-screen p-1">
+                    <div className="flex justify-center">
+                      <Image
+                        className="w-full h-full object-cover flex justify-center"
+                        src={image.url}
+                        alt="Inserat Image"
+                        width={1920}
+                        height={1080}
+                      />
+                    </div>
+                  </DialogContent>
+                </Dialog>
+              </div>
+              <p className="flex justify-center text-gray-100 font-semibold text-sm mt-2">
+                ( {index + 1} / {sortedImages.length} )
+              </p>
+
+            </CarouselItem>
+          </>
         ))}
-      </CarouselContent>
 
+      </CarouselContent>
+      {
+        imagesData.length > 1 && (
+          <div className="sm:block hidden">
+            <CarouselPrevious />
+            <CarouselNext />
+          </div>
+        )
+      }
 
     </Carousel>
   );

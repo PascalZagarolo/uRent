750c8c19c2141d60b5791582e9671b8702fd7cb7
Pascal <zagarolopascal@gmail.com>
2024-04-04 22:45:12 +0200

    reset password in account settings

--- app/settings/(routes)/safety/_components/change-password.tsx
+++ app/settings/(routes)/safety/_components/change-password.tsx
@@ -1,6 +1,26 @@
-import { Label } from "@/components/ui/label";
+'use client'
 
-const ChangePassword = () => {
+import { reset, resetClient } from "@/actions/reset";
+import { Label } from "@/components/ui/label";
+import { sendPasswordResetEmail } from "@/lib/mail";
+import { generatePasswordResetToken } from "@/lib/tokens";
+import toast from "react-hot-toast";
+
+interface ChangePasswordProps {
+    userEmail: string;
+}
+
+const ChangePassword: React.FC<ChangePasswordProps> = ({
+    userEmail
+}) => {
+
+    const onSubmit = async () => {
+        
+        await resetClient(userEmail);
+
+        toast.success("Email gesendet!")
+    }
+
     return (
         <div>
             <div>
@@ -11,12 +31,12 @@ const ChangePassword = () => {
 
                     <div className="w-full">
                         <div className="pl-3 p-2.5 bg-[#141414] text-sm rounded-md">
-                        *********
+                            *********
                         </div>
                         <p className="ml-auto flex justify-end p-1 w-full text-xs font-semibold hover:underline hover:cursor-pointer"
-
+                            onClick={onSubmit}
                         >
-                        Passwort ändern                            
+                            Passwort ändern
                         </p>
 
                     </div>
--- app/settings/_components/settings-tabs.tsx
+++ app/settings/_components/settings-tabs.tsx
@@ -26,18 +26,18 @@ const MenuBar = () => {
     return ( 
         <div className="w-full dark:bg-[#141414]">
             <div className="pt-4 px-4 flex justify-evenly text-sm font-semibold">
-            <a className={cn("p-4 rounded-t-md", isDashboard && "bg-[#1C1C1C]")}
+            <a className={cn("p-4 rounded-t-md dark:text-gray-200/70", isDashboard && "bg-[#1C1C1C] dark:text-gray-200")}
             href={`/settings`}
             >
                 Account
             </a>
-            <a className={cn("p-4 rounded-t-md hover:cursor-pointer", isAnsicht && "bg-[#1C1C1C]")} href={`/settings/view`}>
+            <a className={cn("p-4 rounded-t-md hover:cursor-pointer dark:text-gray-200/70", isAnsicht && "bg-[#1C1C1C] dark:text-gray-200")} href={`/settings/view`}>
                 Ansicht
             </a>
-            <a className={cn("p-4 rounded-t-md hover:cursor-pointer", isPrivacy && "bg-[#1C1C1C]")} href={`/settings/privacy`}>
+            <a className={cn("p-4 rounded-t-md hover:cursor-pointer dark:text-gray-200/70", isPrivacy && "bg-[#1C1C1C] dark:text-gray-200")} href={`/settings/privacy`}>
                 Privatsphäre
             </a>
-            <a className={cn("p-4 rounded-t-md hover:cursor-pointer", isSafety && "bg-[#1C1C1C]")} href={`/settings/safety`}>
+            <a className={cn("p-4 rounded-t-md hover:cursor-pointer dark:text-gray-200/70", isSafety && "bg-[#1C1C1C] dark:text-gray-200")} href={`/settings/safety`}>
                 Sicherheit
             </a>
             

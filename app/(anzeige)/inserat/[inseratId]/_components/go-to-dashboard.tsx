a92b216fe6381f54529e6f44907b86babb0892fd
Pascal <zagarolopascal@gmail.com>
2024-04-05 23:25:19 +0200

    go to dashboard button

---
+++ app/(anzeige)/inserat/[inseratId]/_components/go-to-dashboard.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { Button } from "@/components/ui/button";
+import { PlusSquare } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+interface GoToDashboardProps {
+    userId : string,
+    inseratId : string
+}
+
+const GoToDashboard: React.FC<GoToDashboardProps> = ({
+    userId,
+    inseratId
+}) => {
+    
+    const router = useRouter();
+
+    return (
+        <div className="w-[240px]">
+            <Button className="w-full bg-[#20222F] text-gray-200 border-none border-gray-900 hover:bg-[#20222F]"
+            onClick={() => {
+                router.push(`/dashboard/${userId}/manage`);
+            }}
+            >
+                <PlusSquare className="mr-2 h-4 w-4" /> Zum Dashboard
+            </Button>
+        </div>
+    );
+}
+
+export default GoToDashboard;

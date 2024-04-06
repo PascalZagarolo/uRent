864934f1219a464a5df30ab7123bd5733fcaaa33
Pascal <zagarolopascal@gmail.com>
2024-04-07 00:31:41 +0200

    stripe hook without dynamic packages

--- app/api/stripe/[inseratId]/route.ts
+++ app/api/stripe/[inseratId]/route.ts
@@ -1,3 +1,73 @@
-export async function GET() {
-    return null;
+import getCurrentUser from "@/actions/getCurrentUser";
+import db from "@/db/drizzle";
+import { inseratSubscription } from "@/db/schema";
+import { stripe } from "@/lib/stripe";
+import { eq } from "drizzle-orm";
+
+import { NextResponse } from "next/server";
+
+
+export async function PATCH(
+    { params } : { params : { inseratId : string }}
+) {
+    try {
+
+        const currentUser = await getCurrentUser();
+
+        if(!currentUser) {
+            return new NextResponse("Nicht autorisiert : Kein Login", {status: 401})
+        }
+
+        const existingSubscription = await db.query.inseratSubscription.findFirst({
+            where : (
+                eq(inseratSubscription.inseratId, params.inseratId)
+            )
+        })
+
+        if(existingSubscription && existingSubscription.stripe_customer_id) {
+            //change redirectUrl
+            const stripeSession = await stripe.billingPortal.sessions.create({
+                customer : existingSubscription.stripe_customer_id,
+                return_url : `${process.env.NEXT_PUBLIC_BASE_URL}/app/inserate/${params.inseratId}`
+            })
+
+            return new NextResponse(JSON.stringify({url : stripeSession.url}))
+        }
+
+        //change success && cancel url
+        const stripeSesison = await stripe.checkout.sessions.create({
+            success_url : `${process.env.NEXT_PUBLIC_BASE_URL}/app/inserate/${params.inseratId}`,
+            cancel_url : `${process.env.NEXT_PUBLIC_BASE_URL}/app/inserate/${params.inseratId}`,
+            payment_method_types : ['card', 'paypal', 'sofort'],
+            mode : "subscription",
+            customer_email : currentUser.email,
+            line_items : [
+                {
+                    price_data : {
+                        currency : "eur",
+                        product_data : {
+                            name : "Basis",
+                            description : "Basis...",
+                        },
+                        unit_amount : 2500,
+                        recurring : {
+                            interval : "month"
+                        }
+                    },
+                    quantity : 1
+                }
+            ],
+            metadata : {
+                inseratId : params.inseratId,
+                userId : currentUser.id,
+                subscription : "Basis"
+            }
+        })
+
+        return new NextResponse(JSON.stringify({ url : stripeSesison.url }))
+
+    } catch(error) {
+        console.log("Error in Stripe checkout", error);
+        return new NextResponse(null, {status: 500})
+    }
 }
--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,44 +1,75 @@
-import Stripe from "stripe";
+import db from "@/db/drizzle";
+import { inseratSubscription } from "@/db/schema";
+import { stripe } from "@/lib/stripe";
+import { eq } from "drizzle-orm/sql";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
-import stripe from "stripe";
-import db from "@/db/drizzle";
-import { purchase } from "@/db/schema";
+import Stripe from "stripe";
 
 export async function POST(
-    req : Request,
+    req : Request
 ) {
-    const body = await req.text();
-    const signature = headers().get("Stripe-Signature") as string;
+
+    const body = await req.json();
+    const signature = headers().get("Stripe-Signature") as string
 
     let event: Stripe.Event;
 
+    
     try {
         event = stripe.webhooks.constructEvent(
             body,
             signature,
-            process.env.STRIPE_WEBHOOK_SECRET!
+            process.env.STRIPE_WEBHOOK_SECRET
         )
     } catch(error : any) {
-        return new NextResponse(`Webhook Error ${error.message}` , { status : 400 })
+        return new NextResponse(`Webhook Error : ${error.message}` , { status : 400 })
     }
 
-    const session = event.data.object as Stripe.Checkout.Session;
-    const userId = session?.metadata?.userId;
-    const inseratId = session?.metadata?.inseratId;
+    const session = event.data.object as Stripe.Checkout.Session
 
     if(event.type === "checkout.session.completed") {
-        if(!userId || !inseratId) {
-            return new NextResponse("Invalid metadata: WEBHOOK_2" , { status : 400 })
+
+        const subscription = await stripe.subscriptions.retrieve(session.subscription as string);
+
+        if(!session?.metadata?.inseratId) {
+            return new NextResponse("InseratId nicht gefunden", {status : 400})
         }
 
-        await db.insert(purchase).values({
-            inseratId : inseratId,
-            userId : userId
+        if(!session?.metadata?.userId) {
+            return new NextResponse("UserId nicht gefunden", {status : 400})
+        }
+
+        if(!session?.metadata?.subscription) {
+            return new NextResponse("Subscription nicht gefunden", {status : 400})
+        }
+
+        await db.insert(inseratSubscription).values({
+            inseratId : session?.metadata?.inseratId,
+            stripe_subscription_id : subscription.id,
+            stripe_customer_id : subscription.customer as string,
+            stripe_price_id : subscription.items.data[0].price.id,
+            stripe_current_period_end : new Date(
+                subscription.current_period_end * 1000
+            
+            )
         })
-    } else {
-        return new NextResponse("Invalid event type: WEBHOOK_1" , { status : 200 })
     }
 
-    return new NextResponse(null, { status : 200 })
+    if(event.type === "invoice.payment_succeeded") {
+        const subscription = await stripe.subscriptions.retrieve(session.subscription as string);
+
+        await db.update(inseratSubscription).set({
+            stripe_price_id : subscription.items.data[0].price.id,
+            stripe_current_period_end : new Date(
+                subscription.current_period_end * 1000
+            )
+
+        }).where(
+            eq(inseratSubscription.stripe_subscription_id, subscription.id)
+        )
+    }
+
+    return new NextResponse(null, {status : 200})
+
 }

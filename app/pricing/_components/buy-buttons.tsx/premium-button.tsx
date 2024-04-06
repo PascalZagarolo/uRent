5605a26059009dd1554f62d3f52fdf73895e5339
Pascal <zagarolopascal@gmail.com>
2024-04-07 00:31:41 +0200

    stripe hook without dynamic packages

--- app/pricing/_components/buy-buttons.tsx/premium-button.tsx
+++ app/pricing/_components/buy-buttons.tsx/premium-button.tsx
@@ -4,7 +4,8 @@ const PremiumButton = () => {
     return (
         <Button className="w-full text-sm bg-blue-800 hover:bg-blue-900 text-gray-200 mt-2 mb-2">
             Jetzt starten
-        </Button>);
+        </Button>
+        );
 }
 
 export default PremiumButton;

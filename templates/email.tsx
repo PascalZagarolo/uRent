8c9d594eaac6a3eda3986493a173c65737db978c
Pascal <zagarolopascal@gmail.com>
2024-04-15 00:09:16 +0200

    fix fix

--- templates/email.tsx
+++ templates/email.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Html } from '@react-email/html';
 import { Button } from '@react-email/button';
 
-export function Email(props) {
+export function Email(props : any) {
   const { url } = props;
 
   return (

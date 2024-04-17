06157ca2531d127670218b0cd480bdeafea136a1
Pascal <zagarolopascal@gmail.com>
2024-04-17 17:57:10 +0200

    children in section

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -47,7 +47,10 @@ const DashboardLayout = async (
                     foundNotifications={foundNotifications}
                      />
             </div>
-             {children}
+            <section>
+            {children}
+            </section>
+             
         </div>
      );
 }

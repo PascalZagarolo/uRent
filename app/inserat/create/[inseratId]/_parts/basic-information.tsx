ae891d1d1febe23fd0f63c963ae0b440af17474b
Pascal <zagarolopascal@gmail.com>
2024-04-22 11:33:22 +0200

    removed erweiterte fahrzeugkategorie

--- app/inserat/create/[inseratId]/_parts/basic-information.tsx
+++ app/inserat/create/[inseratId]/_parts/basic-information.tsx
@@ -55,7 +55,7 @@ const BasicInformation: React.FC<BasicInformationProps> = ({
                 <div className="w-1/2">
                     {
                         {
-                            "PKW" : <PkwExtraType thisExtraType={thisInserat?.pkwAttribute?.extraType} />,
+                            
                             "TRANSPORT" : <TransportExtraType thisExtraType={thisInserat?.transportAttribute?.extraType} />,
                             "LKW" : <ApplicationForm thisApplication={thisInserat?.lkwAttribute?.application} />,
                             "TRAILER" : <TrailerExtraType thisExtraType={thisInserat?.trailerAttribute?.extraType} />

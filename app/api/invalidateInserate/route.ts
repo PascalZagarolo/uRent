85a52571d1bdf9a712b946e95dfdc329fdb5c5c6
Pascal <zagarolopascal@gmail.com>
2024-04-24 23:59:18 +0200

    cron fix

--- app/api/invalidateInserate/route.ts
+++ app/api/invalidateInserate/route.ts
@@ -1,10 +1,14 @@
+'use server'
+
 import db from "@/db/drizzle";
 import { NextResponse } from "next/server";
-import { inserat } from '../../../db/schema';
-import { eq } from "drizzle-orm";
 
-export async function GET(
-    req : Request,
+import { eq } from "drizzle-orm";
+import { inserat } from "@/db/schema";
+
+
+export async function PATCH(
+    
 ) {
     try {
 
@@ -21,21 +25,25 @@ export async function GET(
             }
         })
 
-        console.log("sadas")
+        
 
         const currentDate = new Date();
 
-        //privatize every inserat where user has no subscriptions
         for (const pInserat of findMatchingInserate) {
-            if(!inserat.user?.subscription || inserat.user?.subscription?.stripe_current_period_end < currentDate) {
-                
-                const updateInserate = await db.update(inserat).set({
-                    isPublished : false,
-                }).where(eq(inserat.userId, pInserat.userId))
-            }
+            if(!pInserat.user?.subscription || pInserat.user?.subscription.length === 0 || 
+                pInserat.user?.subscription?.stripe_current_period_end < currentDate) {
+                    await db.update(inserat).set({
+                        isPublished : false
+                    }).where(eq(inserat.userId , pInserat.userId))
+                }
         }
 
-        return new NextResponse("Inserate successfully invalidated", { status: 200 });
+    
+               
+            
+                
+
+        return NextResponse.json({ message: "Inserate erfolgreich invalidiert" });
 
     } catch(error : any) {
         console.log(error);

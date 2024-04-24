6660f82f812dcc1186418f26458977b44c2d1a4d
Pascal <zagarolopascal@gmail.com>
2024-04-24 11:32:15 +0200

    dashboard route

---
+++ app/admin/layout.tsx
@@ -0,0 +1,53 @@
+
+import HeaderLogo from "../(dashboard)/_components/header-logo";
+import getCurrentUser from "@/actions/getCurrentUser";
+import MobileHeader from "../(dashboard)/_components/mobile-header";
+import { eq, sql } from "drizzle-orm";
+import db from "@/db/drizzle";
+import { notification } from "@/db/schema";
+import Footer from "../(dashboard)/_components/footer";
+
+const AdminLayout = async (
+    { children }: { children: React.ReactNode },
+
+) => {
+    
+    const currentUser = await getCurrentUser();
+
+    const findNotifications = db.query.notification.findMany({
+        where : (
+            eq(notification.userId, sql.placeholder("currentUserId"))
+        )
+    
+    }).prepare("findNotifications")
+
+    const foundNotifications = await findNotifications.execute({currentUserId: currentUser?.id})
+    
+    return (
+        <div className="bg-[#404040]/10 h-full w-full  dark:bg-[#0F0F0F] ">
+            <div className="relative top-0 w-full z-50">
+                <HeaderLogo
+                    currentUser={currentUser}
+                    foundNotifications={foundNotifications}
+                    />
+            </div>
+            <div className="sm:hidden">
+                <MobileHeader
+                currentUser={currentUser}
+                foundNotifications = {foundNotifications}       
+                />
+             </div>
+            <div >       
+                {children}
+            </div>
+           
+            
+            <div>
+            <Footer />
+            </div>
+            
+        </div>
+    );
+}
+
+export default AdminLayout;

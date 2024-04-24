c5613162639342be64ee29ab792ce073a5b894a6
Pascal <zagarolopascal@gmail.com>
2024-04-24 22:49:54 +0200

    cron

---
+++ app/api/invalidateInserate/route.ts
@@ -0,0 +1,41 @@
+import db from "@/db/drizzle";
+import { NextResponse } from "next/server";
+import { inserat } from '../../../db/schema';
+import { eq } from "drizzle-orm";
+
+export async function PATCH(
+    req : Request,
+) {
+    try {
+
+        const findMatchingInserate = await db.query.inserat.findMany({
+            where : (
+                eq(inserat.isPublished, true)
+            ),
+            with : {
+                user : {
+                    with : {
+                        userSubscription : true
+                    }
+                }
+            }
+        })
+
+        const currentDate = new Date();
+
+        //privatize every inserat where user has no subscriptions
+        for (const pInserat of findMatchingInserate) {
+            if(!inserat.user.userSubscription || inserat.user.userSubscription.stripe_current_period_end < currentDate) {
+                const updateInserate = await db.update(inserat).set({
+                    isPublished : false,
+                }).where(eq(inserat.userId, pInserat.userId))
+            }
+        }
+
+        return new NextResponse("Inserate successfully invalidated", { status: 200 });
+
+    } catch(error : any) {
+        console.log(error);
+        return new NextResponse("Internal Server Error", { status: 500 });
+    }
+}

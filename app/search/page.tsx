d6ebd67e0127c7d60778a72b6f612d9d2c4d563c
Pascal <zagarolopascal@gmail.com>
2024-04-25 19:53:51 +0200

    search page ui fix

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -44,8 +44,8 @@ const SearchPage = async () => {
                     foundNotifications={foundNotifications}
                 />
             </div>
-            <div className="flex justify-center p-8 bg-[#404040]/10">
-                <div className="w-[1044px] dark:bg-[#1c1c1c] rounded-md bg-white">
+            <div className="flex justify-center sm:p-8 bg-[#404040]/10">
+                <div className="sm:w-[1044px] w-full dark:bg-[#1c1c1c] rounded-md bg-white">
                     <div className="  min-h-screen">
 
 

88aeb50bb37b30738ce7a53fb569beaa987d4596
Pascal <zagarolopascal@gmail.com>
2024-04-25 19:07:53 +0200

    function in newsletter component

--- app/settings/(routes)/privacy/_components/data-usage.tsx
+++ app/settings/(routes)/privacy/_components/data-usage.tsx
@@ -1,8 +1,49 @@
+'use client'
+
 import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@/components/ui/label";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
 import { FaDatabase } from "react-icons/fa6";
 
-const DataUsage = () => {
+interface DataUsageProps {
+    usesNewsletter : boolean;
+    currentUserId : string;
+}
+
+const DataUsage : React.FC<DataUsageProps> = ({
+    usesNewsletter,
+    currentUserId
+}) => {
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const router = useRouter();
+
+    const onCheckedChange = async (e : any) => {
+        try {
+            setIsLoading(true);
+
+            const values = {
+                newsletter : e
+            }
+
+            await axios.patch(`/api/profile/${currentUserId}`, values)
+                .then(() => {
+                    router.refresh();
+                    toast.success("Einstellungen erfolgreich aktualisiert");
+                })
+        } catch(error : any) {
+            console.log(error);
+            toast.error("Fehler beim Aktualisieren der Einstellungen");
+        } finally {
+            setIsLoading(false);
+        }
+
+    }
+
     return (
         <div>
             <h1 className="font-semibold flex">
@@ -16,7 +57,11 @@ const DataUsage = () => {
                             Newsletter und Werbe-Emails
                         </div>
                         <div className="flex gap-4 mt-2">
-                            <Checkbox className="w-4 h-4" />
+                            <Checkbox className="w-4 h-4" 
+                            checked={usesNewsletter}
+                            disabled={isLoading}
+                            onCheckedChange={(e) => onCheckedChange(e)}
+                            />
                             <Label className="sm:w-1/2 font-normal text-sm">
                                 Ich möchte gerne per E-Mail von uRent Angebote erhalten, an Umfragen teilnehmen
                                 und Informationen über Produkte und Dienstleistungen von uRent erhalten.</Label>
@@ -26,6 +71,8 @@ const DataUsage = () => {
 
                 </div>
 
+                
+                {/*
                 <div className="flex items-center gap-x-2">
 
                     <div>
@@ -42,6 +89,7 @@ const DataUsage = () => {
 
 
                 </div>
+                */}
 
             </div>
         </div>

187a475994d7052a6291fd3a2e3c54fda7bb8b6c
Pascal <zagarolopascal@gmail.com>
2024-04-25 19:39:19 +0200

    dashboard legals ui fix on mobile

--- app/(dashboard)/(legals)/imprint/page.tsx
+++ app/(dashboard)/(legals)/imprint/page.tsx
@@ -22,14 +22,14 @@ const Imprint = async () => {
         <div>
             
             
-            <div className="flex justify-center p-8 bg-[#404040]/10">
-                <div className="w-[1044px] dark:bg-[#1c1c1c] rounded-md bg-white">
+            <div className="flex justify-center sm:p-8 bg-[#404040]/10">
+                <div className="sm:w-[1044px] w-full dark:bg-[#1c1c1c] rounded-md bg-white">
                     <div className="  min-h-screen">
 
 
                         <div className="p-4 mt-4  rounded-lg ">
                             <h3 className="dark:text-gray-100 text-2xl font-semibold flex items-center">
-                                 uRent - Impressum <p className="ml-auto text-sm"> (01.01.2024) </p>
+                                 Impressum <p className="ml-auto text-sm"> (01.01.2024) </p>
                             </h3>
                         </div>
 

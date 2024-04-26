df18defa855a625a2fad008b2c7f93dde8532f9a
Pascal <zagarolopascal@gmail.com>
2024-04-26 18:59:07 +0200

    profile page...

--- app/profile/[profileId]/page.tsx
+++ app/profile/[profileId]/page.tsx
@@ -33,10 +33,10 @@ export async function generateMetadata({ params }: Props,
     try {
         const res = await db.query.userTable.findFirst({
             where: eq(userTable.id, params.profileId),
-            with : {
-                business : true
+            with: {
+                business: true
             }
-            
+
         })
         return {
             title: res.name,
@@ -89,148 +89,154 @@ const ProfilePage = async ({ params }: { params: { profileId: string } }) => {
 
     const thisUser = await db.query.userTable.findFirst({
         where: eq(userTable.id, pageOwnerId),
-        with : {
-            business : {
-                with : {
-                    businessAddresses : true,
-                    businessImages : true,
-                    openingTimes : true
+        with: {
+            business: {
+                with: {
+                    businessAddresses: true,
+                    businessImages: true,
+                    openingTimes: true
                 }
             }
         }
     })
 
-    
+
 
     const thisBusinessImages = await db.query.businessImages.findMany({
         where: (
             eq(businessImages.businessId, thisUser?.business?.id)
         ),
-        orderBy : (position, { asc }) => [asc(businessImages.position)]
-    
+        orderBy: (position, { asc }) => [asc(businessImages.position)]
+
     })
 
     const ownProfile = currentUser?.id === thisUser?.id ? true : false;
 
-   
 
-    
+
+
 
 
     return (
-        <div className="dark:bg-[#141414] w-full">
-            <div className="relative top-0 w-full z-50">
-                <HeaderLogo
-                    currentUser={currentUser}
-                    foundNotifications={foundNotifications}
-                />
-            </div>
-            <div className="sm:hidden">
-                <MobileHeader
-                    currentUser={currentUser}
-                    foundNotifications={foundNotifications}
-                />
-            </div>
-            <div className="flex justify-center lg:p-8 bg-[#404040]/10 h-full">
-                <div>
-                <AdsComponent dataAdSlot='3797720061' />
+        <>
+            <head>
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9497499351411762"//@ts-ignore
+                    crossorigin="anonymous"></script>
+            </head>
+            <div className="dark:bg-[#141414] w-full">
+                <div className="relative top-0 w-full z-50">
+                    <HeaderLogo
+                        currentUser={currentUser}
+                        foundNotifications={foundNotifications}
+                    />
                 </div>
-                <div className="md:w-[1044px] w-full dark:bg-[#1c1c1c] rounded-md bg-white pb-4 ">
-                    <div className="min-h-screen">
-                        {thisUser ? (
-                            <>
-                            <div className="sm:p-4 p-2">
-                            <div>
-                                {
-                                    thisUser.isBusiness ? (
-                                        <h3 className="text-2xl flex font-bold ">
-                                            <FaBuilding className="mr-2" />  Geschäftsprofil
-                                            <div className="ml-auto">
-                                            <MessageButton />
+                <div className="sm:hidden">
+                    <MobileHeader
+                        currentUser={currentUser}
+                        foundNotifications={foundNotifications}
+                    />
+                </div>
+                <div className="flex justify-center lg:p-8 bg-[#404040]/10 h-full">
+                    <div>
+                        <AdsComponent dataAdSlot='3797720061' />
+                    </div>
+                    <div className="md:w-[1044px] w-full dark:bg-[#1c1c1c] rounded-md bg-white pb-4 ">
+                        <div className="min-h-screen">
+                            {thisUser ? (
+                                <>
+                                    <div className="sm:p-4 p-2">
+                                        <div>
+                                            {
+                                                thisUser.isBusiness ? (
+                                                    <h3 className="text-2xl flex font-bold ">
+                                                        <FaBuilding className="mr-2" />  Geschäftsprofil
+                                                        <div className="ml-auto">
+                                                            <MessageButton />
+                                                        </div>
+                                                    </h3>
+                                                ) : (
+                                                    <h3 className="text-2xl flex font-bold ">
+                                                        <UsersIcon className="mr-2" />  Profilübersicht
+
+                                                    </h3>
+                                                )
+                                            }
+                                        </div>
+                                        {(ownProfile && !thisUser.isBusiness) && (
+                                            <RegisterBusiness />
+                                        )}
+                                        <div className="mt-4 items-center">
+                                            {thisUser.isBusiness ? (
+                                                <h1 className="text-lg font-semibold flex items-center">
+                                                    <FaKey className="w-4 h-4 mr-2" />Vermieterdetails
+                                                </h1>
+                                            ) : (
+                                                <h1 className="text-lg font-semibold flex">
+                                                    <Contact2Icon className="mr-2" />Profildetails
+                                                </h1>
+                                            )}
+                                        </div>
+
+                                        <ProfileHeader
+                                            user={thisUser}
+                                            currentUser={currentUser}
+                                            thisContactOptions={thisContactoptions}
+                                            thisImages={thisBusinessImages}
+
+                                        />
+                                        {thisUser.isBusiness && (
+                                            <div className="p-4">
+                                                <Openhours
+                                                    ownProfile={ownProfile}
+                                                    thisBusiness={thisUser.business}
+                                                />
                                             </div>
-                                        </h3>
-                                    ) : (
-                                        <h3 className="text-2xl flex font-bold ">
-                                            <UsersIcon className="mr-2" />  Profilübersicht 
-                                            
-                                        </h3>
-                                    )
-                                }
-                            </div>
-                            {(ownProfile && !thisUser.isBusiness) && (
-                                <RegisterBusiness />
-                            )}
-                            <div className="mt-4 items-center">
-                                {thisUser.isBusiness ? (
-                                    <h1 className="text-lg font-semibold flex items-center">
-                                        <FaKey className="w-4 h-4 mr-2" />Vermieterdetails
-                                    </h1>
-                                ) : (
-                                    <h1 className="text-lg font-semibold flex">
-                                        <Contact2Icon className="mr-2" />Profildetails
-                                    </h1>
-                                )}
-                            </div>
+                                        )}
+                                        {thisUser.isBusiness && (
+                                            <div className="p-4">
+                                                <AddImpressum
+                                                    ownProfile={ownProfile}
+                                                    user={thisUser}
+                                                />
+                                            </div>
+                                        )}
+                                        <div>
+                                            <div className="mt-8 px-4">
+                                                <h1 className="text-md font-semibold flex items-center">
+                                                    <TruckIcon className="mr-2 w-4 h-4" />Weitere Inhalte <p className=" ml-2 text-sm">{foundInserate.length}</p>
+                                                </h1>
+                                            </div>
+                                        </div>
+                                    </div>
 
-                            <ProfileHeader
-                                user={thisUser}
-                                currentUser={currentUser}
-                                thisContactOptions={thisContactoptions}
-                                thisImages = {thisBusinessImages}
+                                    <div className="p-2">
+                                        <OwnContentSlide
+                                            foundInserate={foundInserate}
+                                            currentUser={currentUser}
+                                        />
+                                    </div>
 
-                            />
-                            {thisUser.isBusiness && (
-                                <div className="p-4">
-                                <Openhours
-                                ownProfile={ownProfile}
-                                thisBusiness={thisUser.business}
-                                />
-                            </div>
-                            )}
-                            {thisUser.isBusiness && (
-                                <div className="p-4">
-                                <AddImpressum
-                                ownProfile={ownProfile}
-                                user={thisUser}
-                                />
-                            </div>
-                            )}
-                            <div>
-                                <div className="mt-8 px-4">
-                                    <h1 className="text-md font-semibold flex items-center">
-                                        <TruckIcon className="mr-2 w-4 h-4" />Weitere Inhalte <p className=" ml-2 text-sm">{foundInserate.length}</p>
-                                    </h1>
+
+                                </>
+                            ) : (
+                                <div className="w-full min-h-screen flex justify-center items-center">
+                                    <h3 className="flex text-xl font-semibold gap-x-4">
+                                        <FaCarCrash className="w-6 h-6" />    Dieser Nutzer scheint nicht zu existieren..
+                                    </h3>
                                 </div>
-                            </div>
+                            )}
                         </div>
-
-                        <div className="p-2">
-                            <OwnContentSlide
-                                foundInserate={foundInserate}
-                                currentUser={currentUser}
-                            />
-                        </div>
-
-                    
-                    </>
-                        ) : (
-                            <div className="w-full min-h-screen flex justify-center items-center">
-                                <h3 className="flex text-xl font-semibold gap-x-4">
-                                <FaCarCrash className="w-6 h-6" />    Dieser Nutzer scheint nicht zu existieren..
-                                </h3>
-                            </div>
-                        )}
-                        </div>
+                    </div>
+                    <div>
+                        <AdsComponent dataAdSlot='3797720061' />
+                    </div>
                 </div>
+
                 <div>
-                <AdsComponent dataAdSlot='3797720061' />
+                    <Footer />
                 </div>
             </div>
-            
-            <div>
-                <Footer />
-            </div>
-        </div>
+        </>
     );
 }
 

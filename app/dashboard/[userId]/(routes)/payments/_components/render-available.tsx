83db55baba2b08e04f7211c12926c369d979a9a3
Pascal <zagarolopascal@gmail.com>
2024-04-27 16:46:14 +0200

    added save description

--- app/dashboard/[userId]/(routes)/payments/_components/render-available.tsx
+++ app/dashboard/[userId]/(routes)/payments/_components/render-available.tsx
@@ -15,7 +15,7 @@ const RenderAvailable : React.FC<RenderAvilableProps> = ({
     return (
         <div>
             <h1 className="font-semibold">
-                Verfügbare Inserate
+                Verfügbare Inserate1
             </h1>
             <p className="text-xs dark:text-gray-200/60 ">
                 Gezählt werden nur veröffentlichte Inserate.

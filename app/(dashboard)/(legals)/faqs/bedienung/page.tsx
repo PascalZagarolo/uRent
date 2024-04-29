3132cb9a3cbb6f8a4e5de2a689add75534372254
Pascal <zagarolopascal@gmail.com>
2024-04-29 11:22:01 +0200

    faq bedienungshilfe mobile fix

--- app/(dashboard)/(legals)/faqs/bedienung/page.tsx
+++ app/(dashboard)/(legals)/faqs/bedienung/page.tsx
@@ -12,11 +12,11 @@ const MieterFaq = () => {
     return (
         <div>
 
-            <div className="flex justify-center p-8 bg-[#404040]/10">
+            <div className="flex justify-center sm:p-8 bg-[#404040]/10">
 
-                <div className="w-[1044px] dark:bg-[#1c1c1c] rounded-md bg-white">
+                <div className="sm:w-[1044px] w-full dark:bg-[#1c1c1c] rounded-md bg-white">
                     <div className="  min-h-screen">
-                        <div className="p-4 mt-4  rounded-lg ">
+                        <div className="sm:p-4 p-2 mt-4  rounded-lg ">
                             <h3 className="dark:text-gray-100 text-2xl font-semibold flex items-center">
                                 Bedienungshilfe uRent - FAQ
                             </h3>

fd5f61af802df854f6d3caf696537db4b3f9fb37
Pascal <zagarolopascal@gmail.com>
2024-05-02 14:41:39 +0200

    functioning stripe subscription

--- app/(prices)/pricing/_components/_buy-buttons.tsx/enterprise-button.tsx
+++ app/(prices)/pricing/_components/_buy-buttons.tsx/enterprise-button.tsx
@@ -12,13 +12,13 @@ import toast, { CheckmarkIcon } from "react-hot-toast";
 
 
 interface EnterpriseButtonProps {
-    selectedAmount : number;
+    selectedAmount: number;
     existingSubscription?: typeof userSubscription;
-    userId : string;
-    diffrence : number;
+    userId: string;
+    diffrence: number;
 }
 
-const EnterpriseButton : React.FC<EnterpriseButtonProps>= ({
+const EnterpriseButton: React.FC<EnterpriseButtonProps> = ({
     selectedAmount,
     existingSubscription,
     userId,
@@ -32,44 +32,54 @@ const EnterpriseButton : React.FC<EnterpriseButtonProps>= ({
 
     const calculateEnterprisePrice = () => {
         switch (selectedAmount) {
-          case 1:
-            return "prod_PtR9L9XRjwzC0N";
-          case 5:
-            return "prod_Pv4FF7MAUT1wGI";
-          case 10:
-            return "prod_Pv4FiNttsprGyN";
-          case 15:
-            return "prod_Pv4Fl9cfjK2RqC";
-          case 25:
-            return "prod_Pv4GtxdFiRuu1P";
-          case 40:
-            return "prod_Pv4Gh88pCZq6ao";
+            case 1:
+                return "prod_PtR9L9XRjwzC0N";
+            case 5:
+                return "prod_Pv4FF7MAUT1wGI";
+            case 10:
+                return "prod_Pv4FiNttsprGyN";
+            case 15:
+                return "prod_Pv4Fl9cfjK2RqC";
+            case 25:
+                return "prod_Pv4GtxdFiRuu1P";
+            case 35:
+                return "prod_Pv4Gh88pCZq6ao";
+            case 50:
+                return "prod_Q200a6I4WAhns9";
+            case 65:
+                return "prod_Q200PwTz5sLcDD";
+            case 80:
+                return "prod_Q2017X3zQWhFyu";
+            case 100:
+                return "prod_Q201WTq6XQ348g";
+            case 250:
+                return "prod_Q2021mUCfveige";
             default:
                 return ""
         }
-      };
+    };
 
-      const productId = calculateEnterprisePrice();
+    const productId = calculateEnterprisePrice();
 
     const [isLoading, setIsLoading] = useState(false)
 
     const onSubscribe = async () => {
         try {
-            
+
             setIsLoading(true);
             const values = {
                 subscriptionType: "ENTERPRISE",
                 productId: productId,
-                amount : selectedAmount,
-                ...usedId && {usedId : usedId}
-                
+                amount: selectedAmount,
+                ...usedId && { usedId: usedId }
+
             }
 
-            
-            
+
+
             const res = await axios.patch(`/api/stripe/user/${userId}`, values);
             window.location.href = res.data.url
-        } catch(error: any) {
+        } catch (error: any) {
             console.log(error)
             toast.error("Etwas ist schief gelaufen")
         } finally {
@@ -81,17 +91,17 @@ const EnterpriseButton : React.FC<EnterpriseButtonProps>= ({
         try {
             setIsLoading(true);
 
-            
+
             const values = {
-                diffrence : diffrence * 100,
+                diffrence: diffrence * 100,
                 productId: productId,
-                subscriptionType : "ENTERPRISE",
-                ...usedId && {usedId : usedId}
+                subscriptionType: "ENTERPRISE",
+                ...usedId && { usedId: usedId }
             }
 
             const res = await axios.patch(`/api/stripe/upgrade/${userId}`, values);
             window.location.href = res.data.url
-        } catch(error : any) {
+        } catch (error: any) {
             console.log(error)
             toast.error("Etwas ist schief gelaufen")
         } finally {
@@ -108,24 +118,24 @@ const EnterpriseButton : React.FC<EnterpriseButtonProps>= ({
     return (
         <>
             {//@ts-ignore
-            existingSubscription ? (
-                canUpgrade ? (
-                    <Button className="w-full text-sm bg-blue-800 hover:bg-blue-900 text-gray-200 mt-2 mb-2" 
-                        onClick={onUpgrade}>
-                        Upgraden
-                    </Button >
+                existingSubscription ? (
+                    canUpgrade ? (
+                        <Button className="w-full text-sm bg-blue-800 hover:bg-blue-900 text-gray-200 mt-2 mb-2"
+                            onClick={onUpgrade}>
+                            Upgraden
+                        </Button >
+                    ) : (
+                        <Button className="w-full text-sm bg-blue-800 hover:bg-blue-900 text-gray-200 mt-2 mb-2" onClick={onSubscribe} disabled>
+                            <CheckIcon className="mr-2" />im Besitz
+                        </Button>
+                    )
                 ) : (
-                    <Button className="w-full text-sm bg-blue-800 hover:bg-blue-900 text-gray-200 mt-2 mb-2" onClick={onSubscribe} disabled>
-                    <CheckIcon className="mr-2" />im Besitz
-                </Button>
-                )
-            ) : (
-                
-                <Button className="w-full text-sm bg-blue-800 hover:bg-blue-900 text-gray-200 mt-2 mb-2" 
-                onClick={onSubscribe}>
-                Vorteile sichern
-            </Button >
-            )}
+
+                    <Button className="w-full text-sm bg-blue-800 hover:bg-blue-900 text-gray-200 mt-2 mb-2"
+                        onClick={onSubscribe}>
+                        Vorteile sichern
+                    </Button >
+                )}
         </>
     );
 }

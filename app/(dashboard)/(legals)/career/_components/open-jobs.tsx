f89673fd1cadd11ae393a4657f632badfb8f72a5
Pascal <zagarolopascal@gmail.com>
2024-05-03 23:01:33 +0200

    career page ui mobile fix

--- app/(dashboard)/(legals)/career/_components/open-jobs.tsx
+++ app/(dashboard)/(legals)/career/_components/open-jobs.tsx
@@ -6,9 +6,9 @@ const OpenJobs = () => {
         <div>
             <div>
                 <h3 className="flex justify-center text-xl font-bold gap-x-2  items-center">
-                <Separator className="w-1/4 bg-[#353535] mr-8 h-[0.5px]" />  
+                <Separator className="w-1/4 bg-[#353535] mr-8 h-[0.5px] hidden sm:block" />  
                 <GrUserWorker className="w-4 h-4" />  Offene Stellen
-                <Separator className="w-1/4 bg-[#353535] ml-8 h-[0.5px]" />
+                <Separator className="w-1/4 bg-[#353535] ml-8 h-[0.5px] hidden sm:block" />
                 </h3>
             </div>
             <div className="text-sm bg-indigo-900 p-8 rounded-md">

a8a1f10a5397408b0457ef960ddd7fd6e081fbde
Pascal <zagarolopascal@gmail.com>
2024-05-04 20:56:36 +0200

    manage reports ui

--- app/admin/reports/_components/accept-inserat-report.tsx
+++ app/admin/reports/_components/accept-inserat-report.tsx
@@ -18,6 +18,7 @@ const AcceptInseratReport : React.FC<AcceptInseratReportProps> = ({
 }) => {
 
     const[isLoading, setIsLoading] = useState(false);
+    const[actionType, setActionType] = useState<"private" | "delete">(null);
 
     const onSubmit = () => {
         try {
@@ -40,20 +41,56 @@ const AcceptInseratReport : React.FC<AcceptInseratReportProps> = ({
                     <h1 className="text-md font-semibold">
                         Verwaltungsoptionen
                     </h1>
+                    <p className="text-xs dark:text-gray-200/60">
+                        ReportId : {thisReport.id}
+                    </p>
                 </div>
-                <div className="mt-4">
-                    <div>
-                        <Checkbox />
-                        <Label className="ml-2">
+                <div className="mt-4 ">
+                    <div className="text-sm">
+                        Gemeldetes Inserat
+                    </div>
+                    <div className=" text-rose-600 font-bold">
+                    {thisReport?.inserat?.title}
+                    </div>
+                </div>
+                <div className="mt-4 ">
+                    <div className="text-sm">
+                        Grund
+                    </div>
+                    <div className=" text-rose-600 font-bold">
+                    {thisReport?.reportType}
+                    </div>
+                </div>
+                <div className="mt-4 ">
+                    <Label className="text-sm font-medium">
+                        Aktion
+                    </Label>
+                    <div  className="flex items-center text-sm mt-2">
+                        <Checkbox 
+                        onCheckedChange={(e) => setActionType("private")}
+                        checked={actionType === "private"}
+                        />
+                        <div className="ml-2 hover:cursor-pointer" onClick={() => {setActionType("private")}}>
                             Inserat privat schalten
-                        </Label>
+                        </div>
                     </div>
-                    <div className="mt-2">
-                        <Checkbox />
-                        <Label className="ml-2">
+                    <div className="mt-2  flex items-center text-sm"  >
+                        <Checkbox 
+                        onCheckedChange={(e) => setActionType("delete")}
+                        checked={actionType === "delete"}
+                        />
+                        <div className="ml-2 w-content hover:cursor-pointer" onClick={() => {setActionType("delete")}}>
                             Inserat löschen
-                        </Label>
+                        </div>
                     </div>
+                </div>
+                <div className="mt-2 w-full  flex justify-end"> 
+                    <Button className="bg-rose-600 hover:bg-rose-700 text-gray-200 font-semibold hover:text-gray-300" size="sm">
+                        Bestätigen
+                    </Button>
+                    <Button className="" variant="ghost" size="sm">
+                        Abbrechen
+                    </Button>
                 </div>
                 </div>
             </DialogContent>

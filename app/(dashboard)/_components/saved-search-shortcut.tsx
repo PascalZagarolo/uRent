361d3fb1b02d8847642ff471e6e9008aa2ab6136
Pascal <zagarolopascal@gmail.com>
2024-05-05 17:53:34 +0200

    click on saved search

--- app/(dashboard)/_components/saved-search-shortcut.tsx
+++ app/(dashboard)/_components/saved-search-shortcut.tsx
@@ -1,7 +1,12 @@
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogTrigger } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { savedSearch } from "@/db/schema";
+import { X } from "lucide-react";
+import { useRouter } from "next/navigation";
+
 import { MdContentPasteSearch, MdManageSearch } from "react-icons/md";
+import { RiShareForward2Fill } from "react-icons/ri";
 
 
 interface SavedSearchesShortCutProps {
@@ -12,6 +17,13 @@ const SavedSearchShortCut : React.FC<SavedSearchesShortCutProps> = ({
     savedSearches
 }) => {
 
+    const router = useRouter();
+
+    const onClick = (usedUrl : string) => {
+        router.push(usedUrl);
+    }
+
+    
     
     return (
         <Dialog>
@@ -30,8 +42,48 @@ const SavedSearchShortCut : React.FC<SavedSearchesShortCutProps> = ({
                     <p className="text-xs dark:text-gray-200/60">
                         Deine gespeicherten Suchen, damit du sie schnell wiederfindest.
                     </p>
-                    <div className="mt-4">
-
+                    <div className="mt-4 block space-y-2">
+                        {savedSearches?.map((search) => (
+                            <div key={search.id} className="dark:bg-[#171717] rounded-md flex items-center p-4 font-semibold text-sm">
+                                <DialogTrigger className="flex items-center">
+                                <div className="font-semibold hover:cursor-pointer hover:underline" onClick={() => {onClick(search.link)}}>
+                                {search.title}
+                                </div>
+                                <div>
+                                <RiShareForward2Fill className="w-4 h-4 ml-2" onClick={() => {onClick(search.link)}}/>
+                                </div>
+                                </DialogTrigger>
+                                <div className="ml-auto">
+                                    <AlertDialog>
+                                        <AlertDialogTrigger asChild>
+                                            <Button variant="ghost" size="sm" className="ml-2 text-xs text-rose-600">
+                                                Löschen
+                                            </Button>
+                                        </AlertDialogTrigger>
+                                        <AlertDialogContent className="dark:border-none dark:bg-[#191919]">
+                                            <div>
+                                                <div>
+                                                    <h3 className="flex items-center1">
+                                                       <X className="text-rose-600 mr-2 w-4 h-4" /> Möchtest du die Suche wirklich löschen?
+                                                    </h3>
+                                                    <p className="text-xs dark:text-gray-200/60">
+                                                        Gelöschte Suchen können nicht wiederhergestellt werden.
+                                                    </p>
+                                                    <div className="mt-2 flex w-full justify-end">
+                                                        <AlertDialogAction className="bg-rose-600 hover:bg-rose-700 text-gray-200 hover:text-gray-300">
+                                                            Löschen
+                                                        </AlertDialogAction>
+                                                        <AlertDialogCancel className="dark:border-none">
+                                                            Abbrechen
+                                                        </AlertDialogCancel>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </AlertDialogContent>
+                                    </AlertDialog>
+                                </div>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </DialogContent>

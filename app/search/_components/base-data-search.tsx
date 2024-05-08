cc4b8a6881a79efb7f9298691859a45a5e845eab
Pascal <zagarolopascal@gmail.com>
2024-05-08 23:06:49 +0200

    space-y-2 ffix

--- app/search/_components/base-data-search.tsx
+++ app/search/_components/base-data-search.tsx
@@ -6,28 +6,28 @@ import DateSearch from "./base-date/date-search";
 import PriceSearch from "./base-date/price-search";
 
 const BaseDataSearch = () => {
-    return ( 
+    return (
         <div>
             <h3 className="font-semibold text-md flex items-center dark:text-gray-100">
                 Basisdaten
-                <Separator 
-                className="h-[0.5px] dark:bg-gray-100/20 w-2/3 ml-6"
-                
+                <Separator
+                    className="h-[0.5px] dark:bg-gray-100/20 w-2/3 ml-6"
+
                 />
             </h3>
             <div className="w-full sm:flex mt-4 sm:space-x-4 space-y-2">
                 <div className="sm:w-1/3">
-<LocationSearch />
+                    <LocationSearch />
                 </div>
                 <div className="sm:w-1/3">
-<DateSearch />
+                    <DateSearch />
                 </div>
                 <div className="sm:w-1/3">
-<PriceSearch />
+                    <PriceSearch />
                 </div>
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default BaseDataSearch;

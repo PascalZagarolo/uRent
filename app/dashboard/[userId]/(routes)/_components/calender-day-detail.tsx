8c201f7a636268ca9270f1f145ac9a290d27e906
Pascal <zagarolopascal@gmail.com>
2024-05-09 13:27:30 +0200

    visuals..

--- app/dashboard/[userId]/(routes)/_components/calender-day-detail.tsx
+++ app/dashboard/[userId]/(routes)/_components/calender-day-detail.tsx
@@ -66,10 +66,18 @@ const CalenderDayDetail: React.FC<CalenderDayDetailProps> = ({
                         {hour}:00 Uhr
                     </div>
                     <div className="h-full ml-auto w-2/4 flex flex-col">
-                        <div className={cn("h-full w-full p-2", checkBooked(String(hour * 60))) + (checkBooked(String(hour * 60)) ? " bg-rose-800" : "")}>
+                        <div className={cn("h-full w-full p-2", 
+                        checkBooked(String(hour * 60)) ? " bg-rose-800" : "",
+                        checkBooked(String((hour * 60) + 30)) ? "" : "rounded-b-lg",
+                        checkBooked(String((hour * 60) - 30)) ? "" : "rounded-t-lg"
+                        )}>
                             1
                         </div>
-                        <div className={cn("h-full w-full p-2", checkBooked(String((hour * 60) + 30))) + (checkBooked(String((hour * 60) + 30)) ? " bg-rose-800" : "")}>
+                        <div className={cn("h-full w-full p-2", 
+                        checkBooked(String((hour * 60) + 30)) ? " bg-rose-800" : "",
+                        checkBooked(String((hour * 60) + 60)) ? "" : "rounded-b-lg",
+                        checkBooked(String((hour * 60))) ? "" : "rounded-t-lg"
+                        )}>
                             2
                         </div>
                     </div>

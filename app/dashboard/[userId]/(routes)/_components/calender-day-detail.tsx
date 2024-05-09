680e71f090f5999fb349672203a2d10c05dad73b
Pascal <zagarolopascal@gmail.com>
2024-05-09 23:54:48 +0200

    image fix on favourites

--- app/dashboard/[userId]/(routes)/_components/calender-day-detail.tsx
+++ app/dashboard/[userId]/(routes)/_components/calender-day-detail.tsx
@@ -3,6 +3,7 @@ import { booking } from "@/db/schema";
 import { cn } from "@/lib/utils";
 import { format, isSameDay } from "date-fns";
 import { de } from "date-fns/locale";
+import { CheckIcon } from "lucide-react";
 import { useState } from "react";
 import { RiCalendarEventFill } from "react-icons/ri";
 
@@ -64,11 +65,13 @@ const CalenderDayDetail: React.FC<CalenderDayDetailProps> = ({
         const segments = [];
         for (let hour = 8; hour <= 23; hour++) {
             segments.push(
-                <div key={hour} className="dark:bg-[#131313] text-sm flex items-center  dark:border-[#1C1C1C]">
-                    <div className="p-2 text-sm w-2/4">
+                <div key={hour} className="dark:bg-[#131313] text-sm flex items-center  dark:border border-[#191919]">
+                    <div>
+                    <div className="p-2 text-sm w-2/5">
                         {hour}:00 Uhr
                     </div>
-                    <div className="h-full ml-auto w-2/4 flex flex-col">
+                    </div>
+                    <div className="h-full ml-auto w-3/5 flex flex-col">
                         <div className={cn("h-[32px] w-full p-2", 
                         checkBooked(String(hour * 60)) ? " bg-rose-800" : "",
                         checkBooked(String((hour * 60) + 30)) ? "" : "rounded-b-lg",
@@ -84,7 +87,9 @@ const CalenderDayDetail: React.FC<CalenderDayDetailProps> = ({
                         checkBooked(String((hour * 60))) ? "" : "rounded-t-lg"
                         )}>
                             {(!checkBooked(String((hour * 60) + 30)) && checkBooked(String((hour * 60)))) && (
-                                `Verfügbar ab ${hour}:30 Uhr`
+                              <div className="flex">
+                              <CheckIcon className="w-4 h-4 mr-2 text-emerald-600" />  Verfügbar ab {hour}:30 Uhr
+                              </div>
                             )}
                         </div>
                     </div>

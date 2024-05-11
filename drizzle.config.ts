1ad134fb6ec7f69468e67c0b6d4c97fce7769b24
Pascal <zagarolopascal@gmail.com>
2024-05-11 17:12:29 +0200

    ...

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,9 +7,9 @@ export default defineConfig({
     dbCredentials: {
         url: process.env.DATABASE_URL
     },
-    migration: {
+    migrations: {
         table: "./drizzle",
         schema: './db/schema.ts',
-        out :  './drizzle'
+        
     }
 })

d9646c5a7aa546fd430a6164c643baa6d96621e3
Pascal <zagarolopascal@gmail.com>
2024-05-11 17:07:35 +0200

    ...

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -3,11 +3,17 @@ import { drizzle } from 'drizzle-orm/neon-http';
 
 import * as schema from "./schema"
 import { neon } from '@neondatabase/serverless';
+import { defineConfig } from 'drizzle-kit';
+
 
 const connnectionString = process.env.DATABASE_URL;
 
+
+
 const client = neon(connnectionString);
 const db = drizzle(client, { schema });
+
+
 
 
 export default db;

441cdae4711e99d87dc68e280e4ed3cfe8ea5489
Pascal <zagarolopascal@gmail.com>
2024-05-12 20:28:26 +0200

    transferry ..

---
+++ app/(anzeige)/inserat/[inseratId]/_components/transfer-car-advert.tsx
@@ -0,0 +1,31 @@
+import { FaShare } from "react-icons/fa";
+import { MdLocalShipping } from "react-icons/md";
+
+const TransferCarAdvert = () => {
+    return ( 
+        <div className="rounded-md bg-[#1B1D28] text-gray-200 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.4)]">
+            <div className="p-4">
+                <h3 className="text-md font-semibold flex items-center">
+                <MdLocalShipping className="w-4 h-4 mr-2" />    Du willst mehr Komfort?
+                </h3>
+                <div className="mt-1 text-sm">
+                    Lasse das Fahrzeug bis vor deine Haustür liefern.
+                </div>
+                <div className="text-sm mt-1  ">
+                    
+                    <div>
+                    <img 
+                src="/transfery_v1.jpg"
+                className=" object-cover  rounded-md"
+                />
+                    </div>
+                    <div className="text-xs font-medium hover:underline flex items-center hover:cursor-pointer">
+                        Zu unserem Partner <FaShare className="w-4 h-4 ml-2" />
+                    </div>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default TransferCarAdvert;

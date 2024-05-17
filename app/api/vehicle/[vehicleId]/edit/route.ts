d6fcf9a459b5093facdf224a28d3369c6ca4cc64
Pascal <zagarolopascal@gmail.com>
2024-05-17 10:30:34 +0200

    edit vehicle

---
+++ app/api/vehicle/[vehicleId]/edit/route.ts
@@ -0,0 +1,47 @@
+import getCurrentUser from "@/actions/getCurrentUser";
+import db from "@/db/drizzle";
+import { vehicle } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+    req : Request,
+    { params } : { params : { vehicleId : string }}
+) {
+    try {
+
+        const findVehicle = await db.query.vehicle.findFirst({
+            where : (eq(vehicle.id, params.vehicleId)),
+            with : {
+                inserat : {
+                    with : {
+                        user : true
+                    }
+                }
+            }
+        })
+
+        if (!findVehicle) {
+            console.log("Fahrzeug nicht gefunden")
+            return new NextResponse("Fahrzeug nicht gefunden", { status : 404 })
+        }
+
+        const currentUser = await getCurrentUser();
+        //@ts-ignore
+        if(!currentUser || currentUser.id !== findVehicle.inserat.userId) {
+            return new NextResponse("Nicht autorisiert", { status : 403 })
+        }
+
+        const values = await req.json();
+
+        const patchInserat = await db.update(vehicle).set({
+            ...values
+        }).where(eq(vehicle.id, params.vehicleId)).returning();
+
+        return NextResponse.json(patchInserat)
+
+    } catch(error : any) {
+        console.log("Fehler beim bearbeiten" , error);
+        return new NextResponse("Fehler beim bearbeiten", { status : 500 })
+    }
+}
---
+++ app/dashboard/[userId]/(routes)/manage/_components/edit-vehicle.tsx
@@ -0,0 +1,100 @@
+'use client'
+
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { vehicle } from "@/db/schema";
+import { Pencil1Icon } from "@radix-ui/react-icons";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { toast } from "react-hot-toast";
+
+interface EditVehicleProps {
+    thisVehicle: typeof vehicle.$inferSelect;
+}
+
+const EditVehicle: React.FC<EditVehicleProps> = ({
+    thisVehicle
+}) => {
+
+    const [title, setTitle] = useState(thisVehicle.title);
+    const [registration, setRegistration] = useState(thisVehicle.registration);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const router = useRouter();
+
+    const onSave = async () => {
+        try {
+            setIsLoading(true);
+            const values = {
+                title: title,
+                registration: registration
+            }
+
+            await axios.patch(`/api/vehicle/${thisVehicle.id}/edit`, values)
+                .then(() => {
+                    router.refresh();
+                    toast.success("Änderungen gespeichert")
+                })
+        } catch (error: any) {
+            console.log(error);
+            toast.error("Fehler beim Speichern")
+        } finally {
+
+        }
+    }
+
+    return (
+        <Dialog>
+            <DialogTrigger>
+                <Pencil1Icon className="w-4 h-4" />
+            </DialogTrigger>
+            <DialogContent className="dark:bg-[#191919] dark:border-none">
+                <div>
+                    <div>
+                        <h3 className="text-md font-semibold">
+                            Fahrzeug bearbeiten
+                        </h3>
+                    </div>
+                    <div className="mt-4">
+                        <Label>
+                            Titel *
+                        </Label>
+                        <div className="mt-2">
+                            <Input
+                                value={title}
+                                onChange={(e) => setTitle(e.target.value)}
+                                className="dark:border-none dark:bg-[#0F0F0F]"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="mt-4">
+                        <Label>
+                            Kennzeichen
+                        </Label>
+                        <div className="mt-2">
+                            <Input
+                                value={registration}
+                                onChange={(e) => setRegistration(e.target.value)}
+                                placeholder="Kennzeichen.."
+                                className="dark:border-none dark:bg-[#0F0F0F]"
+                            />
+                        </div>
+                    </div>
+                    <div className="mt-4 flex justify-end">
+                        <DialogTrigger asChild>
+                            <Button className="bg-indigo-800 hover:bg-indigo-900 text-gray-200 hover:text-gray-300" onClick={onSave}>
+                                Änderungen speichern
+                            </Button>
+                        </DialogTrigger>
+                    </div>
+                </div>
+            </DialogContent>
+        </Dialog>
+    );
+}
+
+export default EditVehicle;

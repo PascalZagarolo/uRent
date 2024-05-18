ef24fa03be77ae5920dd067aa52a8ba72dbae38c
Pascal <zagarolopascal@gmail.com>
2024-05-18 18:31:08 +0200

    search inserat dashboard

--- app/dashboard/[userId]/(routes)/manage/_components/select-inserat.tsx
+++ app/dashboard/[userId]/(routes)/manage/_components/select-inserat.tsx
@@ -1,13 +1,15 @@
 'use client';
 
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { inserat, vehicle,  } from '../../../../../../db/schema';
+import { inserat, vehicle, } from '../../../../../../db/schema';
 import qs from "query-string";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { Label } from "@/components/ui/label";
+
 import { Input } from "@/components/ui/input";
 ;
-import React from "react";
+import React, { use, useMemo } from "react";
+import { useDebounce } from "@/hooks/use-debounce";
+
 
 interface SelectInseratProps {
     foundInserate: typeof inserat.$inferSelect[]
@@ -25,13 +27,15 @@ const SelectInserat: React.FC<SelectInseratProps> = ({
     const currentInserat = searchParams.get("inseratId")
     const currentVehicle = searchParams.get("vehicleId")
 
+    const [renderedInserate, setRenderedInserate] = React.useState(foundInserate)
+
     const [currentTitle, setCurrentTitle] = React.useState("")
 
     const onClick = (id: string) => {
         //@ts-ignore
         let [firstPart, secondPart] = [null, null]
-        if(id) {
-             [firstPart, secondPart] = id?.split("++");
+        if (id) {
+            [firstPart, secondPart] = id?.split("++");
         }
         console.log("First part:", firstPart);
         console.log("Second part:", secondPart);
@@ -45,7 +49,31 @@ const SelectInserat: React.FC<SelectInseratProps> = ({
         router.push(url)
     }
 
+    const debouncedValue = useDebounce(currentTitle, 100);
 
+    useMemo(() => {
+        if (debouncedValue) {
+            const filteredInserate = foundInserate.filter((inserat) => {
+                return inserat.title.toLowerCase().includes(debouncedValue.toLowerCase())
+            })
+            setRenderedInserate(filteredInserate)
+        } else if(!debouncedValue){
+            setRenderedInserate([]);
+        }
+    }, [debouncedValue])
+
+    const onInseratPopoverClick = (id : string) => {
+        setCurrentTitle("")
+        const url = qs.stringifyUrl({
+            url : pathname,
+            query : {
+                inseratId : id
+            }
+        }, { skipEmptyString: true, skipNull: true })
+
+        router.push(url)
+
+    }
 
     return (
         <>
@@ -57,47 +85,61 @@ const SelectInserat: React.FC<SelectInseratProps> = ({
                 value={
                     currentVehicle ? currentInserat + "++" + currentVehicle : currentInserat
                 }
-                
-            >
-                <div className="flex items-center">
-                    <Input
-                     className="dark:border-none dark:bg-[#141414] rounded-r-none"
-                     placeholder="Inserat suchen..."
-                     value={currentTitle}
-                     onChange={(e) => {setCurrentTitle(e.target.value)}}
-                     />
-                <div>
-                <SelectTrigger className="dark:border-none dark:bg-[#0F0F0F] rounded-l-none"/>
-                    
-                    
-                </div>
-                </div>
-                    <SelectContent className="dark:bg-[#0F0F0F] dark:border-none">
-                        
-                        <SelectItem value={null}>
-                            Beliebig
-                        </SelectItem>
-                        {foundInserate.map((thisInserat) => (
-                            <>
-                                <SelectItem value={thisInserat.id} key={thisInserat.id} 
-                                className="w-[400px]  line-clamp-1 break-all h-[30px]">
-                                    {thisInserat.title}
-                                </SelectItem>
-                                {thisInserat.vehicles.map((vehicle : any) => (
-                                    <SelectItem value={thisInserat.id + "++" + vehicle.id} key={vehicle.id} 
-                                    className="text-xs ml-8"
-                                        onClick={(selectedValue) => {
 
-                                            onClick(thisInserat.id)
-                                        }}
-                                    >
-                                        {vehicle.title}
-                                    </SelectItem>
-                                ))}
-                            </>
-                        ))}
-                    </SelectContent>
-                
+            >
+                <div className="relative">
+                    <div className="flex items-center w-full">
+                        <div className="flex-grow">
+                            <Input
+                                className="dark:border-none dark:bg-[#141414] rounded-r-none text-sm font-normal w-full"
+                                placeholder="Inserat suchen..."
+                                value={currentTitle}
+                                onChange={(e) => setCurrentTitle(e.target.value)}
+                            />
+                        </div>
+                        <div>
+                            <SelectTrigger className="dark:border-none dark:bg-[#0F0F0F] rounded-l-none" />
+                        </div>
+                    </div>
+                    {renderedInserate.length > 0 && (
+                        <div className="absolute w-full bg-white dark:bg-[#191919] text-sm border dark:border-[#141414] rounded-b">
+                            {renderedInserate.map((pInserat) => (
+                                <div key={pInserat.id} 
+                                className="px-4 py-3 hover:bg-gray-200 dark:hover:bg-[#2c2c2c] hover:cursor-pointer"
+                                onClick={() => onInseratPopoverClick(pInserat.id)}
+                                >
+                                    {pInserat.title}
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                </div>
+                <SelectContent className="dark:bg-[#0F0F0F] dark:border-none">
+
+                    <SelectItem value={null}>
+                        Beliebig
+                    </SelectItem>
+                    {foundInserate.map((thisInserat) => (
+                        <>
+                            <SelectItem value={thisInserat.id} key={thisInserat.id}
+                                className="w-[400px]  line-clamp-1 break-all h-[30px]">
+                                {thisInserat.title}
+                            </SelectItem>
+                            {thisInserat.vehicles.map((vehicle: any) => (
+                                <SelectItem value={thisInserat.id + "++" + vehicle.id} key={vehicle.id}
+                                    className="text-xs ml-8"
+                                    onClick={(selectedValue) => {
+
+                                        onClick(thisInserat.id)
+                                    }}
+                                >
+                                    {vehicle.title}
+                                </SelectItem>
+                            ))}
+                        </>
+                    ))}
+                </SelectContent>
+
             </Select>
         </>
     );

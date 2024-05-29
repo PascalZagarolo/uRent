916d51034ce626084fe32a11874d37d0115c2e4c
Pascal <zagarolopascal@gmail.com>
2024-05-29 15:58:15 +0200

    transport weightclass fix

--- app/search/_components/_transport/transport-weight-search.tsx
+++ app/search/_components/_transport/transport-weight-search.tsx
@@ -16,6 +16,7 @@ const TransportWeightClassSearch = () => {
     const currentObject: any = useSavedSearchParams((state) => state.searchParams)
 
     const [currentAge, setCurrentAge] = useState(currentObject['weightClass']);
+    const [currentEnd, setCurrentEnd] = useState(currentObject['weightClassMax']);
     const [isLoading, setIsLoading] = useState(false);
 
     const { searchParams, changeSearchParams, deleteSearchParams } = useSavedSearchParams();
@@ -30,9 +31,20 @@ const TransportWeightClassSearch = () => {
         console.log(selectedValue)
     }
 
+    const onSubmitEnd = (selectedValue: string) => {
+        changeSearchParams("weightClassMax", selectedValue);
+        setCurrentEnd(selectedValue)
+        
+    }
+
     const deleteWeight = () => {
         deleteSearchParams("weightClass");
         setCurrentAge(null)
+    }
+
+    const deleteWeightEnd = () => {
+        deleteSearchParams("weightClassMax");
+        setCurrentEnd(null)
     }
 
     function removeUnderscore(inputString: string): string {
@@ -47,7 +59,9 @@ const TransportWeightClassSearch = () => {
                     <p className="ml-2 font-semibold"> Gewichtsklasse </p>
                 </Label>
 
-                <Select
+                <div className="w-full flex items-center gap-x-2">
+                    <div className="w-1/2">
+                    <Select
                     onValueChange={(brand) => {
                         !brand  ? deleteWeight() : onSubmit(brand)
                     }}
@@ -58,7 +72,7 @@ const TransportWeightClassSearch = () => {
                     <SelectTrigger className="dark:bg-[#151515] dark:border-gray-200 dark:border-none focus-visible:ring-0 mt-2 rounded-md "
                         disabled={isLoading}  >
                         <SelectValue
-                            placeholder="Wie viel Gewicht?"
+                            placeholder="Von"
 
 
                         />
@@ -75,6 +89,39 @@ const TransportWeightClassSearch = () => {
                         
                     </SelectContent>
                 </Select>
+                    </div>
+                    <div className="w-1/2">
+                    <Select
+                    onValueChange={(brand) => {
+                        !brand  ? deleteWeightEnd() : onSubmitEnd(brand)
+                    }}
+                    value={currentEnd}
+                    disabled={isLoading}
+                >
+
+                    <SelectTrigger className="dark:bg-[#151515] dark:border-gray-200 dark:border-none focus-visible:ring-0 mt-2 rounded-md "
+                        disabled={isLoading}  >
+                        <SelectValue
+                            placeholder="Bis"
+
+
+                        />
+                    </SelectTrigger>
+
+                    <SelectContent className="dark:bg-[#000000] border-white dark:border-none w-full">
+                        <SelectItem key="beliebig" value={null} className="font-semibold">
+                            Beliebig
+                        </SelectItem>
+                        <SelectItem value="280">2,8 t</SelectItem>
+                        <SelectItem value="350">3,5 t</SelectItem>
+                        <SelectItem value="550">5,5 t</SelectItem>
+                        
+                        
+                    </SelectContent>
+                </Select>
+                    </div>
+                </div>
+
             </div>
         </div>
     );

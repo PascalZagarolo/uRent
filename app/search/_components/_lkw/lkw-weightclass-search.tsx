9f07383a55916ccf292f5663faa8627dbf31a9cb
Pascal <zagarolopascal@gmail.com>
2024-05-29 16:09:25 +0200

    lkw weightclass..

--- app/search/_components/_lkw/lkw-weightclass-search.tsx
+++ app/search/_components/_lkw/lkw-weightclass-search.tsx
@@ -16,6 +16,7 @@ const LkwWeightClassSearch = () => {
     const currentObject: any = useSavedSearchParams((state) => state.searchParams)
 
     const [currentAge, setCurrentAge] = useState(currentObject['weightClass']);
+    const [currentEnd, setCurrentEnd] = useState(currentObject['weightClassMax']);
     const [isLoading, setIsLoading] = useState(false);
 
     const { searchParams, changeSearchParams, deleteSearchParams } = useSavedSearchParams();
@@ -35,6 +36,17 @@ const LkwWeightClassSearch = () => {
         setCurrentAge(null)
     }
 
+    const onSubmitEnd = (selectedValue: string) => {
+        changeSearchParams("weightClassMax", selectedValue);
+        setCurrentEnd(selectedValue)
+        console.log(selectedValue)
+    }
+
+    const deleteWeightEnd = () => {
+        deleteSearchParams("weightClassMax");
+        setCurrentEnd(null)
+    }
+
     function removeUnderscore(inputString: string): string {
         const outputString = inputString.replace(/_/g, ' ');
         return outputString;
@@ -47,7 +59,9 @@ const LkwWeightClassSearch = () => {
                     <p className="ml-2 font-semibold"> Gewichtsklasse </p>
                 </Label>
 
-                <Select
+                <div className="flex items-center gap-x-2">
+                    <div className="w-1/2">
+                    <Select
                     onValueChange={(brand) => {
                         !brand  ? deleteWeight() : onSubmit(brand)
                     }}
@@ -58,7 +72,7 @@ const LkwWeightClassSearch = () => {
                     <SelectTrigger className="dark:bg-[#151515] dark:border-gray-200 dark:border-none focus-visible:ring-0 mt-2 rounded-md "
                         disabled={isLoading}  >
                         <SelectValue
-                            placeholder="Wie viel Gewicht?"
+                            placeholder="Von"
 
 
                         />
@@ -80,6 +94,43 @@ const LkwWeightClassSearch = () => {
                         <SelectItem value="5000">{'>'} 39 t</SelectItem>
                     </SelectContent>
                 </Select>
+                    </div>
+                    <div className="w-1/2">
+                    <Select
+                    onValueChange={(brand) => {
+                        !brand  ? deleteWeightEnd() : onSubmitEnd(brand)
+                    }}
+                    value={currentEnd}
+                    disabled={isLoading}
+                >
+
+                    <SelectTrigger className="dark:bg-[#151515] dark:border-gray-200 dark:border-none focus-visible:ring-0 mt-2 rounded-md "
+                        disabled={isLoading}  >
+                        <SelectValue
+                            placeholder="Bis"
+
+
+                        />
+                    </SelectTrigger>
+
+                    <SelectContent className="dark:bg-[#000000] border-white dark:border-none w-full">
+                        <SelectItem key="beliebig" value={null} className="font-semibold">
+                            Beliebig
+                        </SelectItem>
+                        
+                        <SelectItem value="350">3,5 t</SelectItem>
+                        <SelectItem value="550">5,5 t</SelectItem>
+                        <SelectItem value="750">7,5 t</SelectItem>
+                        <SelectItem value="1200">12 t</SelectItem>
+                        <SelectItem value="1800">18 t</SelectItem>
+                        <SelectItem value="2600">26 t</SelectItem>
+                        <SelectItem value="3200">32 t</SelectItem>
+                        <SelectItem value="3900">39 t</SelectItem>
+                        <SelectItem value="5000">{'>'} 39 t</SelectItem>
+                    </SelectContent>
+                </Select>
+                    </div>
+                </div>
             </div>
         </div>
     );

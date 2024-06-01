e47b93ed53a944871e437728ef342450896888ab
Pascal <zagarolopascal@gmail.com>
2024-06-01 20:26:01 +0200

    faqs/booking v1

--- app/(dashboard)/(legals)/faqs/bookings/page.tsx
+++ app/(dashboard)/(legals)/faqs/bookings/page.tsx
@@ -1,7 +1,7 @@
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 
 const FaqBookings = () => {
-    return ( 
+    return (
         <div>
 
             <div className="flex justify-center sm:p-8 bg-[#404040]/10">
@@ -29,11 +29,33 @@ const FaqBookings = () => {
                                     </BreadcrumbList>
                                 </Breadcrumb>
                             </div>
-                            <div className="p-4"> 
+                            <div className="p-4">
                                 <div>
-                                    <h3>
-                                        
+                                    <h3 className="text-md font-semibold">
+                                        Das Buchungssystem & Sichtbarkeit
                                     </h3>
+                                    <div className=" text-sm text-gray-200/90">
+
+                                        Das Buchungssystem macht es dir kinderleicht, Buchungen und Verfügbarkeiten hinzuzufügen, und hilft dir,
+                                        stets den Überblick zu bewahren. Es ermöglicht deinen Mietern,
+                                        die Verfügbarkeit deiner Fahrzeuge einzusehen, ohne redundante Anfragen stellen zu müssen,
+                                        und spart dir dadurch wertvolle Zeit. 
+                                        Die einfache Bedienung sorgt dafür, dass du den Kopf für andere Aufgaben frei hast.
+                                        So wird die Verwaltung deiner Fahrzeuge effizienter, kundenfreundlicher .
+                                    </div>
+                                </div>
+
+                                <div className="mt-16">
+                                    <h3 className="text-md font-semibold">
+                                        Mehrfachinserate & Fahrzeuge
+                                    </h3>
+                                    <div className=" text-sm text-gray-200/90">
+                                    Bei mehreren identischen Fahrzeugen kann ein Flotteninserat erstellt werden, 
+                                    um die Verwaltung zu vereinfachen. Fahrzeuge ermöglichen es, die einzelnen Flotteninserate im Detail zu verwalten 
+                                    und den Überblick zu behalten, da jedes Fahrzeug seine eigenen Buchungen und Verfügbarkeiten haben kann. 
+                                    Der Mieter sieht dabei nur die generelle Verfügbarkeit, unabhängig vom einzelnen Fahrzeug. Dies macht es für den Vermieter leicht, 
+                                    Verfügbarkeiten und Buchungen zu verwalten, und für den Mieter einfach, passende Inserate zu finden.
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -42,7 +64,7 @@ const FaqBookings = () => {
                 </div>
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default FaqBookings;

78ea899dec19934f2969fcd9ef06fa6bdc005cf7
Pascal <zagarolopascal@gmail.com>
2024-06-01 13:51:59 +0200

    heads up on dashboard

---
+++ app/(dashboard)/(legals)/faqs/bookings/page.tsx
@@ -0,0 +1,48 @@
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
+
+const FaqBookings = () => {
+    return ( 
+        <div>
+
+            <div className="flex justify-center sm:p-8 bg-[#404040]/10">
+
+                <div className="sm:w-[1044px] w-full dark:bg-[#1c1c1c] rounded-md bg-white">
+                    <div className="min-h-screen">
+                        <div className="sm:p-4 p-2 mt-4  rounded-lg ">
+                            <h3 className="dark:text-gray-100 text-2xl font-semibold flex items-center">
+                                Mieter - Häufig gestellte Fragen
+                            </h3>
+                            <div className="pb-2">
+                                <Breadcrumb>
+                                    <BreadcrumbList>
+                                        <BreadcrumbItem>
+                                            <BreadcrumbLink href="/">Home</BreadcrumbLink>
+                                        </BreadcrumbItem>
+                                        <BreadcrumbSeparator />
+                                        <BreadcrumbItem>
+                                            <BreadcrumbLink href="/faqs">Faqs</BreadcrumbLink>
+                                        </BreadcrumbItem>
+                                        <BreadcrumbSeparator />
+                                        <BreadcrumbItem>
+                                            <BreadcrumbPage>Buchungssystem</BreadcrumbPage>
+                                        </BreadcrumbItem>
+                                    </BreadcrumbList>
+                                </Breadcrumb>
+                            </div>
+                            <div className="p-4"> 
+                                <div>
+                                    <h3>
+                                        
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default FaqBookings;
---
+++ app/dashboard/[userId]/(routes)/manage/_components/info-heads-up.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { RocketIcon, X } from "lucide-react";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+import { MdOutlineInfo } from "react-icons/md";
+
+const InfoHeadsUp = () => {
+
+    const router = useRouter();
+
+    const onDecline = () => {
+        toast.success("Hinweis ausgeblendet.");
+        localStorage.setItem("declineInfo", "true");
+        declinedInfo = true;
+        router.refresh();
+        
+    }
+
+    let declinedInfo = localStorage.getItem("declineInfo") ? true : false;
+
+    if (declinedInfo) {
+        return null;
+    }
+
+    return (
+        <div className="px-4">
+            <Alert className="px-4 dark:bg-indigo-900 dark:border-none">
+                <MdOutlineInfo  className="h-4 w-4" />
+                <AlertTitle className="flex items-center">Weitere Informationen
+                    <div className="ml-auto hover:cursor-pointer" onClick={onDecline}>
+                        <X className="text-gray-200 w-4 h-4" />
+                    </div></AlertTitle>
+                <AlertDescription>
+                    Falls du weitere Informationen benötigst, oder eine kurze Einführung zum Buchungssystem wünschst,
+                    schau dir die <a href="/faqs/bookings" target="_blank" className="underline">FAQs</a> an.
+                </AlertDescription>
+            </Alert>
+        </div>
+    );
+}
+
+export default InfoHeadsUp;

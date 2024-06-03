55c18c37f35623ea7e80987c9abb2b03a9117d06
Pascal <zagarolopascal@gmail.com>
2024-06-03 19:37:02 +0200

    faq v1

--- app/profile/[profileId]/_components/business-faqs.tsx
+++ app/profile/[profileId]/_components/business-faqs.tsx
@@ -1,6 +1,8 @@
 import { FaQuestionCircle, FaQuidditch } from "react-icons/fa";
 import AddBusinessFaqs from "./add-business-faqs";
 import { business } from "@/db/schema";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import { MdOutlineQuestionAnswer } from "react-icons/md";
 
 interface BusinessFaqsProps {
     thisBusiness : typeof business.$inferSelect;
@@ -21,13 +23,19 @@ const BusinessFaqs : React.FC<BusinessFaqsProps> = ({
                   </div>
                 </h3>
             </div>
-            <div className="">
+            <Accordion type="single" collapsible className="p-4">
+            
                 {thisBusiness?.faqs?.map((faq, index) => (
-                    <div key={faq.id}>
-
-                    </div>
+                    <AccordionItem value={`${index}`} className="border-none">
+                    <AccordionTrigger 
+                    className="dark:bg-[#1c1c1c]  p-4 text-sm font-semibold">
+                        <div><MdOutlineQuestionAnswer className="w-4 h-4 mr-2"/> </div>{faq.question}</AccordionTrigger>
+                    <AccordionContent className="p-4 border-none  dark:bg-[#1c1c1c]">
+                    {faq.answer}
+                    </AccordionContent>
+                  </AccordionItem>
                 ))}
-            </div>
+            </Accordion>
         </div>
      );
 }

841bbe5b7cdcefc2558dc4f49e239b9668bfb652
Pascal <zagarolopascal@gmail.com>
2024-06-03 10:05:38 +0200

    added topics on bookingspüage

--- app/(dashboard)/(legals)/faqs/bookings/page.tsx
+++ app/(dashboard)/(legals)/faqs/bookings/page.tsx
@@ -32,29 +32,73 @@ const FaqBookings = () => {
                             <div className="p-4">
                                 <div>
                                     <h3 className="text-md font-semibold">
-                                        Das Buchungssystem & Sichtbarkeit
+                                    Der BuchungskalenderDer Buchungskalender
                                     </h3>
                                     <div className=" text-sm text-gray-200/90">
 
-                                        Das Buchungssystem macht es dir kinderleicht, Buchungen und Verfügbarkeiten hinzuzufügen, und hilft dir,
-                                        stets den Überblick zu bewahren. Es ermöglicht deinen Mietern,
-                                        die Verfügbarkeit deiner Fahrzeuge einzusehen, ohne redundante Anfragen stellen zu müssen,
-                                        und spart dir dadurch wertvolle Zeit. 
-                                        Die einfache Bedienung sorgt dafür, dass du den Kopf für andere Aufgaben frei hast.
-                                        So wird die Verwaltung deiner Fahrzeuge effizienter, kundenfreundlicher .
+                                    Neben der Verwaltung aller Fahrzeuge, dem Eintragen von Buchungen hast Du vor allem 
+                                    die Möglichkeit, 
+                                    dir alle Daten strukturiert anzeigen zu lassen. Wenn Du ein Inserat, bzw. ein Fahrzeug 
+                                    bei einem Flotteninserat ausgewählt hast, 
+                                    werden alle eingegebenen Buchungsdaten in der Monatsansicht und Tagesansicht dargestellt. 
+                                    Darüber hinaus kann durch klicken auf die jeweiligen Buchungen eine 
+                                    detaillierte Übersicht geöffnet werden. Du hast also die Möglichkeit, 
+                                    Dein Mietgeschehen immer im Blick zu behalten. 
                                     </div>
                                 </div>
 
-                                <div className="mt-16">
+                                <div className="mt-8">
                                     <h3 className="text-md font-semibold">
-                                        Mehrfachinserate & Fahrzeuge
+                                    Einfluss von Verfügbarkeiten und Buchungen auf die Inserats Darstellung
+                                    </h3>
+                                    <div className=" text-sm text-gray-200/90">
+                                    Ein großer Vorteil beim Eintragen von Verfügbarkeiten ist die Übersicht für den Mieter. 
+                                    Das regelmäßige Verwalten von Buchungen und Verfügbarkeiten dient nämlich nicht nur der persönlichen Darstellung 
+                                    und Übersicht, sondern hat auch Einfluss auf die Darstellung des Fahrzeugs im entsprechenden Inserat. 
+                                    Potentielle Mieter kriegen dort in Form eines Verfügbarkeitskalenders alle verfügbaren Tage angezeigt. 
+                                    So wird der Mietprozess deutlich verkürzt und Anfragen zu belegten Tagen werden vermieden. Wenn Flotteninserate verwaltet werden, 
+                                    wird dem Mieter automatisch, wenn möglich ein verfügbares Fahrzeug angezeigt.
+                                    </div>
+                                </div>
+
+                                <div className="mt-8">
+                                    <h3 className="text-md font-semibold">
+                                    Der Unterschied zwischen Buchungen und Verfügbarkeiten
+                                    </h3>
+                                    <div className=" text-sm text-gray-200/90">
+                                    Buchungen und Verfügbarkeiten haben zunächst den selben Einfluss auf die Darstellung der Verfügbarkeit 
+                                    im entsprechenden Inserat für den Mieter. Die Unterscheidung liegt in der persönlichen Darstellung. 
+                                    Der Vorteil beim Erstellen von Buchungen ist, dass hier deutlich mehr Informationen beispielsweise zum Mieter 
+                                    und zum Fahrzeug eingetragen werden können, welche dann zusätzlich 
+                                    zur Verfügbarkeit im persönlichen Buchungskalender einsehbar sind. Das Erstellen von Verfügbarkeiten 
+                                    dient hingegen dem lediglich schnellen Eintragen der Zeitspanne, in der ein Fahrzeug nicht verfügbar ist. 
+                                    </div>
+                                </div>
+
+                                <div className="mt-8">
+                                    <h3 className="text-md font-semibold">
+                                    Flotteninserate & Fahrzeuge
                                     </h3>
                                     <div className=" text-sm text-gray-200/90">
                                     Bei mehreren identischen Fahrzeugen kann ein Flotteninserat erstellt werden, 
-                                    um die Verwaltung zu vereinfachen. Fahrzeuge ermöglichen es, die einzelnen Flotteninserate im Detail zu verwalten 
-                                    und den Überblick zu behalten, da jedes Fahrzeug seine eigenen Buchungen und Verfügbarkeiten haben kann. 
-                                    Der Mieter sieht dabei nur die generelle Verfügbarkeit, unabhängig vom einzelnen Fahrzeug. Dies macht es für den Vermieter leicht, 
-                                    Verfügbarkeiten und Buchungen zu verwalten, und für den Mieter einfach, passende Inserate zu finden.
+                                    um die Verwaltung zu vereinfachen. Flotteninserate ermöglichen es, die einzelnen Fahrzeuge im Detail 
+                                    zu verwalten und gleichzeitig nur ein Inserat zu erstellen. Zudem hilft es, stehts den Überblick zu behalten, 
+                                    da jedes Fahrzeug seine eigenen Buchungen und Verfügbarkeiten haben kann. Dem Mieter wird dabei automatisch, 
+                                    wenn möglich immer ein Verfügbares Fahrzeug angezeigt. Dies vereinfacht 
+                                    das regelmäßige Verwalten von Verfügbarkeiten und Buchungen und verkürzt den Mietprozess, 
+                                    da potentielle Mieter zeitlich passende Anfragen stellen. 
+                                    </div>
+                                </div>
+
+                                <div className="mt-8">
+                                    <h3 className="text-md font-semibold">
+                                    Die Verwaltung von Anfragen
+                                    </h3>
+                                    <div className=" text-sm text-gray-200/90">
+                                    Um den Mietprozess noch weiter zu verkürzen, hat der Mieter die Möglichkeit Buchungsanfragen zu stellen. 
+                                    Diese können entweder im Chat oder direkt auf der Buchungs-Seite eingesehen werden. 
+                                    Auf der Buchungsseite ergibt sich der große Vorteil, 
+                                    Schnellanfragen direkt einzusehen, eine passende auszuwählen und diese anschließend direkt zu verwalten. 
                                     </div>
                                 </div>
                             </div>

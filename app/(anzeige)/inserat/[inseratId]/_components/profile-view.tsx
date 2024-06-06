0f855098cdfa5d7d7ee9df952d1b891fc1c8d873
Pascal <zagarolopascal@gmail.com>
2024-06-06 19:34:56 +0200

    enterprise..

--- app/(anzeige)/inserat/[inseratId]/_components/profile-view.tsx
+++ app/(anzeige)/inserat/[inseratId]/_components/profile-view.tsx
@@ -3,18 +3,19 @@
 
 
 import { Button } from "@/components/ui/button";
-import { business,  userTable } from "@/db/schema";
+import { business, userTable } from "@/db/schema";
 import axios from "axios";
 import { format } from "date-fns";
 import { AlignCenter, BuildingIcon, Globe2Icon, MailIcon, MapPinIcon, PhoneIcon, User2, UserIcon } from "lucide-react";
 import Image from "next/image";
 import { useParams, useRouter } from "next/navigation";
 import { useEffect, useMemo, useState } from "react";
-import { MdCardMembership } from "react-icons/md";
+import { MdCardMembership, MdOutlineStarRate } from "react-icons/md";
 import { businessAddress } from '../../../../../db/schema';
 import { GiTargetPoster } from "react-icons/gi";
 import { FaFax } from "react-icons/fa6";
 import { Separator } from "@radix-ui/react-separator";
+import { cn } from "@/lib/utils";
 
 interface ProfileViewProps {
     thisUser: typeof userTable.$inferSelect;
@@ -34,24 +35,30 @@ const ProfileView: React.FC<ProfileViewProps> = ({
     const params = useParams();
 
 
-    
 
-   
-    useMemo(() => {
-        axios.patch(`/api/inserat/${params.inseratId}/view`);
-    },[]) 
 
+    /* 
+     useMemo(() => {
+         axios.patch(`/api/inserat/${params.inseratId}/view`);
+     },[]) 
+ */
     const router = useRouter();
 
     return (
-        <div className="px-4 py-4 bg-[#171923] xl:min-w-[360px] xl:w-3/5 w-full 
-          sm:rounded-md 
-         dark:from-[#2a3046] dark:via-[#23283b] dark:to-[#191d2a]
-         ">
 
+
+        <div className={cn(`px-4 py-4 bg-[#171923] xl:min-w-[360px] xl:w-3/5 w-full 
+        sm:rounded-md 
+       dark:from-[#2a3046] dark:via-[#23283b] dark:to-[#191d2a]
+       `, thisUser?.subscription?.subscriptionType === "ENTERPRISE" && "border-2 border-indigo-800")}>
+            {thisUser?.subscription?.subscriptionType === "ENTERPRISE" && (
+                <div className="ml-auto  bg-indigo-800 p-2 rounded-md flex justify-center items-center text-md font-semibold">
+                 <MdOutlineStarRate className="w-4 h-4 mr-2"/>   ENTERPRISE <MdOutlineStarRate className="w-4 h-4 ml-2"/> 
+                </div>
+            )}
             {thisBusiness && (
                 <div className="bg-[#171923]  p-4 text-sm font-semibold flex">
-                  <BuildingIcon className="w-4 h-4 mr-2" />  Gewerblicher Händler
+                    <BuildingIcon className="w-4 h-4 mr-2" />  Gewerblicher Händler
                 </div>
             )}
 
@@ -67,18 +74,18 @@ const ProfileView: React.FC<ProfileViewProps> = ({
                     <div className="font-semibold ml-2 flex items-center text-md w-3/4 truncate">
                         {thisUser.name}
                     </div>
-                    
+
 
                 </div>
                 {(thisUser?.sharesRealName && (thisUser?.vorname || thisUser?.nachname)) && (
                     <div className="text-sm text-gray-200 font-semibold flex items-center mt-1">
-                  <UserIcon className="w-4 h-4 mr-2" />  {thisUser.vorname} {thisUser.nachname}
-                </div>
+                        <UserIcon className="w-4 h-4 mr-2" />  {thisUser.vorname} {thisUser.nachname}
+                    </div>
                 )}
                 {thisUser?.sharesEmail && (
                     <div className="text-sm text-gray-200 font-medium flex items-center mt-1">
-                  <MailIcon className="w-4 h-4 mr-2" />  {thisUser.email}
-                </div>
+                        <MailIcon className="w-4 h-4 mr-2" />  {thisUser.email}
+                    </div>
                 )}
                 <div>
                     <div>
@@ -100,19 +107,19 @@ const ProfileView: React.FC<ProfileViewProps> = ({
                         <MapPinIcon className="w-4 h-4 text-rose-600" />Firmenstandort
                     </h1>
                     <div >
-                        {thisBusiness?.businessAddresses?.map((business : typeof businessAddress.$inferSelect) => (
+                        {thisBusiness?.businessAddresses?.map((business: typeof businessAddress.$inferSelect) => (
                             business.isPrimary && (
                                 <div key={business.id}>
 
                                     <div className="mt-2">
                                         {business?.image ? (
                                             <Image
-                                            src={business?.image}
-                                            width={600}
-                                            height={600}
-                                            className="w-full rounded-none h-[80px] object-cover"
-                                            alt="Business-Bild"
-                                        />
+                                                src={business?.image}
+                                                width={600}
+                                                height={600}
+                                                className="w-full rounded-none h-[80px] object-cover"
+                                                alt="Business-Bild"
+                                            />
                                         ) : (
                                             <div className="h-[80px] bg-[#161923]">
                                                 <p className="flex h-full justify-center items-center text-gray-200/70 text-xs">Kein Bild vorhanden</p>
@@ -134,42 +141,42 @@ const ProfileView: React.FC<ProfileViewProps> = ({
             {thisBusiness && (
                 <div className="bg-[#1b1d28] p-4 w-full rounded-md mt-2">
                     <h1 className="w-full text-sm font-medium flex">
-                        Weitere Kontaktdaten 
+                        Weitere Kontaktdaten
                     </h1>
                     <Separator
-                     className="w-full bg-[#1b1d28] py-1 h-[0.5px]"
+                        className="w-full bg-[#1b1d28] py-1 h-[0.5px]"
                     />
                     <div>
 
-                    <div className="w-full flex text-xs items-center">
-                       <Globe2Icon className="w-4 h-4 mr-2" /> Website  
-                    <div className="text-xs mt-2 w-full ml-auto flex justify-end font-semibold">
-                            {thisBusiness?.website ? thisBusiness?.website : "-"}
+                        <div className="w-full flex text-xs items-center">
+                            <Globe2Icon className="w-4 h-4 mr-2" /> Website
+                            <div className="text-xs mt-2 w-full ml-auto flex justify-end font-semibold">
+                                {thisBusiness?.website ? thisBusiness?.website : "-"}
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="w-full flex text-xs items-center">
-                       <MailIcon className="w-4 h-4 mr-2" /> Email  
-                    <div className="text-xs mt-2 w-full ml-auto flex justify-end font-semibold">
-                            {thisBusiness?.email ? thisBusiness?.email : "-"}
+                        <div className="w-full flex text-xs items-center">
+                            <MailIcon className="w-4 h-4 mr-2" /> Email
+                            <div className="text-xs mt-2 w-full ml-auto flex justify-end font-semibold">
+                                {thisBusiness?.email ? thisBusiness?.email : "-"}
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="w-full flex text-xs items-center">
-                       <PhoneIcon className="w-4 h-4 mr-2" /> Tel.  
-                    <div className="text-xs mt-2 w-full ml-auto flex justify-end font-semibold">
-                    {thisBusiness?.telephone_number ? thisBusiness?.telephone_number : "-"}
+                        <div className="w-full flex text-xs items-center">
+                            <PhoneIcon className="w-4 h-4 mr-2" /> Tel.
+                            <div className="text-xs mt-2 w-full ml-auto flex justify-end font-semibold">
+                                {thisBusiness?.telephone_number ? thisBusiness?.telephone_number : "-"}
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="w-full flex text-xs items-center">
-                       <FaFax  className="w-4 h-4 mr-2" /> Fax  
-                    <div className="text-xs mt-2 w-full ml-auto flex justify-end font-semibold">
-                            {thisBusiness?.fax ? thisBusiness?.fax : "-"}
+                        <div className="w-full flex text-xs items-center">
+                            <FaFax className="w-4 h-4 mr-2" /> Fax
+                            <div className="text-xs mt-2 w-full ml-auto flex justify-end font-semibold">
+                                {thisBusiness?.fax ? thisBusiness?.fax : "-"}
+                            </div>
                         </div>
-                    </div>
                         <div className="text-xs text-gray-200/60 mt-2">
-                           * Kontaktdaten aus dem Profil können von den Kontakdaten des Inserats abweichen.
+                            * Kontaktdaten aus dem Profil können von den Kontakdaten des Inserats abweichen.
                         </div>
                     </div>
                 </div>
@@ -182,6 +189,7 @@ const ProfileView: React.FC<ProfileViewProps> = ({
                 </Button>
             </div>
         </div>
+
     );
 }
 

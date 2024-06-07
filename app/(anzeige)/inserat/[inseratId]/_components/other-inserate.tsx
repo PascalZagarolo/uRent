c8519672c4b119ac4cffb31be3fad24610c96e33
Pascal <zagarolopascal@gmail.com>
2024-06-07 15:53:52 +0200

    centre of inserat page

--- app/(anzeige)/inserat/[inseratId]/_components/other-inserate.tsx
+++ app/(anzeige)/inserat/[inseratId]/_components/other-inserate.tsx
@@ -21,7 +21,7 @@ const OtherInserate: React.FC<OtherInserateProps> = ({
         <div className="xl:w-3/5 xl:min-w-[360px] bg-[#171923] px-4 py-4 rounded-md ">
             <h3 className=" flex gap-x-2 font-medium text-gray-200">
                 <IoIosPaper className="w-4 h-4" />Weiteres von
-                <div className="w-3/5 truncate font-semibold">{thisUser?.name}</div>
+                <div className="w-3/5 line-clamp-1 font-semibold">{thisUser?.name}</div>
             </h3>
             <div className="px-2 mt-2">
                 <Separator className="w-full bg-[#393c53] h-[0.5px]" />

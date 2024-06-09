1e61ea13918a9e77c0d35ee36d5a378e4b2b1827
Pascal <zagarolopascal@gmail.com>
2024-06-09 22:26:49 +0200

    ...

--- app/(dashboard)/_components/login-bar-header.tsx
+++ app/(dashboard)/_components/login-bar-header.tsx
@@ -9,7 +9,7 @@ const LoginBarHeader = () => {
     return (
         <div className="flex justify-start  items-center mt-4 mr-16">
             <Button variant="ghost"  className="outline outline-offset-1 outline-1 mr-4 bg-white 
-            drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] border-2 border-gray-300
+            drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.4)] border-2 border-gray-300
             dark:bg-slate-800 dark:border-none  dark:hover:bg-slate-700 dark:outline-none
             " asChild>
                 <Link href="/login">
@@ -18,7 +18,7 @@ const LoginBarHeader = () => {
             </Button>
             <p className="text-gray-200 font-bold hidden 2xl:flex"> oder </p>
             <Button variant="ghost"  className=" 2xl:flex hidden outline outline-offset-1 outline-1 ml-4 bg-white 
-            drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] border-2 border-gray-300
+            drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.4)] border-2 border-gray-300
             dark:bg-slate-800 dark:border-none dark:hover:bg-slate-700 dark:outline-none
             ">
                 <Link href="/login">

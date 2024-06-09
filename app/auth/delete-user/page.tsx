59092acfe7475a667b6fa3029b151b0cd0ccae66
Pascal <zagarolopascal@gmail.com>
2024-06-09 14:41:07 +0200

    ..:

--- app/auth/delete-user/page.tsx
+++ app/auth/delete-user/page.tsx
@@ -3,15 +3,23 @@
 import { confirmDeleteUser } from "@/actions/confirm-delete-user";
 import { newVerification } from "@/actions/new-verification";
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useCallback, useEffect, useState } from "react";
 import { BeatLoader, ClipLoader } from "react-spinners"
+import { set } from 'lodash';
+import { Textarea } from "@/components/ui/textarea";
+import { check } from "drizzle-orm/mysql-core";
 
 const DeleteUserPage = () => {
     const [error, setError] = useState<string | undefined>();
     const [success, setSuccess] = useState<string | undefined>();
-    const [isLoading, setIsLoading] = useState(false)
+    const [isLoading, setIsLoading] = useState(false);
+
+    const [cancelReason, setCancelReason] = useState<string | undefined>();
+    const [currentContent, setCurrentContent] = useState<string | undefined>();
 
     const searchParams = useSearchParams();
 
@@ -39,8 +47,10 @@ const DeleteUserPage = () => {
                 if(data?.success) {
                     setError("")
                     setSuccess(data.success);
-                } else {
+                } else if(!data?.success){
                     setError(data.error);
+                } else {
+                    setError("Etwas ist schief gelaufen!");
                 }
                 
                 
@@ -60,12 +70,12 @@ const DeleteUserPage = () => {
 
     return (
         <div className="dark:bg-[#0F0F0F] min-h-screen">
-<h3 className="font-semibold text-5xl p-16 rounded-md flex justify-center">
+<h3 className="font-semibold text-5xl pt-4 pb-4 rounded-md flex justify-center">
         <Link href="/" className="flex drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.2)] text-[#414c78] dark:text-gray-100">
             <p className="dark:text-[#414c78]">u</p> Rent
         </Link>
     </h3>
-            <div className="p-8">
+            <div className="">
             {isLoading && (
                 <div className="text-2xl flex justify-center ">
                 Warte einen Moment..
@@ -75,13 +85,128 @@ const DeleteUserPage = () => {
                 <ClipLoader loading={isLoading} />
             </div>
             <div className="flex justify-center">
-                {error}
+                {error && !success && error}
             </div>
-            <div className="flex justify-center text-lg font-bold text-emerald-600">
-                {success}
+            
+            <div>
+                {success && (
+                    <div className="w-full">
+                        <div className="flex justify-center">
+                        <h3 className="text-2xl font-semibold flex justify-between">
+                            Schade, dass du gehst. <br />
+                        </h3>
+                        
+                        </div>
+                        <h1 className="flex text-sm justify-center">
+                            Möchtest du uns sagen warum?
+                        </h1>
+                        <div className="space-y-4 w-full flex justify-center">
+                        	<div className="space-y-4 w-1/4">
+                            <div className="rounded-md dark:bg-[#191919] w-full flex mt-2 p-8 space-x-4">
+                                <Checkbox 
+                                onCheckedChange={(check) => {
+                                    setCancelReason("Ich brauche mein Konto nicht mehr.")
+                                }}
+                                checked={cancelReason === "Ich brauche mein Konto nicht mehr."}
+                                />
+                                <Label>
+                                    Ich brauche mein Konto nicht mehr.
+                                </Label>
+                            </div>
+
+                            <div className="rounded-md dark:bg-[#191919] w-full flex mt-2 p-8 space-x-4">
+                                <Checkbox 
+                                onCheckedChange={(check) => {
+                                    setCancelReason("Ich hatte falsche Erwartungen an die Plattform.")
+                                }}
+                                checked={cancelReason === "Ich hatte falsche Erwartungen an die Plattform."}
+                                />
+                                <Label>
+                                    Ich hatte falsche Erwartungen an die Plattform.
+                                </Label>
+                            </div>
+
+                            <div className="rounded-md dark:bg-[#191919] w-full flex mt-2 p-8 space-x-4">
+                                <Checkbox 
+                                onCheckedChange={(check) => {
+                                    setCancelReason("Schlechte Erfahrungen mit anderen Nutzern oder anderen Vermietern.")
+                                }}
+                                checked={cancelReason === "Schlechte Erfahrungen mit anderen Nutzern oder anderen Vermietern."}
+                                />
+                                <Label>
+                                    Schlechte Erfahrungen mit anderen Nutzern oder anderen Vermietern.
+                                </Label>
+                            </div>
+
+                            <div className="rounded-md dark:bg-[#191919] w-full flex mt-2 p-8 space-x-4">
+                                <Checkbox 
+                                onCheckedChange={(check) => {
+                                    setCancelReason("Schlechte Erfahrungen mit der Plattform, oder mit uRent.")
+                                }}
+                                checked={cancelReason === "Schlechte Erfahrungen mit der Plattform, oder mit uRent."}
+                                />
+                                <Label>
+                                    Schlechte Erfahrungen mit der Plattform, oder mit uRent.
+                                </Label>
+                            </div>
+
+                            <div className="rounded-md dark:bg-[#191919] w-full flex mt-2 p-8 space-x-4">
+                                <Checkbox 
+                                onCheckedChange={(check) => {
+                                    setCancelReason("Ich habe nicht das gefunden, was ich gesucht habe.")
+                                }}
+                                checked={cancelReason === "Ich habe nicht das gefunden, was ich gesucht habe."}
+                                />
+                                <Label>
+                                    Ich habe nicht das gefunden, was ich gesucht habe.
+                                </Label>
+                            </div>
+
+                            <div className="rounded-md dark:bg-[#191919] w-full flex mt-2 p-8 space-x-4">
+                                <Checkbox 
+                                onCheckedChange={(check) => {
+                                    setCancelReason("Sonstiges")
+                                }}
+                                checked={cancelReason === "Sonstiges"}
+                                />
+                                <Label>
+                                    Sonstiges
+                                </Label>
+                            </div>
+                            <div className="">
+                                <Textarea
+                                value={currentContent}
+                                onChange={(e) => {setCurrentContent(e.target.value)}} 
+                                placeholder="Möchtest du uns noch etwas erzählen?"
+                                className="dark:bg-[#191919] dark:border-none h-[200px]"
+                                />
+                            </div>
+                            </div>
+                            
+                            
+                        </div>
+                    </div>
+                )}
             </div>
-            <div className="flex justify-center">
-                <Button variant="ghost" onClick={() => {router.push("/")}} className="dark:bg-[#080808] mt-4">
+            {success && (
+                <div className="text-xs dark:text-gray-200/60 flex justify-center">
+                <div className="w-1/4 mt-2">
+                Wir würden uns freuen, wenn du uns sagst, warum du gehst. <br />
+                Das hilft uns, uRent zu verbessern. <br/>
+                Du kannst auch eine E-Mail schreiben an : support@urent-rental.de <br/>
+                Falls du deine Meinung änderst kannst du auch jederzeit wieder zurückkommen und ein neues Konto erstellen.
+                </div>
+            </div>
+            )}
+            <div className="flex w-full justify-center mt-4 ">
+                {success && (
+                    <Button className="w-1/4 bg-indigo-800 hover:bg-indigo-900 text-gray-200 hover:text-gray-300">
+                        Informationen absenden
+                    </Button>
+                )}
+            </div>
+            <div className="flex justify-center pb-4">
+                <Button variant="ghost" onClick={() => {router.push("/")}} className="dark:bg-[#080808] mt-2 w-1/4 ">
                     zurück zur Homepage
                 </Button>
             </div>
---
+++ react-email-starter/emails/urent/confirm-deleted-account.tsx
@@ -0,0 +1,132 @@
+import {
+    Body,
+    Button,
+    Container,
+    Head,
+    Hr,
+    Html,
+    Img,
+    Link,
+    Preview,
+    Section,
+    Text,
+  } from "@react-email/components";
+import { Inter } from "next/font/google";
+  import * as React from "react";
+  
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
+    ? `${process.env.NEXT_PUBLIC_BASE_URL}`
+    : "";
+  
+    
+
+  export const ConfirmDeletedAccount = () => (
+    <Html>
+      <Head />
+      <Preview>Account gelöscht</Preview>
+      <Body style={main}>
+        <Container style={container}>
+          <Section style={box}>
+            <img              
+              src={`${baseUrl}/uRent.png`}
+              alt="uRent Logo"
+              width={120}
+              height={120}
+            />
+            <Hr style={hr} />
+            <Text style={paragraph}>
+            Dein Konto wurde erfolgreich gelöscht. <br/>
+            Falls du deine Meinung änderst, kannst du jederzeit ein neues Konto erstellen. <br/>
+            Wir freuen uns darauf, dich wieder bei uRent begrüßen zu dürfen.
+            </Text>
+            
+            
+            
+            <Hr style={hr} />
+            
+            
+            
+            <Text style={paragraph2}>
+            Wir möchten sicherstellen, dass Ihre uRent-Erfahrung reibungslos verläuft. 
+            Falls Sie Fragen oder Anliegen haben, zögern Sie bitte nicht, sich an unser Support-Team support@urent-rental.de zu wenden.  <br/>
+            Wir stehen Ihnen jederzeit gerne zur Verfügung.
+            </Text>
+            <Text style={paragraph2}>
+            Wir freuen uns darauf, Ihnen bei der Erfüllung Ihrer Bedürfnisse im Bereich Mieten und Vermieten behilflich zu sein. <br/>
+            
+            </Text>
+            <Text style={paragraph2}>— Das uRent Team</Text>
+            <Hr style={hr} />
+            <Text style={footer}>
+              uRent, 42653 Solingen, NRW, Deutschland
+            </Text>
+          </Section>
+        </Container>
+      </Body>
+    </Html>
+  );
+  
+  export default ConfirmDeletedAccount;
+
+  const inter = Inter({ subsets: ['latin'] });
+
+  const main = {
+  backgroundColor: "#f6f9fc",
+  fontFamily: 'Inter, sans-serif' 
+};
+  
+  const container = {
+    backgroundColor: "#ffffff",
+    margin: "0 auto",
+    padding: "20px 0 48px",
+    marginBottom: "64px",
+  };
+  
+  const box = {
+    padding: "0 48px",
+  };
+  
+  const hr = {
+    borderColor: "#e6ebf1",
+    margin: "20px 0",
+  };
+  
+  const paragraph = {
+    color: "#525f7f",
+    fontWeight : "500",
+    fontSize: "16px",
+    lineHeight: "24px",
+    textAlign: "left" as const,
+  };
+
+  const paragraph2 = {
+    color: "#525f7f",
+    fontWeight : "400",
+    fontSize: "12px",
+    lineHeight: "20px",
+    textAlign: "left" as const,
+  };
+  
+  const anchor = {
+    color: "#556cd6",
+  };
+  
+  const button = {
+    backgroundColor: "#131420",
+    borderRadius: "5px",
+    color: "#fff",
+    fontSize: "16px",
+    fontWeight: "bold",
+    textDecoration: "none",
+    textAlign: "center" as const,
+    display: "block",
+    width: "100%",
+    padding: "10px",
+  };
+  
+  const footer = {
+    color: "#8898aa",
+    fontSize: "12px",
+    lineHeight: "16px",
+  };
+  

f4d111c88debbe27343886cecdbdb91db3d3bf0c
Pascal <zagarolopascal@gmail.com>
2024-06-12 00:54:35 +0200

    redesigneed other inserate

--- app/(anzeige)/inserat/[inseratId]/_components/other-inserate.tsx
+++ app/(anzeige)/inserat/[inseratId]/_components/other-inserate.tsx
@@ -3,6 +3,7 @@ import { Separator } from "@/components/ui/separator";
 import { IoIosPaper } from "react-icons/io";
 import OtherInserateRender from "./other-inserate-render";
 import { inserat, userTable } from "@/db/schema";
+import { UserIcon } from "lucide-react";
 
 interface OtherInserateProps {
     thisUser: typeof userTable.$inferSelect,
@@ -19,9 +20,18 @@ const OtherInserate: React.FC<OtherInserateProps> = ({
 
     return (
         <div className="xl:w-3/5 xl:min-w-[360px] bg-[#171923] px-4 py-4 rounded-md ">
-            <h3 className=" flex gap-x-2 font-medium text-gray-200">
-                <IoIosPaper className="w-4 h-4" />Weiteres von
-                <div className="w-3/5 line-clamp-1 font-semibold">{thisUser?.name}</div>
+            <h3 className=" gap-x-2 font-medium text-gray-200">
+                <div className="flex items-center text-medium dark:text-gray-200/90">
+                <IoIosPaper className="w-4 h-4 mr-2" /> Weitere Inserate von
+                </div>
+                <div className="w-full line-clamp-1 flex items-center font-semibold">
+                   <div>
+                   <UserIcon className="w-4 h-4 mr-2.5" /> 
+                   </div>
+                   <div className="w-full line-clamp-1">
+                   {thisUser?.name}
+                   </div>
+                   </div>
             </h3>
             <div className="px-2 mt-2">
                 <Separator className="w-full bg-[#393c53] h-[0.5px]" />
@@ -38,7 +48,7 @@ const OtherInserate: React.FC<OtherInserateProps> = ({
             ) : (
                 <div className="px-2 max-h-[400px]">
 
-                    <p className="text-gray-200/80 font-medium text-sm mt-2">
+                    <p className="text-gray-200/80 text-xs flex justify-center mt-4">
                         Noch keine weiteren Inserate vorhanden..
                     </p>
                 </div>

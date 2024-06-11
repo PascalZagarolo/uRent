5627f10c754237b83e4ca71a99c4624638048807
Pascal <zagarolopascal@gmail.com>
2024-06-12 00:45:23 +0200

    performance update1 chat

---
+++ actions/findStartedConversations.ts
@@ -0,0 +1,26 @@
+
+
+import db from "@/db/drizzle"
+import { conversation } from "@/db/schema"
+import { eq, or } from "drizzle-orm"
+import { cache } from "react"
+
+export const findStartedConversationsGlobal = (async(currentUserId) => {
+    const findStartedConversations = db.query.conversation.findMany({
+        where: (
+            or(
+                eq(conversation.user1Id, currentUserId),
+                eq(conversation.user2Id, currentUserId)
+
+            )
+        ), with: {
+            messages: true,
+            user1: true,
+            user2: true
+
+
+        }
+    }).prepare("findStartedConversations")
+
+    return await findStartedConversations.execute();
+})

1b54d199a7ccef8dca99b1452b06f5f73cfd3323
Pascal <zagarolopascal@gmail.com>
2024-06-14 18:08:53 +0200

    sites on bookings..

--- app/dashboard/[userId]/(routes)/_components/booking-day-details.tsx
+++ app/dashboard/[userId]/(routes)/_components/booking-day-details.tsx
@@ -4,7 +4,7 @@ import { Label } from "@/components/ui/label";
 import { booking, inserat } from "@/db/schema";
 import { cn } from "@/lib/utils";
 import { Share1Icon } from "@radix-ui/react-icons";
-import { format, isSameDay } from "date-fns";
+import { format, isSameDay, set } from 'date-fns';
 import { de, fi } from "date-fns/locale";
 import { CheckIcon, LinkIcon, Share2Icon } from "lucide-react";
 
@@ -12,15 +12,18 @@ import React, { useCallback, useEffect, useMemo, useState } from "react";
 import BookingDayDetailsPopover from "./booking-day-details-popover";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { vehicle } from '../../../../../db/schema';
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { PiArrowLineLeftBold, PiArrowLineRightBold } from "react-icons/pi";
+import { Button } from "@/components/ui/button";
 
 interface BookingDayDetailsProps {
     selectedDate: Date;
     relevantBookings: typeof booking.$inferSelect[];
     foundInserate: typeof inserat.$inferSelect[];
-    selectedInserat : string;
-    selectedInseratData : typeof inserat.$inferSelect;
-    renderedInserate : any[];
-    
+    selectedInserat: string;
+    selectedInseratData: typeof inserat.$inferSelect;
+    renderedInserate: any[];
+
 }
 
 const BookingDayDetails: React.FC<BookingDayDetailsProps> = ({
@@ -32,18 +35,24 @@ const BookingDayDetails: React.FC<BookingDayDetailsProps> = ({
     renderedInserate
 }) => {
 
+    const [startIndex, setStartIndex] = useState(0);
+    const [endIndex, setEndIndex] = useState(3);
+
     const [appointedTimes, setAppointedTimes] = useState<{ [key: string]: any[] }[]>([]);
     const [usedBookings, setUsedBookings] = useState(relevantBookings);
+    const [renderingInserate, setRenderingInserate] = useState(renderedInserate.slice(startIndex, endIndex));
+
     
-    
-    
+    useEffect(() => {
+        setRenderingInserate(renderedInserate.slice(startIndex, endIndex));
+    },[renderedInserate, startIndex, endIndex])
 
 
 
     useMemo(() => {
         setAppointedTimes([]);
         setUsedBookings(relevantBookings);
-    
+
         for (const pBooking of relevantBookings) {
             if (relevantBookings) {
                 setAppointedTimes(prevAppointedTimes => {
@@ -60,7 +69,7 @@ const BookingDayDetails: React.FC<BookingDayDetailsProps> = ({
                     } else {
                         newAppointedTimes[existingIndex].bookingIds.push(pBooking.id);
                     }
-    
+
                     // Populate appointed times array based on booking dates and periods
                     const index = newAppointedTimes.findIndex(item => item.bookingIds.includes(pBooking.id));
                     if (!isSameDay(pBooking.startDate, selectedDate) && !isSameDay(pBooking.endDate, selectedDate)) {
@@ -80,7 +89,7 @@ const BookingDayDetails: React.FC<BookingDayDetailsProps> = ({
                             newAppointedTimes[index].times.push(i);
                         }
                     }
-    
+
                     return newAppointedTimes;
                 });
             }
@@ -95,7 +104,7 @@ const BookingDayDetails: React.FC<BookingDayDetailsProps> = ({
 
 
     const checkBookedMulti = (inseratId: string, number: string, vehicleId: string) => {
-    
+
         //@ts-ignore
         return appointedTimes.some(item => (item.inseratId === inseratId && item.times.includes(Number(number)) &&
             item.vehicleId.includes(vehicleId)));
@@ -138,10 +147,10 @@ const BookingDayDetails: React.FC<BookingDayDetailsProps> = ({
 
                                     <BookingDayDetailsPopover
                                         // @ts-ignore
-                                        thisBooking={relevantBookings.find(booking => 
+                                        thisBooking={relevantBookings.find(booking =>
                                             //@ts-ignore
                                             appointedTimes.find(item => item.inseratId === inseratId
-                                            && item.times.includes((hour * 60))))}
+                                                && item.times.includes((hour * 60))))}
                                         foundInserate={foundInserate}
                                     />
                                 </div>
@@ -209,12 +218,12 @@ const BookingDayDetails: React.FC<BookingDayDetailsProps> = ({
                                 </div>
                             )}
                             {!checkBookedMulti(inseratId, String(hour * 60), vehicleId) &&
-                            checkBookedMulti(inseratId, String((hour * 60) - 30), vehicleId) && (
-                                <div className="text-xs font-medium gap-x-2 flex">
-                                    <CheckIcon className="w-4 h-4 mr-2 text-emerald-600" />
-                                    Verfügbar ab {hour}:00 Uhr
-                                </div>
-                            )}
+                                checkBookedMulti(inseratId, String((hour * 60) - 30), vehicleId) && (
+                                    <div className="text-xs font-medium gap-x-2 flex">
+                                        <CheckIcon className="w-4 h-4 mr-2 text-emerald-600" />
+                                        Verfügbar ab {hour}:00 Uhr
+                                    </div>
+                                )}
                         </div>
                         <div className={cn("h-[40px] w-full p-2 font-semibold text-xs border-t border-dotted border-[#191919]",
                             checkBookedMulti(inseratId, String((hour * 60) + 30), vehicleId) ? " bg-rose-800" : "",
@@ -229,7 +238,7 @@ const BookingDayDetails: React.FC<BookingDayDetailsProps> = ({
                                             appointedTimes.find(item =>
                                                 //@ts-ignore
                                                 item.inseratId === inseratId &&
-                                                item.times.includes((hour * 60)+30) &&
+                                                item.times.includes((hour * 60) + 30) &&
                                                 item.bookingIds.includes(booking.id) &&
                                                 item.vehicleId.includes(vehicleId)
                                             )
@@ -238,11 +247,11 @@ const BookingDayDetails: React.FC<BookingDayDetailsProps> = ({
                                 </div>
                             )}
                             {!checkBookedMulti(inseratId, String((hour * 60) + 30), vehicleId) &&
-                            checkBookedMulti(inseratId, String((hour * 60)), vehicleId) && (
-                                <div className="flex text-xs font-medium">
-                                    <CheckIcon className="w-4 h-4 mr-2 text-emerald-600" /> Verfügbar ab {hour}:30 Uhr
-                                </div>
-                            )}
+                                checkBookedMulti(inseratId, String((hour * 60)), vehicleId) && (
+                                    <div className="flex text-xs font-medium">
+                                        <CheckIcon className="w-4 h-4 mr-2 text-emerald-600" /> Verfügbar ab {hour}:30 Uhr
+                                    </div>
+                                )}
                         </div>
                     </div>
                 </div>
@@ -255,7 +264,7 @@ const BookingDayDetails: React.FC<BookingDayDetailsProps> = ({
         <div className="w-full">
             <div>
                 <div className="flex items-center w-full">
-                    <Label className="text-md font-semibold flex items-center gap-x-2 w-full">
+                    <Label className="text-md font-semibold flex items-center gap-x-2 ">
                         {selectedInserat &&
                             <div className="font-semibold text-indigo-600 w-1/3 line-clamp-1">
                                 {foundInserate.find(inserat => inserat.id === selectedInserat)?.title}
@@ -267,7 +276,30 @@ const BookingDayDetails: React.FC<BookingDayDetailsProps> = ({
                         )}
 
                     </Label>
-                    
+                    {renderedInserate.length > 3 && !selectedInserat && (
+                        <div className="ml-auto flex justify-end gap-x-2 items-center">
+                        <div>
+                            <Button size="sm" variant="ghost" onClick={() => {
+                                setStartIndex(startIndex - 3)
+                                setEndIndex(endIndex - 3);
+                                }}>
+                            <PiArrowLineLeftBold className="w-4 h-4" />
+                            </Button>
+                            </div>
+                            <div className="">
+                                Seite {startIndex / 3 + 1}
+                            </div>
+                            <div>
+                            <Button size="sm" variant="ghost" onClick={() => {
+                                setEndIndex(endIndex + 3);
+                                setStartIndex(startIndex + 3);
+                                
+                                }}>
+                            <PiArrowLineRightBold className="w-4 h-4" />
+                            </Button>
+                            </div>
+                        </div>
+                    )}
                 </div>
                 <p className="text-xs dark:text-gray-200/60">
                     Detaillierte Tagesansicht für den {selectedDate && (
@@ -277,13 +309,9 @@ const BookingDayDetails: React.FC<BookingDayDetailsProps> = ({
                     )}
                 </p>
             </div>
-            <div className="mt-4 w-full">
-                {!selectedDate && (
-                    <div className="text-sm font-normal dark:text-gray-200/60 w-full flex justify-center">
-                        Klicke auf ein Datum um die Details einzusehen
-                    </div>
-                )}
-                <div className="mt-4 dark:bg-[#0F0F0F] rounded-md">
+            <div className="mt-4 w-full ">
+
+                <div className="mt-4 dark:bg-[#0F0F0F] rounded-md h-[720px] overflow-y-scroll ">
                     <div className="w-full flex ">
 
                         <div className="w-1/4">
@@ -295,9 +323,9 @@ const BookingDayDetails: React.FC<BookingDayDetailsProps> = ({
                             </div>
                         </div>
 
-                        <div className="w-full overflow-x-auto">
-                            <div className={cn("gap-x-16 flex items-center justify-evenly",
-                                (foundInserate.length > 1 && !selectedInserat) && "ml-16")}>
+                        <div className="w-full overflow-x-hidden">
+                            <div className={cn(" flex items-center justify-evenly")
+                                }>
 
 
                                 {selectedInseratData?.multi ? (
@@ -318,7 +346,7 @@ const BookingDayDetails: React.FC<BookingDayDetailsProps> = ({
                                         </div>
                                     ))
                                 ) : (
-                                    renderedInserate.map((inserat) => (
+                                    renderingInserate.map((inserat) => (
                                         <div className={cn("", (selectedInserat || renderedInserate.length === 1) && "w-full")} key={inserat.id}>
                                             <div className="font-medium text-sm p-8 w-[240px] text-left overflow-hidden">
                                                 <a className="line-clamp-1 break-all hover:underline text-left" href={`/inserat/${inserat.id}`} target="_blank">
@@ -343,11 +371,11 @@ const BookingDayDetails: React.FC<BookingDayDetailsProps> = ({
 
                             )}
                             {//@ts-ignore
-                            selectedInseratData?.multi && selectedInseratData?.vehicles.length === 0 && (
-                                <div className="text-sm font-normal dark:text-gray-200/60 w-full justify-center h-full flex items-center">
-                                    Noch keine Fahrzeuge erstellt..
-                                </div>
-                            )}
+                                selectedInseratData?.multi && selectedInseratData?.vehicles.length === 0 && (
+                                    <div className="text-sm font-normal dark:text-gray-200/60 w-full justify-center h-full flex items-center">
+                                        Noch keine Fahrzeuge erstellt..
+                                    </div>
+                                )}
                         </div>
 
                     </div>

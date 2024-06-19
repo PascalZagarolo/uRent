4bdd71dcf1a543e5ded2251f81a5dadd7434bde2
Pascal <zagarolopascal@gmail.com>
2024-06-19 13:14:45 +0200

    ...

---
+++ app/admin/notifications/_compontents/create-and-created-tab.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import CreateNotificationUnauthorized from "./create-notification-unauthorized";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+import { notificationUnauthorized } from "@/db/schema";
+import ExistingNotifications from "./existing-notifications";
+
+
+interface TabSwitchNotificationsProps {
+    foundNotifications : typeof notificationUnauthorized.$inferSelect[];
+}
+
+const TabSwitchNotifications : React.FC<TabSwitchNotificationsProps> = ({
+    foundNotifications
+}) => {
+
+    const [currentTab, setCurrentTab] = useState('create')
+
+    return (
+        <div>
+            <Tabs defaultValue="create">
+                <TabsList className="grid w-full grid-cols-2 bg-[#0F0F0F]" >
+                    <TabsTrigger value="create"
+                    onClick={() => setCurrentTab('create')}
+                    className={cn(currentTab === 'create' ? "bg-[#0F0F0F]" : "bg-[#1c1c1c]")}
+                    >Benachrichtigung erstellen</TabsTrigger>
+                    <TabsTrigger value="existing"
+                    onClick={() => setCurrentTab('existing')}
+                    className={cn(currentTab === 'existing' ? "bg-[#0F0F0F]" : "bg-[#1c1c1c]")}
+                    >Bereits erstellte Benachrichtigungen ({foundNotifications.length})</TabsTrigger>
+                </TabsList>
+                <TabsContent value="create">
+                    <CreateNotificationUnauthorized />
+                </TabsContent>
+                <TabsContent value="existing">
+                <ExistingNotifications 
+                foundNotifications={foundNotifications}
+                />
+                </TabsContent>
+            </Tabs>
+        </div>
+    );
+}
+
+export default TabSwitchNotifications;
--- app/admin/notifications/page.tsx
+++ app/admin/notifications/page.tsx
@@ -1,8 +1,14 @@
+import db from "@/db/drizzle";
 import BreadCrumpPage from "../_components/bread-crump-page";
 import MenuBar from "../_components/menu-bar";
+import TabSwitchNotifications from "./_compontents/create-and-created-tab";
 import CreateNotificationUnauthorized from "./_compontents/create-notification-unauthorized";
+import { notificationUnauthorized } from "@/db/schema";
 
-const NotificationsAdminPage = () => {
+const NotificationsAdminPage = async () => {
+
+    const foundNotifications : any = await db.query.notificationUnauthorized.findMany();
+
     return ( 
         <div className="flex justify-center sm:py-8  sm:px-4">
             <div className="sm:w-[1044px] w-full dark:bg-[#1c1c1c] rounded-md bg-white">
@@ -14,7 +20,9 @@ const NotificationsAdminPage = () => {
                         </div>
                     </div>
                     <div className="p-8">
-                        <CreateNotificationUnauthorized />
+                        <TabSwitchNotifications 
+                        foundNotifications = {foundNotifications}
+                        />
                     </div>
                 </div>
             </div>

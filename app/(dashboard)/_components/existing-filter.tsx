a639af700efb19f685d8c947488eeb4a028d7950
Pascal <zagarolopascal@gmail.com>
2024-07-07 14:11:31 +0200

    ..

---
+++ app/(dashboard)/_components/existing-filter.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+const ExistingFilter = () => {
+    return (
+        <div className="w-full p-2 border-[#202336] border-l-none border-r-none border-t-2 border-b-2 bg-[#1B1E2C] text-xs">
+            <div>
+                <h3>
+                    Suchfilter:
+                </h3>
+                <div className="mt-1">
+                    Platzhalter
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ExistingFilter;
--- app/(dashboard)/_components/main-page-sidebar.tsx
+++ app/(dashboard)/_components/main-page-sidebar.tsx
@@ -14,82 +14,99 @@ import { getSearchParamsFunction } from "@/actions/getSearchParams";
 import { PiVanFill } from "react-icons/pi";
 import { RiCaravanLine } from "react-icons/ri";
 import CategoryOverview from "./filter-categories/category-overview";
-import {  useDeleteParams, useSavedSearchParams } from "@/store";
-import {  MdOutlineCancel } from "react-icons/md";
+import { useDeleteParams, useSavedSearchParams } from "@/store";
+import { MdOutlineCancel } from "react-icons/md";
 import { useEffect, useState } from "react";
 import MainPageResults from "./main-page-results";
 import { CategoryEnumRender } from "@/db/schema";
 import SaveSearch from "./save-search";
+import ExistingFilter from "./existing-filter";
+import { set } from "lodash";
+import { link } from "fs";
 
 
 interface MainPageSideBarProps {
-    userId? : string
+    userId?: string
 }
 
-const MainPageSideBar : React.FC<MainPageSideBarProps> = ({
+const MainPageSideBar: React.FC<MainPageSideBarProps> = ({
     userId
 }) => {
     const pathname = usePathname();
     const usedSearchParams = useSearchParams();
     const currentCategory = usedSearchParams.get("category");
-   
+    
+
     const router = useRouter();
 
     const [isMounted, setIsMounted] = useState(false);
 
     const params = getSearchParamsFunction("category");
 
+    const linkedParams = getSearchParamsFunction("");
+
+    const [hasNonNullValue, setHasNonNullValue] = useState(Object.values(linkedParams).some(value => value !== null))
+
     
+    
+    
+
     useEffect(() => {
-        if(!isMounted) {
+        if (!isMounted) {
             setIsMounted(true);
         }
-    },[])
-    
+    }, [])
+
+   
 
     const onReset = () => {
         const url = process.env.NEXT_PUBLIC_BASE_URL
-        
+
         router.push(url)
-        
+
 
     }
-    
 
-   
+
+
     const { searchParams, changeSearchParams, deleteSearchParams } = useSavedSearchParams();
 
     const currentObject = useSavedSearchParams((state) => state.searchParams)
+    
 
     const { changeAttributes } = useDeleteParams();
 
-    const setCategory = (category : typeof CategoryEnumRender) => {
+    const setCategory = (category: typeof CategoryEnumRender) => {
         deleteAttributes();
         //@ts-ignore
         changeSearchParams("thisCategory", category);
-        
+
     }
 
     useEffect(() => {
-        if(currentCategory){
+        if (currentCategory) {
             changeSearchParams("thisCategory", currentCategory);
         }
-        
+
     }, [])
 
+    useEffect(() => {
+        setHasNonNullValue(Object.values(linkedParams).some(value => value !== null))
+    },[linkedParams])
+
     const deleteCategory = () => {
-        
+
         deleteAttributes();
         deleteSearchParams("thisCategory")
     }
 
-    
-    
-    
-    
+
+
+
+
 
     const deleteAttributes = () => {
-        
+
         changeAttributes(true);
 
         //ALLGEMEIN
@@ -107,7 +124,7 @@ const MainPageSideBar : React.FC<MainPageSideBarProps> = ({
         deleteSearchParams("initialMax")
         deleteSearchParams("type");
         deleteSearchParams("ahk");
-        
+
         //LKW
         deleteSearchParams("application")
         deleteSearchParams("axis")
@@ -122,39 +139,45 @@ const MainPageSideBar : React.FC<MainPageSideBarProps> = ({
         deleteSearchParams("transmission")
         deleteSearchParams("volume")
         deleteSearchParams("weightClass")
-        
+
         //TRAILER
         deleteSearchParams("brake")
         deleteSearchParams("coupling")
         deleteSearchParams("brake")
-        
+
         //TRANSPORT
         deleteSearchParams("transportBrand")
 
-        
-        
+
+
     }
 
-    
-    
-    
 
-    
+
+
+
+
     return (
         <div className=" no-scrollbar w-[280px] rounded-md hidden xl:block bg-[#202336]  sm:overflow-auto    ">
             <h3 className="text-bold text-2xl p-4 font-medium  flex justify-center text-gray-100 items-center  bg-[#1b1e2c]">
-                <FilterIcon className="mr-4" /> Suchfilter                 
+                <FilterIcon className="mr-4" /> Suchfilter
             </h3>
-            <a 
-            className="w-full flex bg-[#1b1e2c] justify-center items-center text-xs pb-2 hover:underline hover:cursor-pointer" 
-            href={process.env.NEXT_PUBLIC_BASE_URL}>
-            <MdOutlineCancel  className="w-4 h-4 mr-2 text-rose-600" />  Filter zurücksetzen
+            <a
+                className="w-full flex bg-[#1b1e2c] justify-center items-center text-xs pb-2 hover:underline hover:cursor-pointer"
+                href={process.env.NEXT_PUBLIC_BASE_URL}>
+                <MdOutlineCancel className="w-4 h-4 mr-2 text-rose-600" />  Filter zurücksetzen
             </a>
             <div className="py-2 bg-[#1b1e2c]">
-                <SaveSearch 
-                userId={userId || ""}
+                <SaveSearch
+                    userId={userId || ""}
                 />
             </div>
+            {hasNonNullValue && (
+                <div className="">
+                    <ExistingFilter />
+                </div>
+            )}
+
             <div className="">
                 <h3 className="text-gray-100 font-semibold    p-2 flex justify-center  bg-[#1b1f2c] dark:border-[#1f2332]">
                     Fahrzeugkategorie
@@ -163,7 +186,7 @@ const MainPageSideBar : React.FC<MainPageSideBarProps> = ({
                     <div className="">
                         <p className={cn("p-4 rounded-md text-gray-200  border-2 hover:cursor-pointer bg-[#1c1f2f]",
                             //@ts-ignore
-                            currentObject["thisCategory"] === "PKW" ? "border-blue-800" : "border-[#212539]")} 
+                            currentObject["thisCategory"] === "PKW" ? "border-blue-800" : "border-[#212539]")}
                             onClick={//@ts-ignore
                                 () => currentObject["thisCategory"] === "PKW" ? deleteCategory() : setCategory("PKW")}>
                             <CarFront />
@@ -176,7 +199,7 @@ const MainPageSideBar : React.FC<MainPageSideBarProps> = ({
                     <div>
                         <p className={cn("p-4 rounded-md text-gray-200 border-2 hover:cursor-pointer bg-[#1c1f2f]",
                             //@ts-ignore
-                         currentObject["thisCategory"] === "LKW" ? "border-blue-800" : "border-[#212539]")} 
+                            currentObject["thisCategory"] === "LKW" ? "border-blue-800" : "border-[#212539]")}
                             onClick={//@ts-ignore
                                 () => currentObject["thisCategory"] === "LKW" ? deleteCategory() : setCategory("LKW")}>
                             <Truck />
@@ -191,8 +214,8 @@ const MainPageSideBar : React.FC<MainPageSideBarProps> = ({
                 <div className="flex justify-between ml-12 mr-12 mt-4 ">
                     <div>
                         <p className={cn("p-4 rounded-md text-gray-200 border-2 hover:cursor-pointer bg-[#1c1f2f]",
-                         //@ts-ignore
-                         currentObject["thisCategory"] === "TRAILER" ? "border-blue-800" : "border-[#212539]")} 
+                            //@ts-ignore
+                            currentObject["thisCategory"] === "TRAILER" ? "border-blue-800" : "border-[#212539]")}
                             onClick={//@ts-ignore
                                 () => currentObject["thisCategory"] === "TRAILER" ? deleteCategory() : setCategory("TRAILER")}>
                             <RiCaravanLine className="w-6 h-6" />
@@ -205,7 +228,7 @@ const MainPageSideBar : React.FC<MainPageSideBarProps> = ({
                     <div className="w-[60px]">
                         <p className={cn("p-4 rounded-md text-gray-200 border-2   flex justify-center hover:cursor-pointer bg-[#1c1f2f]",
                             //@ts-ignore
-                         currentObject["thisCategory"] === "TRANSPORT" ? "border-blue-800" : "border-[#212539]")} 
+                            currentObject["thisCategory"] === "TRANSPORT" ? "border-blue-800" : "border-[#212539]")}
                             onClick={//@ts-ignore
                                 () => currentObject["thisCategory"] === "TRANSPORT" ? deleteCategory() : setCategory("TRANSPORT")}>
                             <PiVanFill className="w-6 h-6" />
@@ -221,20 +244,20 @@ const MainPageSideBar : React.FC<MainPageSideBarProps> = ({
                 <div className="flex justify-between ml-12 mr-12 mt-4 ">
                 </div>
                 <div>
-                <PKW />
+                    <PKW />
                 </div>
                 <div>
-                    
+
                     <CategoryOverview />
-                    
+
                 </div>
-                <div className="text-xs flex justify-center text-gray-100 underline hover:cursor-pointer" onClick={() => {router.push("/search")}}>
+                <div className="text-xs flex justify-center text-gray-100 underline hover:cursor-pointer" onClick={() => { router.push("/search") }}>
                     <Settings2 className="mr-2 h-4 w-4" /> Zu der Erweiterten Suche
                 </div>
                 <div className="flex justify-center mt-2 mb-2 rounded-md">
                     <MainPageResults />
                 </div>
-                
+
             </div>
         </div>
     );

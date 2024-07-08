9a17e2cd4a16c39c856f37767fcb27eb91b95d33
Pascal <zagarolopascal@gmail.com>
2024-07-08 20:00:00 +0200

    ...

--- app/(dashboard)/_components/existing-filter.tsx
+++ app/(dashboard)/_components/existing-filter.tsx
@@ -8,7 +8,6 @@ const ExistingFilter = () => {
 
     const searchParams = getSearchParamsFunction();
 
-
     return (
         <div className="w-full p-2 border-[#202336] border-l-none border-r-none border-t-2 border-b-2 bg-[#1B1E2C] text-xs">
             <div>

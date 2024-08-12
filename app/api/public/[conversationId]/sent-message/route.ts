c92c4defafa3c3bab8089dd63b6fa58ac5a1cb75
Pascal <zagarolopascal@gmail.com>
2024-08-12 17:58:59 +0200

    ...

--- app/api/public/[conversationId]/sent-message/route.ts
+++ app/api/public/[conversationId]/sent-message/route.ts
@@ -16,20 +16,11 @@ export async function POST(
 
         //Route for Mobile App, if App user writes message, the Server will response to the given conversation and notifies the other participant
         
-        const usedMessage = {
-            id : "000eb703-2827-4079-85b2-6c65ff1737bc",
-            content : "Pablo Chicochico",
-            image : null,
-            senderId : "000eb703-2827-4079-85b2-6c65ff1737bc",
-            conversationId : params.conversationId,
-            seen : false,
-            createdAt : new Date().toISOString(),
+        const values = await req.json();
 
-        }
+        await pusherServer.trigger(params.conversationId, 'messages:new', values.currentMessage);
 
-        await pusherServer.trigger(params.conversationId, 'messages:new', usedMessage);
-
-        return NextResponse.json({ success : usedMessage})
+        return NextResponse.json({ success : values.currentMessage})
 
 
     } catch(e : any) {

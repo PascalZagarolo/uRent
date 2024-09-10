4f24085603b6fd274ad0ab1fb34084e2c9262343
Pascal <zagarolopascal@gmail.com>
2024-09-11 00:50:05 +0200

    ...

--- app/inserat/create/_components/input-fields/phone-number.tsx
+++ app/inserat/create/_components/input-fields/phone-number.tsx
@@ -5,14 +5,16 @@ import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { inserat } from "@/db/schema";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { inserat, userContactprofiles } from "@/db/schema";
+import { cn } from "@/lib/utils";
 
 
 
 import axios from "axios";
 import { ChevronDown, MailCheckIcon, MailOpen, PhoneCall, PhoneCallIcon, PhoneForwarded, PhoneIncomingIcon, PinIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import toast from "react-hot-toast";
 
@@ -26,7 +28,25 @@ const PhoneNumber: React.FC<PhoneNumberProps> = ({
 
   const [isLoading, setIsLoading] = useState(false);
   const [currentNumber, setCurrentNumber] = useState(thisInserat.phoneNumber || "");
-  const [value, setValue] = useState("");
+  const [modalOpen, setModalOpen] = useState(false);
+
+  
+
+
+  const [renderedNumbers, setRenderedNumbers] = useState<typeof userContactprofiles.$inferSelect[]>([]);
+
+  useEffect(() => {
+    const phoneNumbers = thisInserat?.user?.userContactprofiles.map((mail) => {
+      if(mail.profileType === "PHONE") {
+        return {
+          id: mail.id,
+          title: mail.title,
+          content: mail.content,
+        }
+      }
+    })
+    setRenderedNumbers([...phoneNumbers]);
+  },[])
 
   const [isPrefill, setIsPrefill] = useState(false);
 
@@ -55,16 +75,12 @@ const PhoneNumber: React.FC<PhoneNumberProps> = ({
 
   const inputRef = React.useRef<HTMLInputElement>(null);
 
-  const onPrefill = () => {
-    if (!isPrefill) {
-      if (thisInserat.user?.business?.telephone_number) {
-        setCurrentNumber(thisInserat.user.business.telephone_number);
-      }
-      setIsPrefill(true);
-    } else if (isPrefill) {
-      setCurrentNumber("");
-      setIsPrefill(false);
-    }
+  const onPrefill = (usedNumber : string) => {
+    setCurrentNumber(usedNumber);
+  }
+
+  const isAlreadyChosen = (number: string) => {
+    return number === currentNumber;
   }
 
   return (
@@ -89,14 +105,52 @@ const PhoneNumber: React.FC<PhoneNumberProps> = ({
               value={currentNumber}
               onChange={(e) => { setCurrentNumber(e.target.value) }}
             />
-            <Button className="mt-2 bg-[#0F0F0F] hover:bg-[#0F0F0F]  py-5 rounded-l-none" size="sm">
-              <ChevronDown className="h-4 w-4 text-gray-200" />
-            </Button>
+            <Popover onOpenChange={(e) => {setModalOpen(e)}}
+            open={modalOpen}
+            >
+              <PopoverTrigger asChild>
+                <Button className="mt-2 bg-[#0F0F0F] hover:bg-[#0F0F0F]  py-5 rounded-l-none" size="sm">
+                  <ChevronDown className="h-4 w-4 text-gray-200" />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="bg-[#171717] rounded-md border-none p-4 shadow-lg">
+                <div className="font-semibold text-base">
+                  Email-Addresse auswählen
+                </div>
+                <div className="flex flex-col h-full space-y-2 mt-2">
+                  {renderedNumbers.length > 0 ? (
+                    renderedNumbers.map((mail) => (
+                      <Button
+                      disabled={isAlreadyChosen(mail.content)}
+                      key={mail.id}
+                      variant="ghost"
+                      className="justify-start text-left w-full p-3 py-6 rounded-md hover:bg-[#1E1E1E] transition-colors duration-200"
+                      onClick={() => { onPrefill(mail.content) }}
+                    >      
+                       <div className="text-left w-full">
+                          <div className={cn("text-sm text-gray-200 font-bold", mail.id == "1" && "text-indigo-800 underline" )}>
+                            {mail.title}
+                          </div>
+                          <div className="mt-1 text-xs text-gray-200/60 font-medium">
+                          {mail.content}
+                          </div>
+                        </div>
+                      </Button>
+                    ))
+                  ) : (
+                    <div className="text-xs text-gray-200/60">
+                      Noch keine Emails hinzugefügt..
+                    </div>
+                  )}
+                </div>
+              </PopoverContent>
+            </Popover>
           </div>
         </div>
 
       </div>
 
+      {/*
       <div className="flex mt-2">
         <Checkbox
           className="h-4 w-4 mr-2"
@@ -106,9 +160,10 @@ const PhoneNumber: React.FC<PhoneNumberProps> = ({
           Informationen aus Profil verwenden
         </Label>
       </div>
+      */}
 
 
-      <Button onClick={() => { onSubmit() }} className="mt-2 dark:bg-[#000000] dark:hover:bg-[#0b0b0b] dark:text-gray-100" //@ts-ignore
+      <Button onClick={() => { onSubmit() }} className="mt-8 dark:bg-[#000000] dark:hover:bg-[#0b0b0b] dark:text-gray-100" //@ts-ignore
         disabled={!currentNumber || currentNumber === thisInserat.phoneNumber || !thisInserat.user?.business?.telephone_number}
       >
         <span className="">Telefonnr. anzeigen</span>

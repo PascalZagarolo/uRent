677cb782e955c4cd2bd90ecca80b2b53e5ff74d9
Pascal <zagarolopascal@gmail.com>
2024-10-18 16:18:58 +0200

    ...

--- components/ad-component.tsx
+++ components/ad-component.tsx
@@ -4,22 +4,25 @@ import React, { useEffect } from 'react';
 import AdSense from 'react-adsense';
 const AdsComponent = (props?: any) => {
 
-    useEffect(() => {
-        try {
-            //@ts-ignore
-            (window.adsbygoogle = window?.adsbygoogle || []).push({});
-        }
-
-        catch (e) {
-            console.log(e)
-        }
-    }, []);
+    
 
     try {
+
+        useEffect(() => {
+            try {
+                //@ts-ignore
+                (window.adsbygoogle = window?.adsbygoogle || []).push({});
+            }
+    
+            catch (e) {
+                console.log(e)
+            }
+        }, []);
+
         return (
 
             <>
-               <AdSense.Google
+               <AdSense
                     client='ca-pub-9497499351411762'
                     slot='3797720061'
                     style={{ display: 'block' }}

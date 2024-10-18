2af9320cdae6077cb2565e3f070f4678d563ce86
Pascal <zagarolopascal@gmail.com>
2024-10-18 13:40:16 +0200

    modified..

--- components/ad-component.tsx
+++ components/ad-component.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useEffect } from 'react';
-
+import AdSense from 'react-adsense';
 const AdsComponent = (props: any) => {
     const { dataAdSlot } = props;
 
@@ -24,13 +24,14 @@ const AdsComponent = (props: any) => {
 
     return (
         <>
-            <ins className="adsbygoogle"
-                style={{ display: "block", height: "100vh" }}
-                data-ad-client="ca-pub-9497499351411762"
-                data-ad-slot="3797720061"
-                data-ad-format="auto"
-                data-full-height-responsive="true"
-                data-full-width-responsive="true"></ins>
+            <AdSense.Google
+                client='ca-pub-9497499351411762'
+                slot='3797720061'
+                style={{ display: 'block' }}
+                format='auto'
+                responsive='true'
+                layoutKey='-gw-1+2a-9x+5c'
+            />
         </>
     );
 };

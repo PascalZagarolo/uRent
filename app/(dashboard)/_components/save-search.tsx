2aea0ae0307e60dd889bb8ed06f998f65c4c473d
Pascal <zagarolopascal@gmail.com>
2024-10-20 18:35:43 +0200

    restriciton on saved searches..

--- app/(dashboard)/_components/save-search.tsx
+++ app/(dashboard)/_components/save-search.tsx
@@ -10,14 +10,15 @@ import { useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { Checkbox } from "@/components/ui/checkbox";
+import LetterRestriction from "@/components/letter-restriction";
 
 
 
 interface SaveSearchProps {
-    userId : string;
+    userId: string;
 }
 
-const SaveSearch : React.FC<SaveSearchProps> = ({
+const SaveSearch: React.FC<SaveSearchProps> = ({
     userId
 }) => {
 
@@ -32,17 +33,17 @@ const SaveSearch : React.FC<SaveSearchProps> = ({
 
     const onGenerateUrl = () => {
         const {//@ts-ignore
-            thisCategory, ...filteredValues} = searchParams;   
+            thisCategory, ...filteredValues } = searchParams;
         //@ts-ignore
         const usedStart = filteredValues.periodBegin;
         let usedEnd = null;
-    //@ts-ignore
-        if(filteredValues.periodEnd){
         //@ts-ignore
-        usedEnd = filteredValues.periodEnd;
+        if (filteredValues.periodEnd) {
+            //@ts-ignore
+            usedEnd = filteredValues.periodEnd;
         } else {
             //@ts-ignore
-            if(filteredValues.periodBegin) {
+            if (filteredValues.periodBegin) {
                 //@ts-ignore
                 usedEnd = filteredValues.periodBegin;
             }
@@ -70,12 +71,12 @@ const SaveSearch : React.FC<SaveSearchProps> = ({
         try {
             setIsLoading(true);
             const url = onGenerateUrl();
-            
+
             const values = {
-                link : url,
-                title : currentTitle,
-                checkAvailability : checkAvailability,
-                getUpdates : getUpdates
+                link: url,
+                title: currentTitle,
+                checkAvailability: checkAvailability,
+                getUpdates: getUpdates
             }
 
             await axios.post(`/api/saved-search/${userId}`, values)
@@ -87,7 +88,7 @@ const SaveSearch : React.FC<SaveSearchProps> = ({
                     router.refresh();
                 })
 
-        } catch(error : any) {
+        } catch (error: any) {
             console.log(error.message);
             toast.error("Fehler beim Speichern der Suche")
         } finally {
@@ -95,26 +96,26 @@ const SaveSearch : React.FC<SaveSearchProps> = ({
         }
     }
 
-    return ( 
+    return (
         <Dialog>
             {userId ? (
                 <DialogTrigger className="flex items-center text-xs justify-center w-full hover:underline">
-                <IoSave  className="w-4 h-4 mr-2"/>  Suche speichern
-              </DialogTrigger>
+                    <IoSave className="w-4 h-4 mr-2" />  Suche speichern
+                </DialogTrigger>
             ) : (
                 <a className="flex items-center text-xs justify-center w-full hover:underline" href="/login">
-                    <IoSave  className="w-4 h-4 mr-2"/>  Suche speichern
+                    <IoSave className="w-4 h-4 mr-2" />  Suche speichern
                 </a>
             )}
             <DialogContent className="dark:border-none dark:bg-[#191919]">
                 <div className="">
                     <div className="">
-                        <h3 className="text-md font-semibold flex items-center"> 
-                        <IoSave  className="w-4 h-4 mr-2"/> Suche speichern
+                        <h3 className="text-md font-semibold flex items-center">
+                            <IoSave className="w-4 h-4 mr-2" /> Suche speichern
                         </h3>
                         <p className="text-xs dark:text-gray-200/60">
                             Speichern Sie Ihre Suche, um sie später wiederzufinden.
-                            <br/>
+                            <br />
                             Gespeicherte Suchen finden sie in Ihrem Dashboard.
                         </p>
                     </div>
@@ -123,57 +124,61 @@ const SaveSearch : React.FC<SaveSearchProps> = ({
                             Titel
                         </Label>
                         <div>
-            	            <Input 
-                            className="dark:border-none dark:bg-[#1C1C1C] dark:text-gray-200"
-                            onChange={(e) => setCurrentTitle(e.target.value)}
+                            <Input
+                                className="dark:border-none dark:bg-[#1C1C1C] dark:text-gray-200"
+                                onChange={(e) => setCurrentTitle(e.target.value)}
+                                maxLength={100}
                             />
+                            <div className="ml-auto flex justify-end">
+                                <LetterRestriction limit={100} currentLength={currentTitle?.length ?  currentTitle?.length : 0} />
+                            </div>
                         </div>
                     </div>
                     <div className="mt-4">
                         <Label className="text-sm font-semibold">
-                        Benachrichtige mich,
+                            Benachrichtige mich,
                         </Label>
                     </div>
-                        <div className="w-full flex items-center gap-x-2 mt-2">
-                           
-                            <Checkbox 
-                            onCheckedChange={(e : boolean) => setCheckAvailability(e)}
-                            checked={checkAvailability} />
-                            
-                            
-                            <Label className="text-xs hover:underline hover:cursor-pointer" onClick={() => {setCheckAvailability(!checkAvailability)}}>
-                                 wenn ein passendes Inserat gefunden wurde.
-                            </Label>
-                        </div>
-                        
-                   
                     <div className="w-full flex items-center gap-x-2 mt-2">
-                           
-                            <Checkbox 
-                            onCheckedChange={(e : boolean) => setGetUpdates(e)}
+
+                        <Checkbox
+                            onCheckedChange={(e: boolean) => setCheckAvailability(e)}
+                            checked={checkAvailability} />
+
+
+                        <Label className="text-xs hover:underline hover:cursor-pointer" onClick={() => { setCheckAvailability(!checkAvailability) }}>
+                            wenn ein passendes Inserat gefunden wurde.
+                        </Label>
+                    </div>
+
+
+                    <div className="w-full flex items-center gap-x-2 mt-2">
+
+                        <Checkbox
+                            onCheckedChange={(e: boolean) => setGetUpdates(e)}
                             checked={getUpdates} />
-                            
-                            
-                            <Label className="text-xs hover:underline hover:cursor-pointer"
-                            onClick={() => {setGetUpdates(!getUpdates)}}
-                            >
-                                 wenn neue Ergebnisse verfügbar sind
-                            </Label>
-                        </div>
+
+
+                        <Label className="text-xs hover:underline hover:cursor-pointer"
+                            onClick={() => { setGetUpdates(!getUpdates) }}
+                        >
+                            wenn neue Ergebnisse verfügbar sind
+                        </Label>
+                    </div>
                     <div className="mt-2 w-full ml-auto flex justify-end">
                         <DialogTrigger asChild>
-                        <Button  className="bg-indigo-800 hover:bg-indigo-900 text-gray-200 hover:text-gray-300" 
-                        onClick={onSubmit}
-                        disabled={isLoading || !currentTitle || currentTitle.trim() === ""}
-                        >
-                            Speichern
-                        </Button>
+                            <Button className="bg-indigo-800 hover:bg-indigo-900 text-gray-200 hover:text-gray-300"
+                                onClick={onSubmit}
+                                disabled={isLoading || !currentTitle || currentTitle.trim() === ""}
+                            >
+                                Speichern
+                            </Button>
                         </DialogTrigger>
                     </div>
                 </div>
             </DialogContent>
         </Dialog>
-     );
+    );
 }
- 
+
 export default SaveSearch;

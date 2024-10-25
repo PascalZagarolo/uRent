594edd840fa58766471dac9d4f8fefba0d74ca2d
Pascal <zagarolopascal@gmail.com>
2024-10-25 19:08:10 +0200

    fixed--

--- hooks/subscription/useCheckSubscription.ts
+++ hooks/subscription/useCheckSubscription.ts
@@ -1,8 +1,11 @@
 import { userSubscription } from "@/db/schema";
-import { isAfter } from "date-fns";
+import { isAfter, isBefore } from "date-fns";
 
 export const checkIfValid = (publicInserate : number, existingSubscription? : typeof userSubscription.$inferSelect) => {
-    if(isAfter(new Date(existingSubscription?.stripe_current_period_end), new Date()) || !existingSubscription || existingSubscription?.amount <= publicInserate ) {
+    if(isBefore(new Date(existingSubscription?.stripe_current_period_end), new Date()) || !existingSubscription || existingSubscription?.amount <= publicInserate ) {
+        console.log(isBefore(new Date(existingSubscription?.stripe_current_period_end), new Date()))
+        console.log(!existingSubscription)
+        console.log(existingSubscription?.amount <= publicInserate)
         return false;
     } else {
         return true;

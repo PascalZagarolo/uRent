0a7a406e8e91eafb280f4d4f092e7e3c499a8c33
Pascal <zagarolopascal@gmail.com>
2024-10-27 23:56:09 +0100

    checkmark

--- app/profile/[profileId]/_components/business-render/display-business-names.tsx
+++ app/profile/[profileId]/_components/business-render/display-business-names.tsx
@@ -1,32 +1,52 @@
 'use client'
 
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 import { format } from "date-fns";
-import { User2Icon } from "lucide-react";
+import { CheckIcon, User2Icon } from "lucide-react";
+import { useParams } from "next/navigation";
 
 interface DisplayBusinessNamesProps {
     name: string;
-    sharesRealName : boolean;
-    firstName : string;
-    lastName : string;
-    joinedAt : Date;
+    sharesRealName: boolean;
+    firstName: string;
+    lastName: string;
+    joinedAt: Date;
 }
 
 
 const DisplayBusinessNames = ({ name, sharesRealName, firstName, lastName, joinedAt }: DisplayBusinessNamesProps) => {
+
+    const params = useParams().profileId;
+
+    const uRent = params == "zhv192zdl1409rq"
+
     return (
         <div className="mt-12 px-8">
-            <div>
-                <p className="text-gray-200 text-2xl font-semibold">{name}</p>
+            <div className="flex flex-row items-center">
+                <p className="text-gray-200 text-2xl font-semibold">{name}</p> {uRent &&
+                    <TooltipProvider>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <CheckIcon className="w-6 h-6 ml-4 text-indigo-800" />
+                            </TooltipTrigger>
+                            <TooltipContent className="bg-[#222222] shadow-lg border-none">
+                                <div className="text-sm text-gray-200">
+                                    <p>Offizieller Account</p>
+                                </div>
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+                }
             </div>
             {sharesRealName && (firstName || lastName) && (
                 <div className="flex flex-row items-center">
-                    <span className="text-gray-200 flex flex-row items-center">  
-                     <User2Icon className="w-4 h-4 mr-2" />   {firstName} {lastName}
+                    <span className="text-gray-200 flex flex-row items-center">
+                        <User2Icon className="w-4 h-4 mr-2" />   {firstName} {lastName}
                     </span>
                 </div>
             )}
             <div className=" text-gray-200/60 text-sm font-normal">
-            Mitglied seit {format(joinedAt, "dd.MM.yyyy")}
+                Mitglied seit {format(joinedAt, "dd.MM.yyyy")}
             </div>
         </div>
     );

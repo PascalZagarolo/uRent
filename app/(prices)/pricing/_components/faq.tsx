5c4b5520fe0b4b5a29fa4864c1571641e1515613
Pascal <zagarolopascal@gmail.com>
2024-11-09 20:16:54 +0100

    added Faq

--- app/(prices)/pricing/_components/faq.tsx
+++ app/(prices)/pricing/_components/faq.tsx
@@ -6,45 +6,57 @@ const Faq = () => {
     return (
         <div className="sm:p-0 p-2">
             <h3 className="font-semibold">
-               FAQ - Häufig gestellte Fragen
+                FAQ - Häufig gestellte Fragen
             </h3>
 
             <div>
                 <Accordion type="single" collapsible>
 
-                <AccordionItem value="item-1">
+                    <AccordionItem value="item-1">
                         <AccordionTrigger>Kann ich mein Abonnement später aufwerten?</AccordionTrigger>
                         <AccordionContent>
-                        Absolut! Wir unterstützen Flexibilität und verstehen, dass deine Bedürfnisse sich mit der Zeit ändern können. 
-                        Das Upgrade deines Abonnements ist ein einfacher Prozess. Du musst lediglich die Differenz zwischen deinem aktuellen Plan und 
-                        dem gewünschten Plan zahlen. Das bedeutet, dass du nahtlos zu einem umfangreicheren Service wechseln kannst, 
-                        um noch mehr von unseren exklusiven Angeboten zu profitieren.
+                            Absolut! Wir unterstützen Flexibilität und verstehen, dass deine Bedürfnisse sich mit der Zeit ändern können.
+                            Das Upgrade deines Abonnements ist ein einfacher Prozess. Du musst lediglich die Differenz zwischen deinem aktuellen Plan und
+                            dem gewünschten Plan zahlen. Das bedeutet, dass du nahtlos zu einem umfangreicheren Service wechseln kannst,
+                            um noch mehr von unseren exklusiven Angeboten zu profitieren.
                         </AccordionContent>
                     </AccordionItem>
 
-                <AccordionItem value="item-2">
-                        <AccordionTrigger>Wie funktioniert die Abrechnung?</AccordionTrigger>
+                    <AccordionItem value="item-2">
+                        <AccordionTrigger>Wie kann ich mein Abonnement aufwerten?</AccordionTrigger>
                         <AccordionContent>
-                        Die Abrechnung erfolgt monatlich. 
-                        Sie werden zu Beginn jedes Abrechnungszeitraums automatisch belastet, 
-                        solange Sie Ihr Abonnement nicht vor Ablauf des aktuellen Zeitraums kündigen.
+                            Um dein Abonnement aufzuwerten, bezahlst du hier lediglich die Differenz zwischen deinem aktuellen und dem gewünschten Plan. <br/>
+                            Der neue Plan wird sofort freigeschaltet, sodass du alle Features direkt nutzen kannst. <br/>
+                            Der Abrechnungszeitraum bleibt unverändert – wenn dein aktueller Abrechnungszeitraum beispielsweise noch 10 Tage umfasst, gilt dies auch nach dem Upgrade auf den neuen Plan weiterhin.  <br/> <br/>
+                            Falls dein Abonnement aktiv ist, wird der neue Plan erneuert, und der vollständige Preis wird für jeden Abo-Zyklus berechnet.
+                            Bereits gekündigte Abos bleiben jedoch weiterhin gekündigt, auch nach einem Wechsel auf einen neuen Plan.
                         </AccordionContent>
                     </AccordionItem>
-                    
+
+
                     <AccordionItem value="item-3">
-                        <AccordionTrigger>Kann ich mein Abonnement jederzeit kündigen?</AccordionTrigger>
+                        <AccordionTrigger>Wie funktioniert die Abrechnung?</AccordionTrigger>
                         <AccordionContent>
-                        Ja, Sie können Ihr Abonnement jederzeit kündigen. Die Kündigung tritt zum Ende des aktuellen Abrechnungszeitraums in Kraft. 
-                        Sie haben jedoch weiterhin Zugriff auf alle Features bis zum Ende dieses Zeitraums.
+                            Die Abrechnung erfolgt monatlich.
+                            Sie werden zu Beginn jedes Abrechnungszeitraums automatisch belastet,
+                            solange Sie Ihr Abonnement nicht vor Ablauf des aktuellen Zeitraums kündigen.
                         </AccordionContent>
                     </AccordionItem>
 
                     <AccordionItem value="item-4">
+                        <AccordionTrigger>Kann ich mein Abonnement jederzeit kündigen?</AccordionTrigger>
+                        <AccordionContent>
+                            Ja, Sie können Ihr Abonnement jederzeit kündigen. Die Kündigung tritt zum Ende des aktuellen Abrechnungszeitraums in Kraft.
+                            Sie haben jedoch weiterhin Zugriff auf alle Features bis zum Ende dieses Zeitraums.
+                        </AccordionContent>
+                    </AccordionItem>
+
+                    <AccordionItem value="item-5">
                         <AccordionTrigger>Gibt es eine Kündigungsfrist für mein Abonnement?</AccordionTrigger>
                         <AccordionContent>
-                        Nein, es gibt keine Kündigungsfrist für Ihr Abonnement. 
-                        Sie können jederzeit vor Ablauf des aktuellen Abrechnungszeitraums kündigen, und die Kündigung tritt zum Ende dieses Zeitraums in Kraft. 
-                        Es gibt keine versteckten Gebühren oder zusätzlichen Verpflichtungen.
+                            Nein, es gibt keine Kündigungsfrist für Ihr Abonnement.
+                            Sie können jederzeit vor Ablauf des aktuellen Abrechnungszeitraums kündigen, und die Kündigung tritt zum Ende dieses Zeitraums in Kraft.
+                            Es gibt keine versteckten Gebühren oder zusätzlichen Verpflichtungen.
                         </AccordionContent>
                     </AccordionItem>
 

c0764110ddd6cc46783fcb9e94ae8eb190ebc128
Pascal <zagarolopascal@gmail.com>
2024-11-10 23:39:18 +0100

    added hover effects on calendar availability..

--- app/(anzeige)/inserat/[inseratId]/_components/booking-day.tsx
+++ app/(anzeige)/inserat/[inseratId]/_components/booking-day.tsx
@@ -54,11 +54,12 @@ const CalendarDay: React.FC<CalendarDayProps> = ({
 
       <div
         key={index}
-        className={clsx("dark:bg-[#0F0F0F]  p-4 text-center rounded-md", {
+        className={clsx("dark:bg-[#0F0F0F]  p-4 text-center rounded-md ", {
           "bg-gray-200": isToday(day),
           "font-black": isToday(day),
-          "dark:bg-rose-800 bg-rose-800": isCompletelyUnaivalable,
-          "dark:bg-indigo-800 bg-indigo-500": isPartiallyUnaivalable && !isCompletelyUnaivalable,
+          "dark:bg-rose-800 bg-rose-800 hover:bg-rose-900": isCompletelyUnaivalable,
+          "dark:bg-indigo-800 bg-indigo-500 hover:bg-indigo-900": isPartiallyUnaivalable && !isCompletelyUnaivalable,
+          "hover:bg-[#1b1b1b]": !isCompletelyUnaivalable && !isPartiallyUnaivalable
         })}
       >
         {isCompletelyUnaivalable ? (

730bd627f80a54b72015876996608787c489c21d
Pascal <zagarolopascal@gmail.com>
2024-11-13 20:56:24 +0100

    replaced images..

--- app/(dashboard)/(legals)/contact/_components/step-plan.tsx
+++ app/(dashboard)/(legals)/contact/_components/step-plan.tsx
@@ -23,7 +23,7 @@ const StepPlan = () => {
         {/* Card Component */}
         <div className="bg-[#1e1e1e] shadow-md hover:shadow-lg transition-transform transform hover:scale-105 rounded-lg w-full md:w-1/3">
             <img 
-                src="https://st.depositphotos.com/1062116/2309/i/950/depositphotos_23099446-stock-photo-searching-like-a-boss.jpg"
+                src="https://res.cloudinary.com/df1vnhnzp/image/upload/v1731527499/jvx4phcwwg6lsge3h2eo.jpg"
                 alt="FAQ Search"
                 className="object-cover h-52 w-full rounded-t-lg"
             />
@@ -48,7 +48,7 @@ const StepPlan = () => {
         {/* Card Component */}
         <div className="bg-[#1e1e1e] shadow-md hover:shadow-lg transition-transform transform hover:scale-105 rounded-lg w-full md:w-1/3">
             <img 
-                src="https://i.pinimg.com/736x/ba/a9/b4/baa9b4bafd99d46140a96d3e9b74fe30.jpg"
+                src="https://res.cloudinary.com/df1vnhnzp/image/upload/v1731527716/tfdnx0w0f0gep26iwgfi.jpg"
                 alt="About Us"
                 className="object-cover h-52 w-full rounded-t-lg"
             />
@@ -60,7 +60,7 @@ const StepPlan = () => {
                     Bei persönlicheren Fragen kann das „Über Uns“ helfen. <br/> Dort findest du mehr Informationen über uRent & Co.
                 </p>
                 <div className="mt-2">
-                    <Button className="bg-[#383838] hover:bg-[#4a4a4a] shadow-md hover:shadow-lg transition rounded-md text-white flex items-center space-x-2"
+                    <Button className="bg-[#383838] hover:bg-[#4a4a4a] shadow-md hovser:shadow-lg transition rounded-md text-white flex items-center space-x-2"
                     onClick={() => router.push("/about-us")}
                     >
                         <span>Mehr erfahren</span> 
@@ -73,7 +73,7 @@ const StepPlan = () => {
         {/* Card Component */}
         <div className="bg-[#1e1e1e] shadow-md hover:shadow-lg transition-transform transform hover:scale-105 rounded-lg w-full md:w-1/3">
             <img 
-                src="https://www.engineersireland.ie/Portals/0/Images/contact/Contact_650x366_GetInTouch.jpg"
+                src="https://res.cloudinary.com/df1vnhnzp/image/upload/v1731527667/azakudbedzjz8d3vfeoh.jpg"
                 alt="Contact Us"
                 className="object-cover h-52 w-full rounded-t-lg"
             />

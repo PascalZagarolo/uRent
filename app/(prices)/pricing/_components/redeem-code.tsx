f36dc3480b2ee33232894f47af571bea355bf9b3
Pascal <zagarolopascal@gmail.com>
2024-11-26 11:48:53 +0100

    ...

--- app/(prices)/pricing/_components/redeem-code.tsx
+++ app/(prices)/pricing/_components/redeem-code.tsx
@@ -38,9 +38,9 @@ const RedeemCode = () => {
                     setUsedCode("")
                     router.refresh();
                 })
-            
-            
-        } catch(error : any) {
+
+
+        } catch (error: any) {
             console.log(error?.response?.data);
             setError(error?.response?.data);
             toast.error("Dieser Gutscheincode kann nicht verwendet werden.");
@@ -48,93 +48,95 @@ const RedeemCode = () => {
             setIsLoading(false);
         }
     }
-    
+
     const formSchema = z.object({
-        pin : z.string().optional()
+        pin: z.string().optional()
     })
 
     const form = useForm<z.infer<typeof formSchema>>({
-        resolver : zodResolver(formSchema),
-        defaultValues : {
-            pin : ""
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            pin: ""
         }
     })
 
     return (
         <div className="w-full">
-            
+
             <div className="w-full mt-2">
-                
-                    
-                    <div className="dark:border-none dark:bg-[#191919] rounded-md p-4 w-full">
+
+
+                <div className="dark:border-none dark:bg-[#191919] rounded-md p-4 w-full">
+                    <div>
                         <div>
-                            <div>
-                                <h3 className="font-semibold text-md flex items-center">
-                                    <MdOutlineRedeem className="w-4 h-4 mr-2" />  Code einlösen
-                                </h3>
-                                <p className="text-xs dark:text-gray-200/70">
-                                    Gebe hier deinen 16-stelligen Code oder diverse Promocodes ein, Abos & Vorteile einzulösen.
-                                </p>
-                            </div>
+                            <h3 className="font-semibold text-md flex items-center">
+                                <MdOutlineRedeem className="w-4 h-4 mr-2" />  Code einlösen
+                            </h3>
+                            <p className="text-xs dark:text-gray-200/70">
+                                Gebe hier deinen 16-stelligen Code oder diverse Promocodes ein, um Abos & Vorteile einzulösen.
+                            </p>
+                        </div>
+                        {error && (
                             <div className="text-sm py-2 bg-rose-600/20 p-4 border border-rose-800/20 mt-2 mb-2 shadow-lg">
                                 <div className="flex flex-row items-center gap-x-2 text-base font-semibold">
                                     <AlertCircleIcon className="w-4 h-4 text-rose-800" />
                                     Beim Einlösen deines Codes ist ein Fehler aufgetreten.
                                 </div>
                                 <div className="text-sm text-gray-200/80 mt-2">
-                                {error}
+                                    {error}
                                 </div>
                             </div>
-                            <div className="mt-2">
-                                <Form {...form}>
-                                    <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
-                                        <FormField
-                                            control={form.control}
-                                            name="pin"
-                                            render={({ field }) => (
-                                                <FormItem>
-                                                    <FormLabel className="text-sm">Gutscheincode</FormLabel>
-                                                    <FormControl>
-                                                        <Input 
-                                                        
+                        )}
+                        <div className="mt-2">
+                            <Form {...form}>
+                                <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
+                                    <FormField
+                                        control={form.control}
+                                        name="pin"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel className="text-sm">Gutscheincode</FormLabel>
+                                                <FormControl>
+                                                    <Input
+
                                                         name="pin"
                                                         className="dark:bg-[#131313] shadow-lg sm:w-1/2 dark:border-none"
                                                         onChange={(e) => setUsedCode(e.target.value)}
                                                         value={usedCode}
                                                         placeholder="Promocode.."
-                                                        />
-                                                    </FormControl>
-                                                    <div className="text-xs dark:text-gray-200/60">
+                                                    />
+                                                </FormControl>
+                                                <div className="text-xs dark:text-gray-200/60">
+                                                    <div>
+                                                        Um über die neuesten Angebote und Aktionen informiert zu werden, <br />
+                                                        melde dich unter deinem Profil für unseren Newsletter an.<br /><br />
                                                         <div>
-                                                        Um über die neuesten Angebote und Aktionen informiert zu werden, <br/>
-                                                        melde dich unter deinem Profil für unseren Newsletter an.<br/><br/>
-                                                        <div>
-                                                        
-                                                        Für weitere Informationen rundum das Thema Gutscheincodes klicke <a className="underline" href="/faq/giftcodes">hier</a>.
-                                                        </div>
+
+                                                            Für weitere Informationen rundum das Thema Gutscheincodes klicke <a className="underline" href="/faq/giftcodes">hier</a>.
                                                         </div>
                                                     </div>
-                                                    <FormMessage />
-                                                </FormItem>
-                                            )}
-                                        />
+                                                </div>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
 
-                                        <Button variant="ghost" 
+                                    <Button variant="ghost"
                                         className="mt-2 text-gray-200 bg-indigo-800 hover:bg-indigo-900 dark:hover:text-gray-300"
                                         disabled={!usedCode || isLoading}
-                                        >
-                                            {isLoading ? <ClipLoader
-                                                color="#2563EB"
-                                                loading={true}
-                                                size={20}
-                                            /> : "Code überprüfen"}
-                                        </Button>
-                                    </form>
-                                </Form>
-                            </div>
+                                    >
+                                        {isLoading ? <ClipLoader
+                                            color="#2563EB"
+                                            loading={true}
+                                            size={20}
+                                        /> : "Code überprüfen"}
+                                    </Button>
+                                </form>
+                            </Form>
                         </div>
                     </div>
-               
+                </div>
+
             </div>
 
         </div>

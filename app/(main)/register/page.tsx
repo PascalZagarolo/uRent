e739bf46000e31189c172f848b830ac0960dbc55
Pascal <zagarolopascal@gmail.com>
2025-01-01 22:56:36 +0100

    ...

--- app/(main)/register/page.tsx
+++ app/(main)/register/page.tsx
@@ -2,11 +2,27 @@ import { RegisterForm } from "../_components/register-form";
 
 
 const RegisterPage = () => {
-  return ( 
-    <div className="w-full flex justify-center">
+  return (
+    <>
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16814367985"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+        window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-16814367985');
+        `
+        }} async>
+
+        </script>
+      </head>
+      <div className="w-full flex justify-center">
         <RegisterForm />
-    </div>
+      </div>
+    </>
   );
 }
- 
+
 export default RegisterPage;

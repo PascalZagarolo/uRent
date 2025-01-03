d2aa1d66da67115fd7e49e906619aa78a76a1dde
Pascal <zagarolopascal@gmail.com>
2025-01-03 19:03:56 +0100

    added face tag..

--- app/(main)/register/page.tsx
+++ app/(main)/register/page.tsx
@@ -19,8 +19,25 @@ const RegisterPage = () => {
                 }} async>
                     
                 </script>
+                <script dangerouslySetInnerHTML={{
+          __html: `
+                    !function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1328385231485028');
+fbq('track', 'PageView');
+                    `
+        }} async>
 
-       
+        </script>
+        <noscript><img height="1" width="1" className="display:none"
+          src="https://www.facebook.com/tr?id=1328385231485028&ev=PageView&noscript=1"
+        /></noscript>
       </head>
       <div className="w-full flex justify-center">
         <RegisterForm />
--- app/layout.tsx
+++ app/layout.tsx
@@ -42,15 +42,34 @@ export default function RootLayout({
         <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16814367985"></script>
 
         <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16814367985"></script>
-        <script>
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){
-            dataLayer.push(arguments);
-          }
-          gtag('js', new Date());
-          gtag('config', 'AW-16814367985');
-          `}
+        <script dangerouslySetInnerHTML={{
+          __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag() {
+                        dataLayer.push(arguments);
+                        }
+                    gtag('js', new Date());
+
+                    gtag('config', 'AW-16814367985');
+                    `
+        }} async>
+
+        </script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+                    !function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1328385231485028');
+fbq('track', 'PageView');
+                    `
+        }} async>
+
         </script>
 
 
@@ -60,7 +79,9 @@ export default function RootLayout({
 
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9497499351411762" //@ts-ignore
           crossorigin="anonymous"></script>
-
+        <noscript><img height="1" width="1" className="display:none"
+          src="https://www.facebook.com/tr?id=1328385231485028&ev=PageView&noscript=1"
+        /></noscript>
       </head>
       <body className={inter.className}>
         <ThemeProvider

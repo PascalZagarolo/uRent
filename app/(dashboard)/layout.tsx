717f11a15f2b75c2c94fcc62d21549fba8d0a893
Pascal <zagarolopascal@gmail.com>
2025-02-11 19:07:33 +0100

    ...

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -33,7 +33,7 @@ const DashboardLayout = cache(async (
 
 
     return (
-        <div className="bg-[#404040]/10 dark:bg-[#0F0F0F] min-h-screen no-scrollbar">
+        <div className=" dark:bg-[#101114] min-h-screen no-scrollbar">
             <div className="sm:hidden">
                 <MobileHeader
                     currentUser={currentUser as any}

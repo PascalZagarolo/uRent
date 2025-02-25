bab651137d44543185d94286c84b9f217311e2b6
Pascal <zagarolopascal@gmail.com>
2025-02-25 12:21:37 +0100

    ...

---
+++ drizzle/0017_burly_blindfold.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "booking" ALTER COLUMN "startDate" SET DATA TYPE timestamp with time zone;--> statement-breakpoint
+ALTER TABLE "booking" ALTER COLUMN "endDate" SET DATA TYPE timestamp with time zone;
